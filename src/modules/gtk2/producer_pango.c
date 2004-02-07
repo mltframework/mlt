36bac6f151e7e97a3caa2ec9169df7e97190b1e3
lilo_booter <lilo_booter@d19143bc-622f-0410-bfdd-b5b2a6649095>
2004-02-07 13:09:44 +0000

    pango producer rework
    git-svn-id: https://mlt.svn.sourceforge.net/svnroot/mlt/trunk/mlt@122 d19143bc-622f-0410-bfdd-b5b2a6649095

--- src/modules/gtk2/producer_pango.c
+++ src/modules/gtk2/producer_pango.c
@@ -130,10 +130,166 @@ mlt_producer producer_pango_init( const char *filename )
 	return NULL;
 }
 
-static int producer_get_image( mlt_frame this, uint8_t **buffer, mlt_image_format *format, int *width, int *height, int writable )
+static void refresh_image( mlt_frame frame, int width, int height )
+{
+	// Pixbuf 
+	GdkPixbuf *pixbuf = NULL;
+
+	// Obtain properties of frame
+	mlt_properties properties = mlt_frame_properties( frame );
+
+	// Obtain the producer pango for this frame
+	producer_pango this = mlt_properties_get_data( properties, "producer_pango", NULL );
+
+	// Obtain the producer 
+	mlt_producer producer = &this->parent;
+
+	// Obtain the producer properties
+	mlt_properties producer_props = mlt_producer_properties( producer );
+
+	// Get producer properties
+	int fg = mlt_properties_get_int( producer_props, "fgcolor" );
+	int bg = mlt_properties_get_int( producer_props, "bgcolor" );
+	int align = mlt_properties_get_int( producer_props, "align" );
+	int pad = mlt_properties_get_int( producer_props, "pad" );
+	char *markup = mlt_properties_get( producer_props, "markup" );
+	char *text = mlt_properties_get( producer_props, "text" );
+	char *font = mlt_properties_get( producer_props, "font" );
+
+	// See if any properties changed
+	int property_changed = ( fg != this->fgcolor );
+	property_changed = property_changed || ( bg != this->bgcolor );
+	property_changed = property_changed || ( align != this->align );
+	property_changed = property_changed || ( pad != this->pad );
+	property_changed = property_changed || ( markup && this->markup && strcmp( markup, this->markup ) );
+	property_changed = property_changed || ( text && this->text && strcmp( text, this->text ) );
+	property_changed = property_changed || ( font && this->font && strcmp( font, this->font ) );
+
+	// Save the properties for next comparison
+	this->fgcolor = fg;
+	this->bgcolor = bg;
+	this->align = align;
+	this->pad = pad;
+	if ( markup != NULL )
+	{
+		free( this->markup );
+		this->markup = strdup( markup );
+	}
+	if ( text != NULL )
+	{
+		free( this->text );
+		this->text = strdup( text );
+	}
+	if ( font != NULL )
+	{
+		free( this->font );
+		this->font = strdup( font );
+	}
+
+	if ( property_changed )
+	{
+		rgba_color fgcolor =
+		{
+			( fg & 0xff000000 ) >> 24,
+			( fg & 0x00ff0000 ) >> 16,
+			( fg & 0x0000ff00 ) >> 8,
+			( fg & 0x000000ff )
+		};
+		rgba_color bgcolor =
+		{
+			( bg & 0xff000000 ) >> 24,
+			( bg & 0x00ff0000 ) >> 16,
+			( bg & 0x0000ff00 ) >> 8,
+			( bg & 0x000000ff )
+		};
+
+		// Render the title
+		pixbuf = pango_get_pixbuf( markup, text, font, fgcolor, bgcolor, pad, align );
+
+		if ( pixbuf != NULL )
+		{
+			// Register this pixbuf for destruction and reuse
+			mlt_properties_set_data( producer_props, "pixbuf", pixbuf, 0, ( mlt_destructor )g_object_unref, NULL );
+
+			// Store the width/height of the pixbuf temporarily
+			this->width = gdk_pixbuf_get_width( pixbuf );
+			this->height = gdk_pixbuf_get_height( pixbuf );
+		}
+	}
+	else if ( width != this->width || height != this->height )
+	{
+		pixbuf = mlt_properties_get_data( producer_props, "pixbuf", NULL );
+	}
+
+	// If we have a pixbuf and a valid width
+	if ( pixbuf && width > 0 )
+	{
+		int delete = 0;
+
+		// Scale to width/height requested
+		if ( width != this->width && height != this->height )
+		{
+			// Note - the original pixbuf is already safe and ready for destruction
+			pixbuf = gdk_pixbuf_scale_simple( pixbuf, width, height, GDK_INTERP_HYPER );
+			delete = 1;
+		}
+
+		// Store width and height
+		this->width = gdk_pixbuf_get_width( pixbuf );
+		this->height = gdk_pixbuf_get_height( pixbuf );
+
+		// Allocate/define image and alpha
+		uint8_t *image = malloc( this->width * this->height * 2 );
+		uint8_t *alpha = NULL;
+
+		// Extract YUV422 and alpha
+		if ( gdk_pixbuf_get_has_alpha( pixbuf ) )
+		{
+			// Allocate the alpha mask
+			alpha = malloc( this->width * this->height );
+
+			// Convert the image
+			mlt_convert_rgb24a_to_yuv422( gdk_pixbuf_get_pixels( pixbuf ),
+										  this->width, this->height,
+										  gdk_pixbuf_get_rowstride( pixbuf ),
+										  image, alpha );
+		}
+		else
+		{ 
+			// No alpha to extract
+			mlt_convert_rgb24_to_yuv422( gdk_pixbuf_get_pixels( pixbuf ),
+										 this->width, this->height,
+										 gdk_pixbuf_get_rowstride( pixbuf ),
+										 image );
+		}
+
+		// Finished with pixbuf now
+		if ( delete )
+			g_object_unref( pixbuf );
+		
+		// if single picture, reference the image and alpha in the producer
+		free( this->image );
+		this->image = image;
+		free( this->alpha );
+		this->alpha = alpha;
+	}
+
+	// Set width/height
+	mlt_properties_set_int( properties, "width", this->width );
+	mlt_properties_set_int( properties, "height", this->height );
+
+	// pass the image and alpha data without destructor
+	mlt_properties_set_data( properties, "image", this->image, this->width * this->height * 2, NULL, NULL );
+	mlt_properties_set_data( properties, "alpha", this->alpha, this->width * this->height, NULL, NULL );
+}
+
+static int producer_get_image( mlt_frame frame, uint8_t **buffer, mlt_image_format *format, int *width, int *height, int writable )
 {
 	// Obtain properties of frame
-	mlt_properties properties = mlt_frame_properties( this );
+	mlt_properties properties = mlt_frame_properties( frame );
+
+	// Refresh the image
+	refresh_image( frame, *width, *height );
 
 	// May need to know the size of the image to clone it
 	int size = 0;
@@ -177,167 +333,24 @@ static uint8_t *producer_get_alpha_mask( mlt_frame this )
 static int producer_get_frame( mlt_producer producer, mlt_frame_ptr frame, int index )
 {
 	producer_pango this = producer->child;
-	GdkPixbuf *pixbuf = NULL;
 
 	// Generate a frame
 	*frame = mlt_frame_init( );
 
 	// Obtain properties of frame and producer
 	mlt_properties properties = mlt_frame_properties( *frame );
-	mlt_properties producer_props = mlt_producer_properties( producer );
-	
-	// Get producer properties
-	int fg = mlt_properties_get_int( producer_props, "fgcolor" );
-	int bg = mlt_properties_get_int( producer_props, "bgcolor" );
-	int align = mlt_properties_get_int( producer_props, "align" );
-	int pad = mlt_properties_get_int( producer_props, "pad" );
-	char *markup = mlt_properties_get( producer_props, "markup" );
-	char *text = mlt_properties_get( producer_props, "text" );
-	char *font = mlt_properties_get( producer_props, "font" );
 
-	// See if any properties changed
-	int property_changed = ( fg != this->fgcolor );
-	property_changed = property_changed || ( bg != this->bgcolor );
-	property_changed = property_changed || ( align != this->align );
-	property_changed = property_changed || ( pad != this->pad );
-	property_changed = property_changed || ( markup && this->markup && strcmp( markup, this->markup ) );
-	property_changed = property_changed || ( text && this->text && strcmp( text, this->text ) );
-	property_changed = property_changed || ( font && this->font && strcmp( font, this->font ) );
+	// Set the producer on the frame properties
+	mlt_properties_set_data( properties, "producer_pango", this, 0, NULL, NULL );
 
-	// Save the properties for next comparison
-	this->fgcolor = fg;
-	this->bgcolor = bg;
-	this->align = align;
-	this->pad = pad;
-	if ( markup != NULL )
-	{
-		if ( this->markup != NULL )
-			free( this->markup );
-		this->markup = strdup( markup );
-	}
-	if ( text != NULL )
-	{
-		if ( this->text != NULL )
-			free( this->text );
-		this->text = strdup( text );
-	}
-	if ( font != NULL )
-	{
-		if ( this->font != NULL )
-			free( this->font );
-		this->font = strdup( font );
-	}
+	// Refresh the pango image
+	refresh_image( *frame, 0, 0 );
 
-	if ( property_changed )
-	{
-		rgba_color fgcolor =
-		{
-			( fg & 0xff000000 ) >> 24,
-			( fg & 0x00ff0000 ) >> 16,
-			( fg & 0x0000ff00 ) >> 8,
-			( fg & 0x000000ff )
-		};
-		rgba_color bgcolor =
-		{
-			( bg & 0xff000000 ) >> 24,
-			( bg & 0x00ff0000 ) >> 16,
-			( bg & 0x0000ff00 ) >> 8,
-			( bg & 0x000000ff )
-		};
+	// Set alpha mask call back
+	( *frame )->get_alpha_mask = producer_get_alpha_mask;
 
-		// Render the title
-		pixbuf = pango_get_pixbuf( markup, text, font, fgcolor, bgcolor, pad, align );
-	}
-
-	// If we have a pixbuf
-	if ( pixbuf )
-	{
-		// Scale to adjust for sample aspect ratio
-		if ( mlt_properties_get_int( properties, "video_standard" ) == mlt_video_standard_pal )
-		{
-			GdkPixbuf *temp = pixbuf;
-			GdkPixbuf *scaled = gdk_pixbuf_scale_simple( pixbuf,
-				(gint) ( (float) gdk_pixbuf_get_width( pixbuf ) * 45.0/48.0),
-				gdk_pixbuf_get_height( pixbuf ), GDK_INTERP_HYPER );
-			pixbuf = scaled;
-			g_object_unref( temp );
-		}
-		else
-		{
-			GdkPixbuf *temp = pixbuf;
-			GdkPixbuf *scaled = gdk_pixbuf_scale_simple( pixbuf,
-				(gint) ( (float) gdk_pixbuf_get_width( pixbuf ) * 9.0/8.0 ),
-				gdk_pixbuf_get_height( pixbuf ), GDK_INTERP_HYPER );
-			pixbuf = scaled;
-			g_object_unref( temp );
-		}
-
-		// Store width and height
-		this->width = gdk_pixbuf_get_width( pixbuf );
-		this->height = gdk_pixbuf_get_height( pixbuf );
-
-		// Allocate/define image and alpha
-		uint8_t *image = malloc( this->width * this->height * 2 );
-		uint8_t *alpha = NULL;
-
-		// Extract YUV422 and alpha
-		if ( gdk_pixbuf_get_has_alpha( pixbuf ) )
-		{
-			// Allocate the alpha mask
-			alpha = malloc( this->width * this->height );
-
-			// Convert the image
-			mlt_convert_rgb24a_to_yuv422( gdk_pixbuf_get_pixels( pixbuf ),
-										  this->width, this->height,
-										  gdk_pixbuf_get_rowstride( pixbuf ),
-										  image, alpha );
-		}
-		else
-		{ 
-			// No alpha to extract
-			mlt_convert_rgb24_to_yuv422( gdk_pixbuf_get_pixels( pixbuf ),
-										 this->width, this->height,
-										 gdk_pixbuf_get_rowstride( pixbuf ),
-										 image );
-		}
-
-		// Finished with pixbuf now
-		g_object_unref( pixbuf );
-		
-		// if single picture, reference the image and alpha in the producer
-		if ( this->image != NULL )
-			free( this->image );
-		this->image = image;
-		if ( this->alpha != NULL )
-			free( this->alpha );
-		this->alpha = alpha;
-
-	}
-
-	if ( this->image != NULL )
-	{
-		// Set width/height
-		mlt_properties_set_int( properties, "width", this->width );
-		mlt_properties_set_int( properties, "height", this->height );
-
-		// Set the compositing properties
-		if ( mlt_properties_get( producer_props, "x" ) != NULL )
-			mlt_properties_set_int( properties, "x", mlt_properties_get_int( producer_props, "x" ) );
-		if ( mlt_properties_get( producer_props, "y" ) != NULL )
-			mlt_properties_set_int( properties, "y", mlt_properties_get_int( producer_props, "y" ) );
-		if ( mlt_properties_get( producer_props, "mix" ) != NULL )
-			mlt_properties_set_double( properties, "image.mix",  mlt_properties_get_double( producer_props, "mix" ) );
-
-		// if picture sequence pass the image and alpha data without destructor
-		mlt_properties_set_data( properties, "image", this->image, this->width * this->height * 2, NULL, NULL );
-		mlt_properties_set_data( properties, "alpha", this->alpha, this->width * this->height, NULL, NULL );
-
-		// Set alpha mask call back
-		( *frame )->get_alpha_mask = producer_get_alpha_mask;
-
-		// Stack the get image callback
-		mlt_frame_push_get_image( *frame, producer_get_image );
-	}
+	// Stack the get image callback
+	mlt_frame_push_get_image( *frame, producer_get_image );
 
 	// Update timecode on the frame we're creating
 	mlt_frame_set_position( *frame, mlt_producer_position( producer ) );
@@ -351,16 +364,11 @@ static int producer_get_frame( mlt_producer producer, mlt_frame_ptr frame, int i
 static void producer_close( mlt_producer parent )
 {
 	producer_pango this = parent->child;
-	if ( this->image != NULL )
-		free( this->image );
-	if ( this->alpha != NULL )
-		free( this->alpha );
-	if ( this->markup != NULL )
-		free( this->markup );
-	if ( this->text != NULL )
-		free( this->text );
-	if ( this->font != NULL )
-		free( this->font );
+	free( this->image );
+	free( this->alpha );
+	free( this->markup );
+	free( this->text );
+	free( this->font );
 	parent->close = NULL;
 	mlt_producer_close( parent );
 	free( this );
