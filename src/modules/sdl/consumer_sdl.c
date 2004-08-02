7f00120cd5cc8a7194ca016f3a340b57cf5703f5
lilo_booter <lilo_booter@d19143bc-622f-0410-bfdd-b5b2a6649095>
2004-08-02 14:18:54 +0000

    Rectangle added to properties
    git-svn-id: https://mlt.svn.sourceforge.net/svnroot/mlt/trunk/mlt@349 d19143bc-622f-0410-bfdd-b5b2a6649095

--- src/modules/sdl/consumer_sdl.c
+++ src/modules/sdl/consumer_sdl.c
@@ -472,6 +472,11 @@ static int consumer_play_video( consumer_sdl this, mlt_frame frame )
 			
 			this->rect.x = ( this->window_width - this->rect.w ) / 2;
 			this->rect.y = ( this->window_height - this->rect.h ) / 2;
+
+			mlt_properties_set_int( this->properties, "rect_x", this->rect.x );
+			mlt_properties_set_int( this->properties, "rect_y", this->rect.y );
+			mlt_properties_set_int( this->properties, "rect_w", this->rect.w );
+			mlt_properties_set_int( this->properties, "rect_h", this->rect.h );
 			
 			// Force an overlay recreation
 			if ( this->sdl_overlay != NULL )
