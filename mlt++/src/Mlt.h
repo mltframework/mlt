4bf33d8de1c1887c6386f8d151c479ae0631a9aa
lilo_booter <lilo_booter@d19143bc-622f-0410-bfdd-b5b2a6649095>
2004-08-16 08:00:58 +0000

    Added Mlt.h convenience header
    git-svn-id: https://mlt.svn.sourceforge.net/svnroot/mlt/trunk/mlt++@368 d19143bc-622f-0410-bfdd-b5b2a6649095

---
+++ mlt++/src/Mlt.h
@@ -0,0 +1,34 @@
+/**
+ * Mlt.h - Convenience header file for all mlt++ objects
+ * Copyright (C) 2004-2005 Charles Yates
+ * Author: Charles Yates <charles.yates@pandora.be>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _MLTPP_H_
+#define _MLTPP_H_
+
+#include "MltConsumer.h"
+#include "MltFactory.h"
+#include "MltFilter.h"
+#include "MltFrame.h"
+#include "MltPlaylist.h"
+#include "MltProducer.h"
+#include "MltProperties.h"
+#include "MltService.h"
+#include "MltTransition.h"
+
+#endif
