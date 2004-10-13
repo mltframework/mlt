d3e965e4d67fcba9565f415d6915690306e31d6a
lilo_booter <lilo_booter@d19143bc-622f-0410-bfdd-b5b2a6649095>
2004-10-13 07:40:15 +0000

    Oops - fix for consumer progressive
    git-svn-id: https://mlt.svn.sourceforge.net/svnroot/mlt/trunk/mlt@474 d19143bc-622f-0410-bfdd-b5b2a6649095

--- src/framework/mlt_consumer.c
+++ src/framework/mlt_consumer.c
@@ -221,7 +221,7 @@ mlt_frame mlt_consumer_get_frame( mlt_consumer this )
 		// Aspect ratio and other jiggery pokery
 		mlt_properties_set_double( frame_properties, "consumer_aspect_ratio", mlt_properties_get_double( properties, "aspect_ratio" ) );
 		if ( mlt_properties_get_int( properties, "progressive" ) || mlt_properties_get_int( properties, "deinterlace" ) )
-			mlt_properties_set_int( properties, "consumer_deinterlace", 1 );
+			mlt_properties_set_int( frame_properties, "consumer_deinterlace", 1 );
 	}
 
 	// Return the frame
