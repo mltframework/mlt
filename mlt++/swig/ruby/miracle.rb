43df55b16132c0fd064266c5148c5ac2835c9880
lilo_booter <lilo_booter@d19143bc-622f-0410-bfdd-b5b2a6649095>
2004-10-27 10:55:49 +0000

    Config changes
    git-svn-id: https://mlt.svn.sourceforge.net/svnroot/mlt/trunk/mlt++@493 d19143bc-622f-0410-bfdd-b5b2a6649095

--- mlt++/swig/ruby/miracle.rb
+++ mlt++/swig/ruby/miracle.rb
@@ -1,5 +1,17 @@
 require 'mltpp'
-miracle = Mltpp::Miracle.new( "miracle-ruby" )
+
+def command 
+	puts "command"
+end
+
+def push 
+	puts "push"
+end
+
+miracle = Mltpp::Miracle.new( "miracle-ruby", 5260 )
 miracle.start
+miracle.execute( "uadd sdl" )
+listener = Mltpp::Listener.new( miracle, "command-received", method( :command ) )
+listener = Mltpp::Listener.new( miracle, "push-received", method( :push ) )
 miracle.wait_for_shutdown
 
