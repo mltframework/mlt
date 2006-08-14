4aa9641feaa9f2799616ec0371f403b9ee875cf5
lilo_booter <lilo_booter@d19143bc-622f-0410-bfdd-b5b2a6649095>
2006-08-14 09:56:04 +0000

    + Header file for mono filter
    git-svn-id: https://mlt.svn.sourceforge.net/svnroot/mlt/trunk/mlt@926 d19143bc-622f-0410-bfdd-b5b2a6649095

---
+++ src/modules/vmfx/filter_mono.h
@@ -0,0 +1,28 @@
+/*
+ * filter_shape.h -- Arbitrary alpha channel shaping
+ * Copyright (C) 2005 Visual Media Fx Inc.
+ * Author: Charles Yates <charles.yates@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _FILTER_MONO_H_
+#define _FILTER_MONO_H_
+
+#include <framework/mlt_filter.h>
+
+extern mlt_filter filter_mono_init( char *arg );
+
+#endif
