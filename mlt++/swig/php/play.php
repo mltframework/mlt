b5ffb17cd26375ec255f2820cd224495f6586e5b
ddennedy <ddennedy@d19143bc-622f-0410-bfdd-b5b2a6649095>
2009-01-26 18:32:03 +0000

    swig/configure, swig/php/*: add php bindings
    git-svn-id: https://mlt.svn.sourceforge.net/svnroot/mlt/trunk/mlt++@1325 d19143bc-622f-0410-bfdd-b5b2a6649095

---
+++ mlt++/swig/php/play.php
@@ -0,0 +1,21 @@
+<?php
+$filename = $argv[1];
+dl("mltpp.so");
+mlt_factory_init(NULL);
+$profile = new_profile("dv_ntsc");
+$p = new_producer( $profile, $filename );
+if ( $p ) {
+	$c = new_consumer( $profile, "sdl" );
+	consumer_connect( $c, $p );
+	$e = properties_setup_wait_for( $c, "consumer-stopped" );
+	consumer_start( $c );
+	properties_wait_for( $c, $e );
+	consumer_stop( $c );
+	$e = NULL;
+	$c = NULL;
+}
+$p = NULL;
+$profile = NULL;
+mlt_factory_close();
+?>
+
