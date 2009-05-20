7241f53da8e5c13e149fbbc68cd23173155d9da7
Dan Dennedy <dan@dennedy.org>
2009-05-20 00:45:58 -0700

    Bump to an interim version.
    Signed-off-by: Dan Dennedy <dan@dennedy.org>

--- src/framework/mlt.h
+++ src/framework/mlt.h
@@ -23,8 +23,8 @@
 #ifndef _MLT_H_
 #define _MLT_H_
 
-#define LIBMLT_VERSION_INT ((0<<16)+(4<<8)+0)
-#define LIBMLT_VERSION     0.4.0
+#define LIBMLT_VERSION_INT ((0<<16)+(4<<8)+1)
+#define LIBMLT_VERSION     0.4.1
 
 #ifdef __cplusplus
 extern "C"
