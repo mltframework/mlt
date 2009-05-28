e96178aee05967d3f10921eecd2991aed2b6a26a
Dan Dennedy <dan@dennedy.org>
2009-05-27 20:23:20 -0700

    Change default log level to panic.
    Some testing against FFmpeg v0.5 shows logging is still verbose with
    warnings enabled, but panic is better?
    
    Signed-off-by: Dan Dennedy <dan@dennedy.org>

--- src/framework/mlt_log.c
+++ src/framework/mlt_log.c
@@ -26,7 +26,7 @@
 
 #include <string.h>
 
-static int log_level = MLT_LOG_WARNING;
+static int log_level = MLT_LOG_PANIC;
 
 void default_callback( void* ptr, int level, const char* fmt, va_list vl )
 {
