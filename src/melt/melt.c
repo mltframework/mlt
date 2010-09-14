a51ff9de89df080d6a495d5830446cde16ca3056
Dan Dennedy <dan@dennedy.org>
2010-09-13 22:58:21 -0700

    Update year in copyright notice.

--- src/melt/melt.c
+++ src/melt/melt.c
@@ -1,6 +1,6 @@
 /*
  * melt.c -- MLT command line utility
- * Copyright (C) 2002-2008 Ushodaya Enterprises Limited
+ * Copyright (C) 2002-2010 Ushodaya Enterprises Limited
  * Author: Charles Yates <charles.yates@pandora.be>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -409,7 +409,7 @@ query_all:
 		else if ( !strcmp( argv[ i ], "-version" ) || !strcmp( argv[ i ], "--version" ) )
 		{
 			fprintf( stderr, "MLT %s " VERSION "\n"
-				"Copyright (C) 2002-2009 Ushodaya Enterprises Limited\n"
+				"Copyright (C) 2002-2010 Ushodaya Enterprises Limited\n"
 				"<http://www.mltframework.org/>\n"
 				"This is free software; see the source for copying conditions.  There is NO\n"
 				"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n",
