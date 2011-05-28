46edc85abe5c4eef148e862e51199641e17c7ba4
Dan Dennedy <dan@dennedy.org>
2011-05-28 00:28:22 -0700

    Refactor melt to mlt_profile_from_producer.

--- src/melt/melt.c
+++ src/melt/melt.c
@@ -421,52 +421,6 @@ static void show_usage( char *program_name )
 	basename( program_name ) );
 }
 
-static void guess_profile( mlt_producer melt, mlt_profile profile )
-{
-	mlt_frame fr = NULL;
-	uint8_t *buffer;
-	mlt_image_format fmt = mlt_image_yuv422;
-	mlt_properties p;
-	int w = profile->width;
-	int h = profile->height;
-
-	if ( ! mlt_service_get_frame( MLT_PRODUCER_SERVICE(melt), &fr, 0 ) && fr )
-	{
-		mlt_properties_set_double( MLT_FRAME_PROPERTIES( fr ), "consumer_aspect_ratio", mlt_profile_sar( profile ) );
-		if ( ! mlt_frame_get_image( fr, &buffer, &fmt, &w, &h, 0 ) )
-		{
-			// Some source properties are not exposed until after the first get_image call.
-			mlt_frame_close( fr );
-			mlt_service_get_frame( MLT_PRODUCER_SERVICE(melt), &fr, 0 );
-			p = MLT_FRAME_PROPERTIES( fr );
-//			mlt_properties_dump(p, stderr);
-			if ( mlt_properties_get_int( p, "meta.media.frame_rate_den" ) && mlt_properties_get_int( p, "meta.media.sample_aspect_den" ) )
-			{
-				profile->width = mlt_properties_get_int( p, "meta.media.width" );
-				profile->height = mlt_properties_get_int( p, "meta.media.height" );
-				profile->progressive = mlt_properties_get_int( p, "meta.media.progressive" );
-				profile->frame_rate_num = mlt_properties_get_int( p, "meta.media.frame_rate_num" );
-				profile->frame_rate_den = mlt_properties_get_int( p, "meta.media.frame_rate_den" );
-				// AVCHD is mis-reported as double frame rate.
-				if ( profile->progressive == 0 && (
-				     profile->frame_rate_num / profile->frame_rate_den == 50 ||
-				     profile->frame_rate_num / profile->frame_rate_den == 59 ) )
-					profile->frame_rate_num /= 2;
-				profile->sample_aspect_num = mlt_properties_get_int( p, "meta.media.sample_aspect_num" );
-				profile->sample_aspect_den = mlt_properties_get_int( p, "meta.media.sample_aspect_den" );
-				profile->colorspace = mlt_properties_get_int( p, "meta.media.colorspace" );
-				profile->display_aspect_num = (int) ( (double) profile->sample_aspect_num * profile->width / profile->sample_aspect_den + 0.5 );
-				profile->display_aspect_den = profile->height;
-				free( profile->description );
-				profile->description = strdup( "automatic" );
-				profile->is_explicit = 0;
-			}
-		}
-	}
-	mlt_frame_close( fr );
-	mlt_producer_seek( melt, 0 );
-}
-
 static void query_metadata( mlt_repository repo, mlt_service_type type, const char *typestr, char *id )
 {
 	mlt_properties metadata = mlt_repository_metadata( repo, type, id );
@@ -785,13 +739,13 @@ query_all:
 		// Generate an automatic profile if needed.
 		if ( ! profile->is_explicit )
 		{
-			guess_profile( melt, profile );
+			mlt_profile_from_producer( profile, melt );
 			mlt_producer_close( melt );
 			melt = mlt_factory_producer( profile, "melt", &argv[ 1 ] );
 		}
 		
 		// Reload the consumer with the fully qualified profile.
-		// The producer or guess_profile could have changed the profile.
+		// The producer or auto-profile could have changed the profile.
 		load_consumer( &consumer, profile, argc, argv );
 
 		// If we have no consumer, default to sdl
