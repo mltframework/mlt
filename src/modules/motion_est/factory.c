3a83a3dc308901092e23f56272304186cd1ed7f4
Dan Dennedy <dan@dennedy.org>
2011-05-29 22:48:59 -0700

    Add service metadata to motion_est module (WIP).

--- src/modules/motion_est/factory.c
+++ src/modules/motion_est/factory.c
@@ -15,6 +15,7 @@
  */
 
 #include <string.h>
+#include <limits.h>
 #include <framework/mlt.h>
 
 extern mlt_filter filter_motion_est_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg );
@@ -23,6 +24,13 @@ extern mlt_filter filter_crop_detect_init( mlt_profile profile, mlt_service_type
 extern mlt_filter filter_autotrack_rectangle_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg );
 extern mlt_producer producer_slowmotion_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg );
 
+static mlt_properties metadata( mlt_service_type type, const char *id, void *data )
+{
+	char file[ PATH_MAX ];
+	snprintf( file, PATH_MAX, "%s/motion_est/%s", mlt_environment( "MLT_DATA" ), (char*) data );
+	return mlt_properties_parse_yaml( file );
+}
+
 MLT_REPOSITORY
 {
 	MLT_REGISTER( filter_type, "motion_est", filter_motion_est_init );
@@ -30,4 +38,10 @@ MLT_REPOSITORY
 	MLT_REGISTER( filter_type, "crop_detect", filter_crop_detect_init );
 	MLT_REGISTER( filter_type, "autotrack_rectangle", filter_autotrack_rectangle_init );
 	MLT_REGISTER( producer_type, "slowmotion", producer_slowmotion_init );
+
+	MLT_REGISTER_METADATA( filter_type, "motion_est", metadata, "filter_motion_est.yml" );
+	MLT_REGISTER_METADATA( filter_type, "vismv", metadata, "filter_vismv.yml" );
+	MLT_REGISTER_METADATA( filter_type, "crop_detect", metadata, "filter_crop_detect.yml" );
+	MLT_REGISTER_METADATA( filter_type, "autotrack_rectangle", metadata, "filter_autotrack_rectangle.yml" );
+	MLT_REGISTER_METADATA( producer_type, "slowmotion", metadata, "producer_slowmotion.yml" );
 }
