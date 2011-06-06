7a394eef31c8cc30756169d3e071e827304afb46
Dan Dennedy <dan@dennedy.org>
2011-06-05 19:48:04 -0700

    Drop usage of av_demuxer_open() in avformat v53.
    It is causing problems and need more stability after the raft of
    changes.

--- src/modules/avformat/producer_avformat.c
+++ src/modules/avformat/producer_avformat.c
@@ -748,10 +748,6 @@ static int producer_open( producer_avformat self, mlt_profile profile, const cha
 	char *filename = parse_url( profile, URL, &format, &params );
 
 	// Now attempt to open the file or device with filename
-#if LIBAVFORMAT_VERSION_MAJOR > 52
-	self->video_format = avformat_alloc_context();
-	self->video_format->flags |= AVFMT_FLAG_PRIV_OPT;
-#endif
 	error = av_open_input_file( &self->video_format, filename, format, 0, &params ) < 0;
 	if ( error )
 		// If the URL is a network stream URL, then we probably need to open with full URL
@@ -764,12 +760,6 @@ static int producer_open( producer_avformat self, mlt_profile profile, const cha
 #if LIBAVFORMAT_VERSION_MAJOR > 52
 		if ( self->video_format->iformat && self->video_format->iformat->priv_class && self->video_format->priv_data )
 			apply_properties( self->video_format->priv_data, properties, AV_OPT_FLAG_DECODING_PARAM );
-		error = av_demuxer_open( self->video_format, &params );
-	}
-	else if ( self->video_format )
-	{
-		avformat_free_context( self->video_format );
-		self->video_format = NULL;
 #endif
 	}
 
