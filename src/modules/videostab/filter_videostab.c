d7ffb0cdb3ba73da5ecd0c6aaa3d1ea30d5be14c
Marco Gittler <g.marco@freenet.de>
2011-08-12 07:12:21 +0200

    some cleanup, loading from deshake file works now (if correct) for my sample (45 frames) the filter will do 2 runs, first with 37 , second with 45 frames. so the resut will look like not working.
    correct length deshake file will work.

--- src/modules/videostab/filter_videostab.c
+++ src/modules/videostab/filter_videostab.c
@@ -19,6 +19,7 @@
 
 #include <framework/mlt_filter.h>
 #include <framework/mlt_frame.h>
+#include <framework/mlt_log.h>
 #include <framework/mlt_producer.h>
 
 #include <stdio.h>
@@ -43,8 +44,49 @@ vc *pos_h=NULL, *pos_y=NULL;
 rs_ctx *rs=NULL;
 int initialized=0;
 
+int load_vc_from_file(const char* filename ,vc* pos,int maxlength){
 
-int load_or_generate_pos_y(mlt_frame this,int  *h,int *w,int tfs, int fps2){
+	struct stat stat_buff;
+	stat (filename,&stat_buff);
+	if (S_ISREG(stat_buff.st_mode) ){ 
+		//load file
+		mlt_log_verbose(NULL,"loading file %s\n",filename);
+		FILE *infile;
+		infile=fopen(filename,"r");
+		if (infile){
+			for (i=0;i< maxlength;i++){
+				float x,y;
+				fscanf(infile,"%f%f",&x,&y);
+				pos_h[i].x=x;
+				pos_h[i].y=y;
+				//pos_h[i]=vc_set(x,y);
+			}
+			fclose(infile);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int save_vc_to_file(const char* filename ,vc* pos,int length){
+
+	FILE *outfile;
+	int i=0;
+	outfile=fopen(filename,"w+");
+	if (!outfile){
+		mlt_log_error(NULL,"could not save shakefile %s\n",filename);
+		return -1;
+	}
+	for (i=0;i< length ;i++){
+		fprintf(outfile,"%f %f\n",pos[i].x,pos[i].y);
+		//mlt_log_verbose(NULL,"writing %d/%d %f %f\n",i,length,pos[i].x,pos[i].y);
+	}
+
+	fclose(outfile);
+	return 0;
+}
+
+int load_or_generate_pos_h(mlt_frame this,int  *h,int *w,int tfs, int fps2){
 	int i=0;
 	char shakefile[2048];
 	mlt_producer producer = mlt_frame_get_original_producer(this) ;
@@ -58,53 +100,26 @@ int load_or_generate_pos_y(mlt_frame this,int  *h,int *w,int tfs, int fps2){
 		parent_prod = producer;
 	}
 	sprintf(shakefile,"%s%s", mlt_properties_get (MLT_PRODUCER_PROPERTIES(parent_prod), "resource"),".deshake");
-	struct stat stat_buff;
-	stat (shakefile,&stat_buff);
-	if (S_ISREG(stat_buff.st_mode) && 0 ){ // TODO load stored pos_h , but is does not produce the same result now
-		//load file
-		printf("loading file %s\n",shakefile);
-		FILE *infile;
-		infile=fopen(shakefile,"r");
-		if (infile){
-			for (i=0;i< tfs;i++){
-				float x,y;
-				fscanf(infile,"%f %f",&x,&y);
-				pos_h[i]=vc_set(x,y);
-			}
+	if (!load_vc_from_file(shakefile,pos_h,tfs)){
 
-			fclose(infile);
-			return 0;
-		}
+		mlt_log_verbose(this,"calculating deshake, please wait\n");
+		mlt_image_format format = mlt_image_rgb24;
+		vc* pos_i = (vc *)malloc(tfs * sizeof(vc));
+		es_ctx *es1=es_init(*w,*h);
+		for (i=0;i< tfs;i++){
+			mlt_producer_seek(producer,i);
+			mlt_frame frame;
+			mlt_service_get_frame( mlt_producer_service(producer), &frame, 0 );
+			uint8_t *buffer= NULL;
+			int error = mlt_frame_get_image( frame, &buffer, &format, w, h, 1 );
 
+			pos_i[i] = vc_add( i > 0 ? pos_i[i - 1] : vc_set(0.0, 0.0), es_estimate(es1, buffer));
+		} 
+		hipass(pos_i, pos_h, tfs, fps2);
+		free (pos_i);
+		free(es1);
+		save_vc_to_file(shakefile,pos_h,tfs);
 	}
-	
-	mlt_image_format format = mlt_image_rgb24;
-	vc* pos_i = (vc *)malloc(tfs * sizeof(vc));
-	es_ctx *es1=es_init(*w,*h);
-	for (i=0;i< mlt_properties_get_int( prod_props, "length" );i++){
-		mlt_producer_seek(producer,i);
-		mlt_frame frame;
-		mlt_service_get_frame( mlt_producer_service(producer), &frame, 0 );
-		uint8_t *buffer= NULL;
-		int error = mlt_frame_get_image( frame, &buffer, &format, w, h, 1 );
-
-		pos_i[i] = vc_add( i > 0 ? pos_i[i - 1] : vc_set(0.0, 0.0), es_estimate(es1, buffer));
-	} 
-	hipass(pos_i, pos_h, tfs, fps2);
-	free (pos_i);
-	free(es1);
-
-	FILE *outfile;
-	outfile=fopen(shakefile,"a");
-	if (!outfile){
-		printf("could not save shakefile %s\n",shakefile);
-		return -1;
-	}
-	for (i=0;i< tfs ;i++){
-		fprintf(outfile,"%f %f\n",pos_h[i].x,pos_h[i].y);
-	}
-
-	fclose(outfile);
 	return 0;
 }
 
@@ -134,6 +149,7 @@ static int filter_get_image( mlt_frame this, uint8_t **image, mlt_image_format *
 		in=mlt_properties_get_int( pro, "in" );
 		out=mlt_properties_get_int( pro, "out" );
 		length=mlt_properties_get_int( pro, "length" );
+		mlt_log_verbose(filter,"deshaing for in=%d out=%d length=%d\n",in,out,length);
 		if (!initialized){
 			tfs = length;// mv_in.header->TotalFrames;
 			fps =  mlt_profile_fps( profile ); //1000000 / 20000;//mv_in.header->MicroSecPerFrame;
@@ -144,9 +160,7 @@ static int filter_get_image( mlt_frame this, uint8_t **image, mlt_image_format *
 			rs = rs_init(w, h);
 			initialized=1;
 
-			if ( mlt_properties_get_int (MLT_FILTER_PROPERTIES( filter ), "calculated" )!=1 )
-				load_or_generate_pos_y(this,&h,&w,tfs,fps/2);
-			mlt_properties_set( MLT_FILTER_PROPERTIES( filter ), "calculated", "1" );
+			load_or_generate_pos_h(this,&h,&w,tfs,fps/2);
 
 		}
 		if (initialized>=1){
@@ -170,7 +184,6 @@ static mlt_frame filter_process( mlt_filter this, mlt_frame frame )
 
 void filter_close( mlt_filter this )
 {
-	//if (pos_i) {free(pos_i); pos_i=NULL;}
 	if (pos_h) {free(pos_h); pos_h=NULL;}
 	if (pos_y) {free(pos_y); pos_y=NULL;}
 	if (rs) {rs_free(rs); rs=NULL;}
