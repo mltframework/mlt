90596e488de71920c21b7fe7bc5a82822ff328e2
Dan Dennedy <dan@dennedy.org>
2011-09-09 17:37:27 -0700

    Fix composite_copy_region on locale using comma for decimal.

--- src/modules/core/transition_composite.c
+++ src/modules/core/transition_composite.c
@@ -919,7 +919,7 @@ static mlt_geometry composite_calculate( mlt_transition this, struct geometry_s 
 	sprintf( key, "%s.in", name );
 	if ( mlt_properties_get( a_props, key ) )
 	{
-		sscanf( mlt_properties_get( a_props, key ), "%f,%f,%f,%f,%f,%d,%d", &result->item.x, &result->item.y, &result->item.w, &result->item.h, &result->item.mix, &result->nw, &result->nh );
+		sscanf( mlt_properties_get( a_props, key ), "%f %f %f %f %f %d %d", &result->item.x, &result->item.y, &result->item.w, &result->item.h, &result->item.mix, &result->nw, &result->nh );
 	}
 	else
 	{
@@ -1022,9 +1022,9 @@ mlt_frame composite_copy_region( mlt_transition this, mlt_frame a_frame, mlt_pos
 	}
 
 	// Store the key
-	sprintf( key, "%s.in=%d,%d,%d,%d,%f,%d,%d", name, x, y, w, h, result.item.mix, width, height );
+	sprintf( key, "%s.in=%d %d %d %d %f %d %d", name, x, y, w, h, result.item.mix, width, height );
 	mlt_properties_parse( a_props, key );
-	sprintf( key, "%s.out=%d,%d,%d,%d,%f,%d,%d", name, x, y, w, h, result.item.mix, width, height );
+	sprintf( key, "%s.out=%d %d %d %d %f %d %d", name, x, y, w, h, result.item.mix, width, height );
 	mlt_properties_parse( a_props, key );
 
 	ds = w * 2;
