4b1a201b176e91ca60d58df4dc78e16bae0110b8
Dan Dennedy <dan@dennedy.org>
2012-07-22 18:59:26 -0700

    fix uninitialized memory (coverity-709434)

--- src/modules/videostab/stab/klt/klt.c
+++ src/modules/videostab/stab/klt/klt.c
@@ -25,7 +25,7 @@ KLT_TrackingContext KLTCreateTrackingContext()
   KLT_TrackingContext tc = NULL;
 
   /* Allocate memory */
-  tc = (KLT_TrackingContext)  malloc(sizeof(KLT_TrackingContextRec));
+  tc = (KLT_TrackingContext) calloc(1, sizeof(KLT_TrackingContextRec));
 
   /* Set values to default values */
   tc->mindist = 10;
