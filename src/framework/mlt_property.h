6a5e8781dade4bb685207c91ddbe91e3391487aa
Dan Dennedy <dan@dennedy.org>
2012-09-08 19:32:22 -0700

    xlocale was backported to FreeBSD 9-STABLE
    Patch by Alberto Villa

--- src/framework/mlt_property.h
+++ src/framework/mlt_property.h
@@ -30,7 +30,7 @@
 #include <sys/param.h>
 #endif
 
-#if defined(__GLIBC__) || defined(__DARWIN__) || (__FreeBSD_version >= 1000002)
+#if defined(__GLIBC__) || defined(__DARWIN__) || (__FreeBSD_version >= 900506)
 #include <xlocale.h>
 #else
 typedef void* locale_t;
