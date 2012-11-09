412c6ed756bcc9ee02969eadc6636c3c41d63cfb
Jean-Baptiste Mardelle <jb@kdenlive.org>
2012-11-09 22:10:27 +0100

    Fix kdenlivetitle producer position

--- src/modules/qimage/producer_kdenlivetitle.c
+++ src/modules/qimage/producer_kdenlivetitle.c
@@ -75,13 +75,12 @@ static int producer_get_image( mlt_frame frame, uint8_t **buffer, mlt_image_form
 
 	/* Allocate the image */
 	*format = mlt_image_rgb24a;
-	mlt_position time = mlt_producer_position( &this->parent ) + mlt_producer_get_in( &this->parent );
 	if ( mlt_properties_get_int( producer_props, "force_reload" ) ) {
 		if (mlt_properties_get_int( producer_props, "force_reload" ) > 1) read_xml(producer_props);
 		mlt_properties_set_int( producer_props, "force_reload", 0 );
-		drawKdenliveTitle( this, frame, *width, *height, time, 1);
+		drawKdenliveTitle( this, frame, *width, *height, mlt_frame_original_position(frame), 1);
 	}
-	else drawKdenliveTitle( this, frame, *width, *height, time, 0);
+	else drawKdenliveTitle( this, frame, *width, *height, mlt_frame_original_position(frame), 0);
 
 	// Get width and height (may have changed during the refresh)
 	*width = mlt_properties_get_int( properties, "width" );
