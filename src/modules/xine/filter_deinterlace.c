2aa5821e1248e9a6e40128f681fc34d37d0f5e6a
Dan Dennedy <dan@dennedy.org>
2013-03-23 23:52:39 -0700

    Do not deinterlace framw with test_image.

--- src/modules/xine/filter_deinterlace.c
+++ src/modules/xine/filter_deinterlace.c
@@ -195,7 +195,7 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 	// At this point - before image was requested - (progressive == 0) cannot be trusted because
 	// some producers (avformat) do not yet know.
 
-	if ( deinterlace )
+	if ( deinterlace && !mlt_properties_get_int( properties, "test_image" ) )
 	{
 		// Determine deinterlace method
 		char *method_str = mlt_properties_get( MLT_FILTER_PROPERTIES( filter ), "method" );
