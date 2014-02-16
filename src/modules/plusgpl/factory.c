6453d1c04f43c722559ceff2293a24b281ff3262
Dan Dennedy <dan@dennedy.org>
2014-02-16 11:10:22 -0800

    Move burningtv into plusgpl module.

--- src/modules/plusgpl/factory.c
+++ src/modules/plusgpl/factory.c
@@ -1,6 +1,6 @@
 /*
  * factory.c -- the factory method interfaces
- * Copyright (C) 2008 Dan Dennedy <dan@dennedy.org>
+ * Copyright (C) 2008-2014 Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,10 +19,24 @@
 
 #include <string.h>
 #include <framework/mlt.h>
+#include <limits.h>
 
+extern mlt_filter filter_burn_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg );
 extern mlt_filter filter_telecide_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg );
+
+static mlt_properties metadata( mlt_service_type type, const char *id, void *data )
+{
+	char file[ PATH_MAX ];
+	snprintf( file, PATH_MAX, "%s/plusgpl/%s", mlt_environment( "MLT_DATA" ), (char*) data );
+	return mlt_properties_parse_yaml( file );
+}
 
 MLT_REPOSITORY
 {
+	MLT_REGISTER( filter_type, "BurningTV", filter_burn_init );
+	MLT_REGISTER( filter_type, "burningtv", filter_burn_init );
 	MLT_REGISTER( filter_type, "telecide", filter_telecide_init );
+
+	MLT_REGISTER_METADATA( filter_type, "BurningTV", metadata, "filter_burningtv.yml" );
+	MLT_REGISTER_METADATA( filter_type, "burningtv", metadata, "filter_burningtv.yml" );
 }
