393074236e81d220d44bb3d80c1b8dd45954228d
Maksym Veremeyenko <verem@m1.tv>
2015-02-03 12:36:54 +0200

    implement 8 variants of compositing function

--- src/modules/core/composite_line_yuv_sse2_simple.c
+++ src/modules/core/composite_line_yuv_sse2_simple.c
@@ -108,13 +108,13 @@ const static unsigned char const2[] =
         "add            $0x10, %[dest]          \n\t"   \
         "dec            %[width]                \n\t"
 
-void composite_line_yuv_sse2_simple(uint8_t *dest, uint8_t *src, int width, uint8_t *src_a, uint8_t *dest_a, int weight)
+static void blend_case7(uint8_t *dest, uint8_t *src, int width, uint8_t *src_a, uint8_t *dest_a, int weight)
 {
     __asm__ volatile
     (
         LOAD_CONSTS
         LOAD_WEIGHT
-        "loop_start:                            \n\t"
+        "loop_start7:                           \n\t"
         LOAD_SRC_A
         SRC_A_PREMUL
         DST_A_CALC
@@ -122,9 +122,137 @@ void composite_line_yuv_sse2_simple(uint8_t *dest, uint8_t *src, int width, uint
         "add            $0x08, %[src_a]         \n\t"
         "add            $0x08, %[dest_a]        \n\t"
         PIX_POINTER_INC
-        "jnz            loop_start              \n\t"
+        "jnz            loop_start7             \n\t"
         :
-        : [weight] "r" (weight >> 8), [src_a] "r" (src_a), [src] "r" (src), [dest] "r" (dest), [const1] "r" (const1) , [dest_a] "r" (dest_a), [width] "r" (width / 8), [const2] "r" (const2)
-        //: "xmm0","xmm1","xmm2","xmm3","xmm4","xmm5","xmm6","xmm7","xmm8","xmm9", "memory"
+        : [weight] "r" (weight), [src_a] "r" (src_a), [src] "r" (src), [dest] "r" (dest), [const1] "r" (const1) , [dest_a] "r" (dest_a), [width] "r" (width / 8), [const2] "r" (const2)
     );
+};
+
+//  | 3   | dest_a == NULL | src_a != NULL | weight != 256 | blend: premultiply src alpha
+static void blend_case3(uint8_t *dest, uint8_t *src, int width, uint8_t *src_a, int weight)
+{
+    __asm__ volatile
+    (
+        LOAD_CONSTS
+        LOAD_WEIGHT
+        "loop_start3:                           \n\t"
+        LOAD_SRC_A
+        SRC_A_PREMUL
+        DST_PIX_CALC
+        "add            $0x08, %[src_a]         \n\t"
+        PIX_POINTER_INC
+        "jnz            loop_start3             \n\t"
+        :
+        : [weight] "r" (weight), [src_a] "r" (src_a), [src] "r" (src), [dest] "r" (dest), [const1] "r" (const1), [width] "r" (width / 8), [const2] "r" (const2)
+    );
+};
+
+//  | 2   | dest_a == NULL | src_a != NULL | weight == 255 | blend: only src alpha
+static void blend_case2(uint8_t *dest, uint8_t *src, int width, uint8_t *src_a)
+{
+    __asm__ volatile
+    (
+        LOAD_CONSTS
+        "loop_start2:                           \n\t"
+        LOAD_SRC_A
+        DST_PIX_CALC
+        "add            $0x08, %[src_a]         \n\t"
+        PIX_POINTER_INC
+        "jnz            loop_start2             \n\t"
+        :
+        : [src_a] "r" (src_a), [src] "r" (src), [dest] "r" (dest), [const1] "r" (const1) , [width] "r" (width / 8), [const2] "r" (const2)
+    );
+};
+
+
+//  | 1   | dest_a == NULL | src_a == NULL | weight != 256 | blend: with given alpha
+static void blend_case1(uint8_t *dest, uint8_t *src, int width, int weight)
+{
+    __asm__ volatile
+    (
+        LOAD_CONSTS
+        LOAD_WEIGHT
+        "loop_start1:                           \n\t"
+        "movdqa         %%xmm1, %%xmm2          \n\t"   /* src alpha cames from weight */
+        DST_PIX_CALC
+        PIX_POINTER_INC
+        "jnz            loop_start1             \n\t"
+        :
+        : [weight] "r" (weight), [src] "r" (src), [dest] "r" (dest), [const1] "r" (const1), [width] "r" (width / 8), [const2] "r" (const2)
+    );
+};
+
+//  | 5   | dest_a != NULL | src_a == NULL | weight != 256 | blend: with given alpha
+static void blend_case5(uint8_t *dest, uint8_t *src, int width, uint8_t *dest_a, int weight)
+{
+    __asm__ volatile
+    (
+        LOAD_CONSTS
+        LOAD_WEIGHT
+        "loop_start5:                           \n\t"
+        "movdqa         %%xmm1, %%xmm2          \n\t"   /* source alpha comes from weight */
+        DST_A_CALC
+        DST_PIX_CALC
+        "add            $0x08, %[dest_a]        \n\t"
+        PIX_POINTER_INC
+        "jnz            loop_start5             \n\t"
+
+        :
+        : [weight] "r" (weight), [src] "r" (src), [dest] "r" (dest), [const1] "r" (const1) , [dest_a] "r" (dest_a), [width] "r" (width / 8), [const2] "r" (const2)
+    );
+};
+
+//  | 6   | dest_a != NULL | src_a != NULL | weight == 256 | blend: full blend without src alpha premutiply
+static void blend_case6(uint8_t *dest, uint8_t *src, int width, uint8_t *src_a, uint8_t *dest_a)
+{
+    __asm__ volatile
+    (
+        LOAD_CONSTS
+        "loop_start6:                           \n\t"
+        LOAD_SRC_A
+        DST_A_CALC
+        DST_PIX_CALC
+        "add            $0x08, %[src_a]         \n\t"
+        "add            $0x08, %[dest_a]        \n\t"
+        PIX_POINTER_INC
+        "jnz            loop_start6             \n\t"
+        :
+        : [src_a] "r" (src_a), [src] "r" (src), [dest] "r" (dest), [const1] "r" (const1) , [dest_a] "r" (dest_a), [width] "r" (width / 8), [const2] "r" (const2)
+    );
+};
+
+
+void composite_line_yuv_sse2_simple(uint8_t *dest, uint8_t *src, int width, uint8_t *src_a, uint8_t *dest_a, int weight)
+{
+    weight >>= 8;
+
+    /*
+        | 0   | dest_a == NULL | src_a == NULL | weight == 256 | blit
+        | 1   | dest_a == NULL | src_a == NULL | weight != 256 | blend: with given alpha
+        | 2   | dest_a == NULL | src_a != NULL | weight == 256 | blend: only src alpha
+        | 3   | dest_a == NULL | src_a != NULL | weight != 256 | blend: premultiply src alpha
+        | 4   | dest_a != NULL | src_a == NULL | weight == 256 | blit: blit and set dst alpha to FF
+        | 5   | dest_a != NULL | src_a == NULL | weight != 256 | blend: with given alpha
+        | 6   | dest_a != NULL | src_a != NULL | weight == 256 | blend: full blend without src alpha premutiply
+        | 7   | dest_a != NULL | src_a != NULL | weight != 256 | blend: full (origin version)
+    */
+
+    int cond = ((dest_a != NULL)?4:0) + ((src_a != NULL)?2:0) + ((weight != 256)?1:0);
+
+    switch(cond)
+    {
+        case 0:
+            memcpy(dest, src, 2 * width);
+            break;
+        case 1: blend_case1(dest, src, width, weight); break;
+        case 2: blend_case2(dest, src, width, src_a); break;
+        case 3: blend_case3(dest, src, width, src_a, weight); break;
+        case 4:
+            memcpy(dest, src, 2 * width);
+            memset(dest_a, 0xFF, width);
+            break;
+        case 5: blend_case5(dest, src, width, dest_a, weight); break;
+        case 6: blend_case6(dest, src, width, src_a, dest_a); break;
+        case 7: blend_case7(dest, src, width, src_a, dest_a, weight); break;
+    };
 };
