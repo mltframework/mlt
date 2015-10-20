2563d65a04db999e4003108634a045fcc0e72ed8
Dan Dennedy <dan@dennedy.org>
2015-10-19 20:46:18 -0700

    Add -getc option to melt.
    This makes it easier to control melt from a parent process by writing
    commands to stdin. It really only works for non-auto-terminating jobs
    such as continous player, streamer, or capture utility that ends when
    told to.

--- src/melt/melt.c
+++ src/melt/melt.c
@@ -382,6 +382,7 @@ static void transport( mlt_producer producer, mlt_consumer consumer )
 	mlt_properties properties = MLT_PRODUCER_PROPERTIES( producer );
 	int silent = mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( consumer ), "silent" );
 	int progress = mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( consumer ), "progress" );
+	int is_getc = mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( consumer ), "melt_getc" );
 	struct timespec tm = { 0, 40000000 };
 	int total_length = mlt_producer_get_length( producer );
 	int last_position = 0;
@@ -390,7 +391,8 @@ static void transport( mlt_producer producer, mlt_consumer consumer )
 	{
 		if ( !silent && !progress )
 		{
-			term_init( );
+			if ( !is_getc )
+				term_init( );
 
 			fprintf( stderr, "+-----+ +-----+ +-----+ +-----+ +-----+ +-----+ +-----+ +-----+ +-----+\n" );
 			fprintf( stderr, "|1=-10| |2= -5| |3= -2| |4= -1| |5=  0| |6=  1| |7=  2| |8=  5| |9= 10|\n" );
@@ -406,7 +408,12 @@ static void transport( mlt_producer producer, mlt_consumer consumer )
 
 		while( mlt_properties_get_int( properties, "done" ) == 0 && !mlt_consumer_is_stopped( consumer ) )
 		{
-			int value = ( silent || progress )? -1 : term_read( );
+			int value = ( silent || progress || is_getc )? -1 : term_read( );
+			if ( is_getc )
+			{
+				value = getc( stdin );
+				value = ( value == EOF )? 'q' : value;
+			}
 
 			if ( value != -1 )
 			{
@@ -461,6 +468,7 @@ static void show_usage( char *program_name )
 "  -consumer id[:arg] [name=value]*         Set the consumer (sink)\n"
 "  -debug                                   Set the logging level to debug\n"
 "  -filter filter[:arg] [name=value]*       Add a filter to the current track\n"
+"  -getc                                    Get keyboard input using getc\n"
 "  -group [name=value]*                     Apply properties repeatedly\n"
 "  -help                                    Show this message\n"
 "  -jack                                    Enable JACK transport synchronization\n"
@@ -697,6 +705,7 @@ int main( int argc, char **argv )
 	int is_progress = 0;
 	int is_silent = 0;
 	int is_abort = 0;
+	int is_getc = 0;
 	int error = 0;
 	mlt_profile backup_profile;
 
@@ -708,10 +717,6 @@ int main( int argc, char **argv )
 	// Construct the factory
 	mlt_repository repo = mlt_factory_init( NULL );
 
-#if defined(WIN32) && !defined(MELT_NOSDL)
-	is_silent = 1;
-#endif
-	
 	for ( i = 1; i < argc; i ++ )
 	{
 		// Check for serialisation switch
@@ -806,7 +811,7 @@ query_all:
 		else if ( !strcmp( argv[ i ], "-version" ) || !strcmp( argv[ i ], "--version" ) )
 		{
 			fprintf( stdout, "%s " VERSION "\n"
-				"Copyright (C) 2002-2014 Meltytech, LLC\n"
+				"Copyright (C) 2002-2015 Meltytech, LLC\n"
 				"<http://www.mltframework.org/>\n"
 				"This is free software; see the source for copying conditions.  There is NO\n"
 				"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n",
@@ -820,6 +825,10 @@ query_all:
 		else if ( !strcmp( argv[ i ], "-abort" ) )
 		{
 			is_abort = 1;
+		}
+		else if ( !strcmp( argv[ i ], "-getc" ) )
+		{
+			is_getc = 1;
 		}
 	}
 	if ( !is_silent && !isatty( STDIN_FILENO ) && !is_progress )
@@ -893,6 +902,8 @@ query_all:
 			mlt_properties_set_int(  MLT_CONSUMER_PROPERTIES( consumer ), "progress", is_progress );
 		if ( is_silent )
 			mlt_properties_set_int(  MLT_CONSUMER_PROPERTIES( consumer ), "silent", is_silent );
+		if ( is_getc )
+			mlt_properties_set_int(  MLT_CONSUMER_PROPERTIES( consumer ), "melt_getc", is_getc );
 	}
 
 	if ( argc > 1 && melt != NULL && mlt_producer_get_length( melt ) > 0 )
