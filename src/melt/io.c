1065fda0375e1cd296bce35f7e72368f9abc2ead
Dan Dennedy <dan@dennedy.org>
2015-12-12 13:36:48 -0800

    Cleanup whitespace chars in melt/io.c.

--- src/melt/io.c
+++ src/melt/io.c
@@ -169,24 +169,24 @@ void term_init( )
 int term_read( )
 {
 #ifndef WIN32
-    int n = 1;
-    unsigned char ch;
-    struct timeval tv;
-    fd_set rfds;
+	int n = 1;
+	unsigned char ch;
+	struct timeval tv;
+	fd_set rfds;
 
-    FD_ZERO( &rfds );
-    FD_SET( 0, &rfds );
-    tv.tv_sec = 0;
-    tv.tv_usec = 40000;
-    n = select( 1, &rfds, NULL, NULL, &tv );
-    if (n > 0) 
+	FD_ZERO( &rfds );
+	FD_SET( 0, &rfds );
+	tv.tv_sec = 0;
+	tv.tv_usec = 40000;
+	n = select( 1, &rfds, NULL, NULL, &tv );
+	if (n > 0)
 	{
-        n = read( 0, &ch, 1 );
+		n = read( 0, &ch, 1 );
 		tcflush( 0, TCIFLUSH );
-        if (n == 1)
-            return ch;
-        return n;
-    }
+		if (n == 1)
+			return ch;
+		return n;
+	}
 #else
 	HANDLE h = GetStdHandle( STD_INPUT_HANDLE );
 	if ( h && WaitForSingleObject( h, 0 ) == WAIT_OBJECT_0 )
