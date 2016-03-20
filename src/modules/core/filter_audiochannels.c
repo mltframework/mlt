1f8ee7769e33b0aceb161ddaafa9eab7112ad7b0
Brian Matherly <code@brianmatherly.com>
2016-03-20 14:49:10 -0500

    Add 5.1 to stereo downmix to audiochannels filter.

--- src/modules/core/filter_audiochannels.c
+++ src/modules/core/filter_audiochannels.c
@@ -98,6 +98,105 @@ static int filter_get_audio( mlt_frame frame, void **buffer, mlt_audio_format *f
 		mlt_frame_set_audio( frame, new_buffer, *format, size, mlt_pool_release );
 		*buffer = new_buffer;
 	}
+	else if ( channels_avail == 6 && *channels == 2 )
+	{
+		// Downmix 5.1 audio to stereo.
+		// Mix levels taken from ATSC A/52 assuming maximum center and surround
+		// mix levels.
+		#define MIX(front, center, surr) (front + (0.707 * center) + (0.5 * surr))
+
+		// Convert to a supported format if necessary
+		mlt_audio_format new_format = *format;
+		switch( *format )
+		{
+		default:
+			// Unknown. Try to convert to float anyway.
+			mlt_log_error( NULL, "[audiochannels] Unknown format %d\n", *format );
+		case mlt_audio_float:
+		case mlt_audio_f32le:
+			new_format = mlt_audio_float;
+			break;
+		case mlt_audio_s32le:
+		case mlt_audio_s32:
+			new_format = mlt_audio_s32;
+			break;
+		case mlt_audio_s16:
+		case mlt_audio_u8:
+			new_format = mlt_audio_s16;
+			break;
+		case mlt_audio_none:
+			new_format = mlt_audio_none;
+			break;
+		}
+		if ( *format != new_format && frame->convert_audio )
+			frame->convert_audio( frame, buffer, format, new_format );
+
+		// Perform the downmix. Operate on the buffer in place to avoid realloc.
+		if ( *format == mlt_audio_s16 )
+		{
+			int16_t* in = *buffer;
+			int16_t* out = *buffer;
+			int i;
+			for ( i = 0; i < *samples; i++ )
+			{
+				float fl = in[0];
+				float fr = in[1];
+				float c = in[2];
+				// in[3] is LFE
+				float sl = in[4];
+				float sr = in[5];
+				*out++ = CLAMP( MIX(fl, c, sl), INT16_MIN, INT16_MAX ); // Left
+				*out++ = CLAMP( MIX(fr, c, sr), INT16_MIN, INT16_MAX ); // Right
+				in +=6;
+			}
+		}
+		else if ( *format == mlt_audio_s32 )
+		{
+			int32_t* flin = *buffer;
+			int32_t* frin = *buffer + (*samples * sizeof(float));
+			int32_t* cin  = *buffer + (2 * *samples * sizeof(float));
+			int32_t* slin = *buffer + (4 * *samples * sizeof(float));
+			int32_t* srin = *buffer + (5 * *samples * sizeof(float));
+			int32_t* lout = *buffer;
+			int32_t* rout = *buffer + (*samples * sizeof(float));
+			int i;
+			for ( i = 0; i < *samples; i++ )
+			{
+				double fl = *flin++;
+				double fr = *frin++;
+				double c = *cin++;
+				double sl = *slin++;
+				double sr = *srin++;
+				*lout++ = CLAMP( MIX(fl, c, sl), INT32_MIN, INT32_MAX );
+				*rout++ = CLAMP( MIX(fr, c, sr), INT32_MIN, INT32_MAX );
+			}
+		}
+		else if ( *format == mlt_audio_float )
+		{
+			float* flin = *buffer;
+			float* frin = *buffer + (*samples * sizeof(float));
+			float* cin  = *buffer + (2 * *samples * sizeof(float));
+			float* slin = *buffer + (4 * *samples * sizeof(float));
+			float* srin = *buffer + (5 * *samples * sizeof(float));
+			float* lout = *buffer;
+			float* rout = *buffer + (*samples * sizeof(float));
+			int i;
+			for ( i = 0; i < *samples; i++ )
+			{
+				float fl = *flin++;
+				float fr = *frin++;
+				float c = *cin++;
+				float sl = *slin++;
+				float sr = *srin++;
+				*lout++ = MIX(fl, c, sl);
+				*rout++ = MIX(fr, c, sr);
+			}
+		}
+		else
+		{
+			mlt_log_error( NULL, "[audiochannels] Unable to mix format %d\n", *format );
+		}
+	}
 	else if ( channels_avail > *channels )
 	{
 		int size = mlt_audio_format_size( *format, *samples, *channels );
