mltframework/mlt

commit 06c06629d9651951553fd59aeae63f7b37dce7a0

Fix relative path handling for "plain" webvfx resources.

src/modules/xml/consumer_xml.c
@@ -208,12 +208,29 @@ static void serialise_properties( serialise_context context, mlt_properties prop
 				value = mlt_properties_get_time( properties, name, context->time_format );
 			else
 				value = mlt_properties_get_value( properties, i );
+			char *value_orig = value;
 			if ( value )
 			{
 				int rootlen = strlen( context->root );
+
+				// Strip off WebVfx "plain:" prefix.
+				if ( !strncmp( value_orig, "plain:", 6 ) )
+					value += 6;
+
 				// convert absolute path to relative
 				if ( rootlen && !strncmp( value, context->root, rootlen ) && value[ rootlen ] == '/' )
-					p = xmlNewTextChild( node, NULL, _x("property"), _x(value + rootlen + 1 ) );
+				{
+					if ( !strncmp( value_orig, "plain:", 6 ) )
+					{
+						char *s = calloc( 1, strlen( value_orig ) - rootlen + 1 );
+						strcat( s, "plain:" );
+						strcat( s, value + rootlen + 1 );
+						p = xmlNewTextChild( node, NULL, _x("property"), _x(s) );
+						free( s );
+					} else {
+						p = xmlNewTextChild( node, NULL, _x("property"), _x(value + rootlen + 1) );
+					}
+				}
 				else
 					p = xmlNewTextChild( node, NULL, _x("property"), _x(value) );
 				xmlNewProp( p, _x("name"), _x(name) );

src/modules/xml/producer_xml.c
@@ -191,23 +191,32 @@ static void track_service( mlt_properties properties, void *service, mlt_destruc
 // Prepend the property value with the document root
 static inline void qualify_property( deserialise_context context, mlt_properties properties, const char *name )
 {
+	char *resource_orig = mlt_properties_get( properties, name );
 	char *resource = mlt_properties_get( properties, name );
 	if ( resource != NULL && resource[0] )
 	{
-		// Qualify file name properties	
 		char *root = mlt_properties_get( context->producer_map, "root" );
+		int n = strlen( root ) + strlen( resource ) + 2;
+
+		// Strip off WebVfx "plain:" prefix.
+		if ( !strncmp( resource_orig, "plain:", 6 ) )
+			resource += 6;
+
+		// Qualify file name properties	
 		if ( root != NULL && strcmp( root, "" ) )
 		{
-			char *full_resource = malloc( strlen( root ) + strlen( resource ) + 2 );
+			char *full_resource = calloc( 1, n );
 			if ( resource[ 0 ] != '/' && strchr( resource, ':' ) == NULL )
 			{
-				strcpy( full_resource, root );
+				if ( !strncmp( resource_orig, "plain:", 6 ) )
+					strcat( full_resource, "plain:" );
+				strcat( full_resource, root );
 				strcat( full_resource, "/" );
 				strcat( full_resource, resource );
 			}
 			else
 			{
-				strcpy( full_resource, resource );
+				strcpy( full_resource, resource_orig );
 			}
 			mlt_properties_set( properties, name, full_resource );
 			free( full_resource );

commit 5510592acae7fd12edc62ee1d1102498efeb635e

Fix saving plain webvfx resource with absolute path.
Regression in previous commit 06c0662.
Fixes Shotcut bug https://github.com/mltframework/shotcut/issues/225

src/modules/xml/consumer_xml.c
@@ -228,11 +228,11 @@ static void serialise_properties( serialise_context context, mlt_properties prop
 						p = xmlNewTextChild( node, NULL, _x("property"), _x(s) );
 						free( s );
 					} else {
-						p = xmlNewTextChild( node, NULL, _x("property"), _x(value + rootlen + 1) );
+						p = xmlNewTextChild( node, NULL, _x("property"), _x(value_orig + rootlen + 1) );
 					}
 				}
 				else
-					p = xmlNewTextChild( node, NULL, _x("property"), _x(value) );
+					p = xmlNewTextChild( node, NULL, _x("property"), _x(value_orig) );
 				xmlNewProp( p, _x("name"), _x(name) );
 			}
 		}