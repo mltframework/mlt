mltframework/mlt

commit 3d099f268216991172f7dc04e976a3ec124e154b

Factorize default format getting

src/framework/mlt_consumer.c
@@ -486,17 +486,8 @@ static void set_image_format( mlt_consumer self )
 	const char* format = mlt_properties_get( properties, "mlt_image_format" );
 	if ( format )
 	{
-		if ( !strcmp( format, "rgb24" ) )
-			priv->image_format = mlt_image_rgb24;
-		else if ( !strcmp( format, "rgb24a" ) )
-			priv->image_format = mlt_image_rgb24a;
-		else if ( !strcmp( format, "yuv420p" ) )
-			priv->image_format = mlt_image_yuv420p;
-		else if ( !strcmp( format, "none" ) )
-			priv->image_format = mlt_image_none;
-		else if ( !strcmp( format, "glsl" ) )
-			priv->image_format = mlt_image_glsl_texture;
-		else
+		priv->image_format = mlt_image_format_id( format );
+		if ( mlt_image_invalid == priv->image_format )
 			priv->image_format = mlt_image_yuv422;
 	}
 }

commit 9faa059604986c969c5877cb840e3fb2cdfa7a04

Fix setting consumer mlt_image_format=glsl.
This was a regression in 3d099f26. It broke GPU processing preview in
Shotcut; could affect other apps as well.

src/framework/mlt_consumer.c
@@ -489,6 +489,11 @@ static void set_image_format( mlt_consumer self )
 		priv->image_format = mlt_image_format_id( format );
 		if ( mlt_image_invalid == priv->image_format )
 			priv->image_format = mlt_image_yuv422;
+		// mlt_image_glsl is for internal use only.
+		// Remapping it glsl_texture prevents breaking existing apps
+		// using the legacy "glsl" name.
+		else if ( mlt_image_glsl == priv->image_format )
+			priv->image_format = mlt_image_glsl_texture;
 	}
 }
 