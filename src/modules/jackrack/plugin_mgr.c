6bb17e1305106a7dd04278f0050f74feea54bdcd
Dan Dennedy <dan@dennedy.org>
2017-03-30 19:55:36 -0700

    Use RTLD_LAZY for listing LADSPA plugins.
    This is a little faster to init MLT and resolves Shotcut bug
    https://github.com/mltframework/shotcut/issues/404

--- src/modules/jackrack/plugin_mgr.c
+++ src/modules/jackrack/plugin_mgr.c
@@ -79,7 +79,7 @@ plugin_mgr_get_object_file_plugins (plugin_mgr_t * plugin_mgr, const char * file
   int err;
   
   /* open the object file */
-  dl_handle = dlopen (filename, RTLD_NOW);
+  dl_handle = dlopen (filename, RTLD_LAZY);
   if (!dl_handle)
     {
       mlt_log_info( NULL, "%s: error opening shared object file '%s': %s\n",
