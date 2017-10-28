5e0383304cc1d9b4ba873966429936287bfda0e7
Dan Dennedy <dan@dennedy.org>
2017-10-28 16:03:44 -0700

    Minor cleanup.

--- src/modules/avformat/consumer_avformat.c
+++ src/modules/avformat/consumer_avformat.c
@@ -1104,20 +1104,19 @@ typedef struct encode_ctx_desc
 static int encode_audio(encode_ctx_t* ctx)
 {
 	char key[27];
-    int i, j = 0, samples = ctx->audio_input_frame_size;
+	int i, j = 0, samples = ctx->audio_input_frame_size;
 
 	int frame_length = ctx->audio_input_frame_size * ctx->channels * ctx->sample_bytes;
 
 	// Get samples count to fetch from fifo
 	if ( sample_fifo_used( ctx->fifo ) < frame_length )
-    {
-		samples = sample_fifo_used( ctx->fifo ) / ( ctx->channels * ctx->sample_bytes );
-    }
-    else if ( ctx->audio_input_frame_size == 1 )
 	{
-        // PCM consumes as much as possible.
-		int frames = FFMIN( sample_fifo_used( ctx->fifo ), AUDIO_ENCODE_BUFFER_SIZE ) / frame_length;
-		samples = frames * ctx->audio_input_frame_size;
+		samples = sample_fifo_used( ctx->fifo ) / ( ctx->channels * ctx->sample_bytes );
+	}
+	else if ( ctx->audio_input_frame_size == 1 )
+	{
+		// PCM consumes as much as possible.
+		samples = FFMIN( sample_fifo_used( ctx->fifo ), AUDIO_ENCODE_BUFFER_SIZE ) / frame_length;
 	}
 
 	// Get the audio samples
