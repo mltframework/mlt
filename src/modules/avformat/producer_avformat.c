mltframework/mlt

commit c4d4e0a8f9ba53345a8dc81e510b0b74f4b36027

Fix left column flicker on sliced pixfmt conversion.
Fixes Shotcut bug https://github.com/mltframework/shotcut/issues/418

Require the width of the last slice to be a multiple of 8.

src/modules/avformat/producer_avformat.c
@@ -1461,21 +1461,23 @@ static int convert_image( producer_avformat self, AVFrame *frame, uint8_t *buffe
 
 		avpicture_fill( ctx.output, buffer, ctx.dst_format, width, height );
 
-		if ( !getenv("MLT_AVFORMAT_SLICED_PIXFMT_DISABLE") )
+		if ( !getenv("MLT_AVFORMAT_SLICED_PIXFMT_DISABLE") ) {
 			ctx.slice_w = ( width < 1000 )
 				? ( 256 >> frame->interlaced_frame )
 				: ( 512 >> frame->interlaced_frame );
-		else
-			ctx.slice_w = width;
+		}
 
 		c = ( width + ctx.slice_w - 1 ) / ctx.slice_w;
+		int last_slice_w = width - ctx.slice_w * (c - 1);
 		c *= frame->interlaced_frame ? 2 : 1;
 
-		if ( !getenv("MLT_AVFORMAT_SLICED_PIXFMT_DISABLE") )
+		if ( (last_slice_w % 8) || !getenv("MLT_AVFORMAT_SLICED_PIXFMT_DISABLE") ) {
+			ctx.slice_w = width;
 			mlt_slices_run_normal( c, sliced_h_pix_fmt_conv_proc, &ctx );
-		else
+		} else {
 			for ( i = 0 ; i < c; i++ )
 				sliced_h_pix_fmt_conv_proc( i, i, c, &ctx );
+		}
 
 		result = profile->colorspace;
 	}

commit c71dd89105a311c5423bbe1ff6a05b46c252c245

Fix sliced pix_fmt conversion regression in c4d4e0a8.
It would crash with MLT_AVFORMAT_SLICED_PIXFMT_DISABLE defined since
ctx.slice_w was not initialized, and it was always running with one
slice with it undefined.

src/modules/avformat/producer_avformat.c
@@ -1492,16 +1492,18 @@ static int convert_image( producer_avformat self, AVFrame *frame, uint8_t *buffe
 			ctx.slice_w = ( width < 1000 )
 				? ( 256 >> frame->interlaced_frame )
 				: ( 512 >> frame->interlaced_frame );
+		} else {
+			ctx.slice_w = width;
 		}
 
 		c = ( width + ctx.slice_w - 1 ) / ctx.slice_w;
 		int last_slice_w = width - ctx.slice_w * (c - 1);
 		c *= frame->interlaced_frame ? 2 : 1;
 
-		if ( (last_slice_w % 8) || !getenv("MLT_AVFORMAT_SLICED_PIXFMT_DISABLE") ) {
-			ctx.slice_w = width;
+		if ( (last_slice_w % 8) == 0 && !getenv("MLT_AVFORMAT_SLICED_PIXFMT_DISABLE") ) {
 			mlt_slices_run_normal( c, sliced_h_pix_fmt_conv_proc, &ctx );
 		} else {
+			ctx.slice_w = width;
 			for ( i = 0 ; i < c; i++ )
 				sliced_h_pix_fmt_conv_proc( i, i, c, &ctx );
 		}