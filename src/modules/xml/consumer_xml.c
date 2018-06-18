mltframework/mlt

commit c091cae3a34fe9046e7ed610bd64d2df3c11dfe0

Add time format support for animations to xml consumer.

src/modules/xml/consumer_xml.c
@@ -1,6 +1,6 @@
 /*
  * consumer_xml.c -- a libxml2 serialiser of mlt service networks
- * Copyright (C) 2003-2017 Meltytech, LLC
+ * Copyright (C) 2003-2018 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -205,11 +205,11 @@ static void serialise_properties( serialise_context context, mlt_properties prop
 			 strcmp( name, "width" ) &&
 			 strcmp( name, "height" ) )
 		{
-			char *value = NULL;
-			if ( !strcmp( name, "length" ) )
-				value = mlt_properties_get_time( properties, name, context->time_format );
-			else
-				value = mlt_properties_get_value( properties, i );
+			// Force a string with keyframes to be parsed with mlt_animation to
+			// facilitate output with the specified time format.
+			mlt_properties_anim_get_int( properties, name, 0, 0 );
+
+			char *value = mlt_properties_get_value_tf( properties, i, context->time_format );
 			if ( value )
 			{
 				int rootlen = strlen( context->root );
@@ -258,7 +258,11 @@ static void serialise_store_properties( serialise_context context, mlt_propertie
 		char *name = mlt_properties_get_name( properties, i );
 		if ( !strncmp( name, store, strlen( store ) ) )
 		{
-			char *value = mlt_properties_get_value( properties, i );
+			// Force a string with keyframes to be parsed with mlt_animation to
+			// facilitate output with the specified time format.
+			mlt_properties_anim_get_int( properties, name, 0, 0 );
+
+			char *value = mlt_properties_get_value_tf( properties, i, context->time_format );
 			if ( value )
 			{
 				int rootlen = strlen( context->root );

commit 3e3c5fffcc351e556fc48bee83a80aa3918c3718

Fix serializing non-animation strings with '='.
Regression in c091cae3a34fe9046e7ed610bd64d2df3c11dfe0

src/modules/xml/consumer_xml.c
@@ -205,10 +205,6 @@ static void serialise_properties( serialise_context context, mlt_properties prop
 			 strcmp( name, "width" ) &&
 			 strcmp( name, "height" ) )
 		{
-			// Force a string with keyframes to be parsed with mlt_animation to
-			// facilitate output with the specified time format.
-			mlt_properties_anim_get_int( properties, name, 0, 0 );
-
 			char *value = mlt_properties_get_value_tf( properties, i, context->time_format );
 			if ( value )
 			{
@@ -258,10 +254,6 @@ static void serialise_store_properties( serialise_context context, mlt_propertie
 		char *name = mlt_properties_get_name( properties, i );
 		if ( !strncmp( name, store, strlen( store ) ) )
 		{
-			// Force a string with keyframes to be parsed with mlt_animation to
-			// facilitate output with the specified time format.
-			mlt_properties_anim_get_int( properties, name, 0, 0 );
-
 			char *value = mlt_properties_get_value_tf( properties, i, context->time_format );
 			if ( value )
 			{