mltframework/mlt

commit bc4d0765aedfad67c105897560cf2cc78964a66d

Fix SDL_MixAudioFormat() degrades scrubbing.

src/modules/sdl2/consumer_sdl2_audio.c
@@ -121,7 +121,7 @@ mlt_consumer consumer_sdl2_audio_init( mlt_profile profile, mlt_service_type typ
 
 		// Ensure we don't join on a non-running object
 		self->joined = 1;
-		
+
 		// Allow thread to be started/stopped
 		parent->start = consumer_start;
 		parent->stop = consumer_stop;
@@ -258,6 +258,7 @@ void consumer_purge( mlt_consumer parent )
 static void sdl_fill_audio( void *udata, uint8_t *stream, int len )
 {
 	consumer_sdl self = udata;
+	int bytes = MIN(len, self->audio_avail);
 
 	// Get the volume
 	double volume = mlt_properties_get_double( self->properties, "volume" );
@@ -267,28 +268,24 @@ static void sdl_fill_audio( void *udata, uint8_t *stream, int len )
 
 	pthread_mutex_lock( &self->audio_mutex );
 
-	if ( self->audio_avail >= len )
-	{
-		// Place in the audio buffer
-		if ( volume != 1.0 )
-			SDL_MixAudioFormat( stream, self->audio_buffer, AUDIO_S16SYS, len, ( int )( ( float )SDL_MIX_MAXVOLUME * volume ) );
-		else
-			memcpy( stream, self->audio_buffer, len );
-		// Remove len from the audio available
-		self->audio_avail -= len;
+	// Place in the audio buffer
+	if ( volume != 1.0 ) {
+		// Adjust the volume while copying.
+		int16_t *src = (int16_t*) self->audio_buffer;
+		int16_t *dst = (int16_t*) stream;
+		int i;
+		for (i = 0; i < bytes / sizeof(*dst); i++)
+			dst[i] = CLAMP(volume * src[i], -32768, 32767);
+	} else {
+		memcpy( stream, self->audio_buffer, bytes );
+	}
 
-		// Remove the samples
+	// Remove len from the audio available
+	self->audio_avail -= bytes;
+
+	// Remove the samples
+	if ( self->audio_avail > len )
 		memmove( self->audio_buffer, self->audio_buffer + len, self->audio_avail );
-	}
-	else
-	{
-		// Mix the audio
-		SDL_MixAudioFormat( stream, self->audio_buffer, self->audio_avail, AUDIO_S16SYS,
-			( int )( ( float )SDL_MIX_MAXVOLUME * volume ) );
-
-		// No audio left
-		self->audio_avail = 0;
-	}
 
 	// We're definitely playing now
 	self->playing = 1;

commit 71e549c379c932a72a3a4cc3457e66d09a92e758

Fix sdl2_audio distortion regression in bc4d076.
Fixes https://github.com/mltframework/shotcut/issues/608

src/modules/sdl2/consumer_sdl2_audio.c
@@ -258,7 +258,6 @@ void consumer_purge( mlt_consumer parent )
 static void sdl_fill_audio( void *udata, uint8_t *stream, int len )
 {
 	consumer_sdl self = udata;
-	int bytes = MIN(len, self->audio_avail);
 
 	// Get the volume
 	double volume = mlt_properties_get_double( self->properties, "volume" );
@@ -267,15 +266,18 @@ static void sdl_fill_audio( void *udata, uint8_t *stream, int len )
 	memset( stream, 0, len );
 
 	pthread_mutex_lock( &self->audio_mutex );
+	int bytes = MIN(len, self->audio_avail);
 
 	// Place in the audio buffer
 	if ( volume != 1.0 ) {
 		// Adjust the volume while copying.
 		int16_t *src = (int16_t*) self->audio_buffer;
 		int16_t *dst = (int16_t*) stream;
-		int i;
-		for (i = 0; i < bytes / sizeof(*dst); i++)
-			dst[i] = CLAMP(volume * src[i], -32768, 32767);
+		int i = bytes / sizeof(*dst) + 1;
+		while (--i) {
+			*dst++ = CLAMP(volume * src[0], -32768, 32767);
+			src++;
+		}
 	} else {
 		memcpy( stream, self->audio_buffer, bytes );
 	}
@@ -284,8 +286,7 @@ static void sdl_fill_audio( void *udata, uint8_t *stream, int len )
 	self->audio_avail -= bytes;
 
 	// Remove the samples
-	if ( self->audio_avail > len )
-		memmove( self->audio_buffer, self->audio_buffer + len, self->audio_avail );
+	memmove( self->audio_buffer, self->audio_buffer + bytes, self->audio_avail );
 
 	// We're definitely playing now
 	self->playing = 1;