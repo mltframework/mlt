mltframework/mlt

commit e86a51587a6646404ef84458c0dbd72a084e2929

Restart SDL audio in case of timeout.
A timeout may occur due to a device or device configuration change,
typically initiated by the user. This change tries to detect it and
restart SDL audio. Note that on Windows while testing, any attempt to
close the SDL audio device or quit the audio subsystem results in
deadlock.

https://forum.shotcut.org/t/changing-the-bit-depth-and-sample-rate-in-
windows-renders-the-player-unresponsive-in-v18-06-02-and-later-
v18-03-06-and-earlier-dont-have-this-problem/7185

src/modules/sdl2/consumer_sdl2.c
@@ -275,8 +275,6 @@ int consumer_stop( mlt_consumer parent )
 		if ( self->sdl_window )
 			SDL_DestroyWindow( self->sdl_window );
 		self->sdl_window = NULL;
-		if ( !mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "audio_off" ) )
-			SDL_QuitSubSystem( SDL_INIT_AUDIO );
 		if ( mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "sdl_started" ) == 0 )
 			SDL_Quit( );
 		pthread_mutex_unlock( &mlt_sdl_mutex );
@@ -425,8 +423,21 @@ static int consumer_play_audio( consumer_sdl self, mlt_frame frame, int init_aud
 			int sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
 			while ( self->running && sample_space == 0 )
 			{
-				pthread_cond_wait( &self->audio_cond, &self->audio_mutex );
+				struct timeval now;
+				struct timespec tm;
+
+				gettimeofday( &now, NULL );
+				tm.tv_sec = now.tv_sec + 1;
+				tm.tv_nsec = now.tv_usec * 1000;
+				pthread_cond_timedwait( &self->audio_cond, &self->audio_mutex, &tm );
 				sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
+
+				if ( sample_space == 0 )
+				{
+					mlt_log_warning( MLT_CONSUMER_SERVICE(&self->parent), "audio timed out\n" );
+					pthread_mutex_unlock( &self->audio_mutex );
+					return 1;
+				}
 			}
 			if ( self->running )
 			{

src/modules/sdl2/consumer_sdl2_audio.c
@@ -1,7 +1,6 @@
 /*
  * consumer_sdl2_audio.c -- A Simple DirectMedia Layer audio-only consumer
  * Copyright (C) 2009-2018 Meltytech, LLC
- * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -223,8 +222,6 @@ int consumer_stop( mlt_consumer parent )
 		pthread_mutex_lock( &self->audio_mutex );
 		pthread_cond_broadcast( &self->audio_cond );
 		pthread_mutex_unlock( &self->audio_mutex );
-
-		SDL_QuitSubSystem( SDL_INIT_AUDIO );
 	}
 
 	return 0;
@@ -369,8 +366,21 @@ static int consumer_play_audio( consumer_sdl self, mlt_frame frame, int init_aud
 			int sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
 			while ( self->running && sample_space == 0 )
 			{
-				pthread_cond_wait( &self->audio_cond, &self->audio_mutex );
+				struct timeval now;
+				struct timespec tm;
+
+				gettimeofday( &now, NULL );
+				tm.tv_sec = now.tv_sec + 1;
+				tm.tv_nsec = now.tv_usec * 1000;
+				pthread_cond_timedwait( &self->audio_cond, &self->audio_mutex, &tm );
 				sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
+
+				if ( sample_space == 0 )
+				{
+					mlt_log_warning( MLT_CONSUMER_SERVICE(&self->parent), "audio timed out\n" );
+					pthread_mutex_unlock( &self->audio_mutex );
+					return 1;
+				}
 			}
 			if ( self->running )
 			{

commit bec3b18ec598cd82d2a81cdea90ff5a76c7174aa

Fix crash in SDL2 at exit on Linux and macOS.
Regression in previous commit e86a515.

src/modules/sdl2/consumer_sdl2.c
@@ -275,6 +275,10 @@ int consumer_stop( mlt_consumer parent )
 		if ( self->sdl_window )
 			SDL_DestroyWindow( self->sdl_window );
 		self->sdl_window = NULL;
+#ifndef _WIN32
+		if ( !mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "audio_off" ) )
+			SDL_QuitSubSystem( SDL_INIT_AUDIO );
+#endif
 		if ( mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "sdl_started" ) == 0 )
 			SDL_Quit( );
 		pthread_mutex_unlock( &mlt_sdl_mutex );

src/modules/sdl2/consumer_sdl2_audio.c
@@ -222,6 +222,9 @@ int consumer_stop( mlt_consumer parent )
 		pthread_mutex_lock( &self->audio_mutex );
 		pthread_cond_broadcast( &self->audio_cond );
 		pthread_mutex_unlock( &self->audio_mutex );
+#ifndef _WIN32
+		SDL_QuitSubSystem( SDL_INIT_AUDIO );
+#endif
 	}
 
 	return 0;