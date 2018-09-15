mltframework/mlt

commit d3949dc224a94a1b9093eaefe25f2b4ba9ee2eed

Convert frei0r to use mlt_color.

src/modules/frei0r/frei0r_helper.c
@@ -21,16 +21,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-static void parse_color( int color, f0r_param_color_t *fcolor )
-{
-	fcolor->r = ( color >> 24 ) & 0xff;
-	fcolor->r /= 255;
-	fcolor->g = ( color >> 16 ) & 0xff;
-	fcolor->g /= 255;
-	fcolor->b = ( color >>  8 ) & 0xff;
-	fcolor->b /= 255;
-}
-
 static void rgba_bgra( uint8_t *src, uint8_t* dst, int width, int height )
 {
 	int n = width * height + 1;
@@ -160,11 +150,13 @@ int process_frei0r_item( mlt_service service, double position, double time, int 
 					}
 					case F0R_PARAM_COLOR:
 					{
-						f0r_param_color_t color;
-						int int_color = mlt_properties_get(prop, index) ?
-							mlt_properties_get_int(prop, index) : mlt_properties_get_int(prop, pinfo.name);
-						parse_color(int_color, &color);
-						f0r_set_param_value(inst, &color, i);
+						f0r_param_color_t f_color;
+						mlt_color m_color = mlt_properties_get(prop, index) ?
+							mlt_properties_get_color(prop, index) : mlt_properties_get_color(prop, pinfo.name);
+						f_color.r = m_color.r / 255;
+						f_color.g = m_color.g / 255;
+						f_color.b = m_color.b / 255;
+						f0r_set_param_value(inst, &f_color, i);
 						break;
 					}
 					case F0R_PARAM_STRING:

commit ac8d1f3485c58139429e4503a7fcc2daf0d72659

Fix converting mlt_color to f0r_param_color_t.
Regression in d3949dc2.

src/modules/frei0r/frei0r_helper.c
@@ -153,9 +153,9 @@ int process_frei0r_item( mlt_service service, double position, double time, int 
 						f0r_param_color_t f_color;
 						mlt_color m_color = mlt_properties_get(prop, index) ?
 							mlt_properties_get_color(prop, index) : mlt_properties_get_color(prop, pinfo.name);
-						f_color.r = m_color.r / 255;
-						f_color.g = m_color.g / 255;
-						f_color.b = m_color.b / 255;
+						f_color.r = (float) m_color.r / 255.0f;
+						f_color.g = (float) m_color.g / 255.0f;
+						f_color.b = (float) m_color.b / 255.0f;
 						f0r_set_param_value(inst, &f_color, i);
 						break;
 					}