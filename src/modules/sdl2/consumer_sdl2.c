mltframework/mlt

commit e86a51587a6646404ef84458c0dbd72a084e2929

Restart SDL audio in case of timeout.
A timeout may occur due to a device or device configuration change,
typically initiated by the user. This change tries to detect it and
restart SDL audio. Note that on Windows while testing, any attempt to
close the SDL audio device or quit the audio subsystem results in
deadlock.

https://forum.shotcut.org/t/changing-the-bit-depth-and-sample-rate-in-
windows-renders-the-player-unresponsive-in-v18-06-02-and-later-
v18-03-06-and-earlier-dont-have-this-problem/7185

src/modules/sdl2/consumer_sdl2.c
@@ -275,8 +275,6 @@ int consumer_stop( mlt_consumer parent )
 		if ( self->sdl_window )
 			SDL_DestroyWindow( self->sdl_window );
 		self->sdl_window = NULL;
-		if ( !mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "audio_off" ) )
-			SDL_QuitSubSystem( SDL_INIT_AUDIO );
 		if ( mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "sdl_started" ) == 0 )
 			SDL_Quit( );
 		pthread_mutex_unlock( &mlt_sdl_mutex );
@@ -425,8 +423,21 @@ static int consumer_play_audio( consumer_sdl self, mlt_frame frame, int init_aud
 			int sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
 			while ( self->running && sample_space == 0 )
 			{
-				pthread_cond_wait( &self->audio_cond, &self->audio_mutex );
+				struct timeval now;
+				struct timespec tm;
+
+				gettimeofday( &now, NULL );
+				tm.tv_sec = now.tv_sec + 1;
+				tm.tv_nsec = now.tv_usec * 1000;
+				pthread_cond_timedwait( &self->audio_cond, &self->audio_mutex, &tm );
 				sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
+
+				if ( sample_space == 0 )
+				{
+					mlt_log_warning( MLT_CONSUMER_SERVICE(&self->parent), "audio timed out\n" );
+					pthread_mutex_unlock( &self->audio_mutex );
+					return 1;
+				}
 			}
 			if ( self->running )
 			{

src/modules/sdl2/consumer_sdl2_audio.c
@@ -1,7 +1,6 @@
 /*
  * consumer_sdl2_audio.c -- A Simple DirectMedia Layer audio-only consumer
  * Copyright (C) 2009-2018 Meltytech, LLC
- * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -223,8 +222,6 @@ int consumer_stop( mlt_consumer parent )
 		pthread_mutex_lock( &self->audio_mutex );
 		pthread_cond_broadcast( &self->audio_cond );
 		pthread_mutex_unlock( &self->audio_mutex );
-
-		SDL_QuitSubSystem( SDL_INIT_AUDIO );
 	}
 
 	return 0;
@@ -369,8 +366,21 @@ static int consumer_play_audio( consumer_sdl self, mlt_frame frame, int init_aud
 			int sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
 			while ( self->running && sample_space == 0 )
 			{
-				pthread_cond_wait( &self->audio_cond, &self->audio_mutex );
+				struct timeval now;
+				struct timespec tm;
+
+				gettimeofday( &now, NULL );
+				tm.tv_sec = now.tv_sec + 1;
+				tm.tv_nsec = now.tv_usec * 1000;
+				pthread_cond_timedwait( &self->audio_cond, &self->audio_mutex, &tm );
 				sample_space = ( sizeof( self->audio_buffer ) - self->audio_avail ) / dst_stride;
+
+				if ( sample_space == 0 )
+				{
+					mlt_log_warning( MLT_CONSUMER_SERVICE(&self->parent), "audio timed out\n" );
+					pthread_mutex_unlock( &self->audio_mutex );
+					return 1;
+				}
 			}
 			if ( self->running )
 			{

commit f42aaf821eec1fd99180874795c8283d43bb7c3e

Fix audio distortion regression in e86a515 on Windows.

src/modules/sdl2/consumer_sdl2.c
@@ -69,6 +69,9 @@ struct consumer_sdl_s
 	SDL_Rect sdl_rect;
 	uint8_t *buffer;
 	int is_purge;
+#ifdef _WIN32
+	int no_quit_subsystem;
+#endif
 };
 
 /** Forward references to static functions.
@@ -275,10 +278,11 @@ int consumer_stop( mlt_consumer parent )
 		if ( self->sdl_window )
 			SDL_DestroyWindow( self->sdl_window );
 		self->sdl_window = NULL;
-#ifndef _WIN32
+#ifdef _WIN32
+		if ( !self->no_quit_subsystem )
+#endif
 		if ( !mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "audio_off" ) )
 			SDL_QuitSubSystem( SDL_INIT_AUDIO );
-#endif
 		if ( mlt_properties_get_int( MLT_CONSUMER_PROPERTIES( parent ), "sdl_started" ) == 0 )
 			SDL_Quit( );
 		pthread_mutex_unlock( &mlt_sdl_mutex );
@@ -440,6 +444,9 @@ static int consumer_play_audio( consumer_sdl self, mlt_frame frame, int init_aud
 				{
 					mlt_log_warning( MLT_CONSUMER_SERVICE(&self->parent), "audio timed out\n" );
 					pthread_mutex_unlock( &self->audio_mutex );
+#ifdef _WIN32
+					self->no_quit_subsystem = 1;
+#endif
 					return 1;
 				}
 			}

src/modules/sdl2/consumer_sdl2_audio.c
@@ -60,6 +60,9 @@ struct consumer_sdl_s
 	pthread_mutex_t refresh_mutex;
 	int refresh_count;
 	int is_purge;
+#ifdef _WIN32
+	int no_quit_subsystem;
+#endif
 };
 
 /** Forward references to static functions.
@@ -182,7 +185,6 @@ int consumer_start( mlt_consumer parent )
 			mlt_log_error( MLT_CONSUMER_SERVICE(parent), "Failed to initialize SDL: %s\n", SDL_GetError() );
 			return -1;
 		}
-
 		self->running = 1;
 		self->joined = 0;
 		pthread_create( &self->thread, NULL, consumer_thread, self );
@@ -222,9 +224,10 @@ int consumer_stop( mlt_consumer parent )
 		pthread_mutex_lock( &self->audio_mutex );
 		pthread_cond_broadcast( &self->audio_cond );
 		pthread_mutex_unlock( &self->audio_mutex );
-#ifndef _WIN32
-		SDL_QuitSubSystem( SDL_INIT_AUDIO );
+#ifdef _WIN32
+		if ( !self->no_quit_subsystem )
 #endif
+		SDL_QuitSubSystem( SDL_INIT_AUDIO );
 	}
 
 	return 0;
@@ -382,6 +385,9 @@ static int consumer_play_audio( consumer_sdl self, mlt_frame frame, int init_aud
 				{
 					mlt_log_warning( MLT_CONSUMER_SERVICE(&self->parent), "audio timed out\n" );
 					pthread_mutex_unlock( &self->audio_mutex );
+#ifdef _WIN32
+					self->no_quit_subsystem = 1;
+#endif
 					return 1;
 				}
 			}