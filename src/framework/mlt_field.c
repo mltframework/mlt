mltframework/mlt

commit d0a2a23dda1bf745a2d4cefd8c8205362c375e87

Prevent crash on invalid transition track index values.

src/framework/mlt_field.c
@@ -3,7 +3,7 @@
  * \brief a field for planting multiple transitions and filters
  * \see mlt_field_s
  *
- * Copyright (C) 2003-2014 Meltytech, LLC
+ * Copyright (C) 2003-2018 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -202,6 +202,9 @@ int mlt_field_plant_filter( mlt_field self, mlt_filter that, int track )
 int mlt_field_plant_transition( mlt_field self, mlt_transition that, int a_track, int b_track )
 {
 	// Connect the transition to the last producer
+	int track_max = MAX( mlt_multitrack_count( self->multitrack ) - 1, 0 );
+	a_track = CLAMP( a_track, 0, track_max );
+	b_track = CLAMP( b_track, 0, track_max );
 	int result = mlt_transition_connect( that, self->producer, a_track, b_track );
 
 	// If sucessful, then we'll use self for connecting in the future

src/framework/mlt_tractor.c
@@ -311,6 +311,7 @@ int mlt_tractor_remove_track( mlt_tractor self, int index )
 		{
 			mlt_service_type type = mlt_service_identify( service );
 			mlt_properties properties = MLT_SERVICE_PROPERTIES( service );
+			int track_max = MAX( mlt_multitrack_count( mlt_tractor_multitrack( self ) ) - 1, 0 );
 
 			if ( type == transition_type )
 			{
@@ -318,10 +319,10 @@ int mlt_tractor_remove_track( mlt_tractor self, int index )
 				int a_track = mlt_transition_get_a_track( transition );
 				int b_track = mlt_transition_get_b_track( transition );
 
-				if ( a_track >= index || b_track >= index )
+				if ( a_track > index || b_track >= index )
 				{
-					a_track = MAX( a_track > index ? a_track - 1 : a_track, 0 );
-					b_track = MAX( b_track >= index ? b_track - 1 : b_track, 0 );
+					a_track = CLAMP( a_track > index ? a_track - 1 : a_track, 0,  track_max );
+					b_track = CLAMP( b_track >= index ? b_track - 1 : b_track, 0, track_max );
 					mlt_transition_set_tracks( transition, a_track, b_track );
 				}
 			}
@@ -329,7 +330,7 @@ int mlt_tractor_remove_track( mlt_tractor self, int index )
 			{
 				int current_track = mlt_properties_get_int( properties, "track" );
 				if ( current_track >= index )
-					mlt_properties_set_int( properties, "track", MAX( current_track - 1, 0 ) );
+					mlt_properties_set_int( properties, "track", CLAMP( current_track - 1, 0, track_max ) );
 			}
 			service = mlt_service_producer( service );
 		}

commit d6d9b864f33cd1e37e921a2d874a1ab7405076eb

Fix #385 melt -transition is broken.
Regression in d0a2a23.

src/framework/mlt_field.c
@@ -202,9 +202,6 @@ int mlt_field_plant_filter( mlt_field self, mlt_filter that, int track )
 int mlt_field_plant_transition( mlt_field self, mlt_transition that, int a_track, int b_track )
 {
 	// Connect the transition to the last producer
-	int track_max = MAX( mlt_multitrack_count( self->multitrack ) - 1, 0 );
-	a_track = CLAMP( a_track, 0, track_max );
-	b_track = CLAMP( b_track, 0, track_max );
 	int result = mlt_transition_connect( that, self->producer, a_track, b_track );
 
 	// If successful, then we'll use self for connecting in the future