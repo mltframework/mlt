2d8a90e3f8c784768e38ac947cb0bdedcd7295b0
Dan Dennedy <dan@dennedy.org>
2018-12-13 19:08:45 -0800

    Make the mask filters' nested service mutable.

--- src/modules/core/filter_mask_apply.c
+++ src/modules/core/filter_mask_apply.c
@@ -28,6 +28,7 @@
 static int get_image(mlt_frame frame, uint8_t **image, mlt_image_format *format, int *width, int *height, int writable)
 {
 	mlt_transition transition = mlt_frame_pop_service(frame);
+	*format = mlt_frame_pop_service_int(frame);
 	int error = mlt_frame_get_image(frame, image, format, width, height, writable);
 	if (!error) {
 		mlt_properties properties = MLT_FRAME_PROPERTIES(frame);
@@ -46,14 +47,17 @@ static mlt_frame process(mlt_filter filter, mlt_frame frame)
 {
 	mlt_properties properties = MLT_FILTER_PROPERTIES(filter);
 	mlt_transition transition = mlt_properties_get_data(properties, "instance", NULL);
+	char *name = mlt_properties_get(MLT_FILTER_PROPERTIES(filter), "transition");
+
+	if (!name || !strcmp("", name))
+		return frame;
 
 	// Create the transition if needed.
-	if (!transition) {
-		char *name = mlt_properties_get(MLT_FILTER_PROPERTIES(filter), "transition");
+	if (!transition
+		|| !mlt_properties_get(MLT_FILTER_PROPERTIES(transition), "mlt_service") 
+		|| strcmp(name, mlt_properties_get(MLT_FILTER_PROPERTIES(transition), "mlt_service"))) {
 		mlt_profile profile = mlt_service_profile(MLT_FILTER_SERVICE(filter));
 
-		if (!name || !strcmp("", name))
-			name = "frei0r.composition";
 		transition = mlt_factory_transition(profile, name, NULL);
 		mlt_properties_set_data(MLT_FILTER_PROPERTIES(filter), "instance", transition, 0, (mlt_destructor) mlt_transition_close, NULL);
 	}
@@ -68,6 +72,8 @@ static mlt_frame process(mlt_filter filter, mlt_frame frame)
 
 		// Only if video transition on visible track.
 		if ((type & 1) && !mlt_frame_is_test_card(frame) && !(hide & 1)) {
+			mlt_frame_push_service_int(frame,
+				mlt_image_format_id(mlt_properties_get(properties, "mlt_image_format")));
 			mlt_frame_push_service(frame, transition);
 			mlt_frame_push_get_image(frame, get_image);
 		}
@@ -84,7 +90,8 @@ mlt_filter filter_mask_apply_init(mlt_profile profile, mlt_service_type type, co
 {
 	mlt_filter filter = mlt_filter_new( );
 	if (filter) {
-		mlt_properties_set(MLT_FILTER_PROPERTIES(filter), "transition", arg);
+		mlt_properties_set(MLT_FILTER_PROPERTIES(filter), "transition", arg? arg : "frei0r.composition");
+		mlt_properties_set(MLT_FILTER_PROPERTIES(filter), "mlt_image_format", "rgb24a");
 		filter->process = process;
 	}
 	return filter;
--- src/modules/core/filter_mask_start.c
+++ src/modules/core/filter_mask_start.c
@@ -30,6 +30,8 @@ static int get_image(mlt_frame frame, uint8_t **image, mlt_image_format *format,
 	int error = mlt_frame_get_image(frame, image, format, width, height, writable);
 	if (!error) {
 		mlt_frame clone = mlt_frame_clone(frame, 1);
+		clone->convert_audio = frame->convert_audio;
+		clone->convert_image = frame->convert_image;
 		mlt_properties_set_data(properties,
 			"mask frame", clone, 0, (mlt_destructor) mlt_frame_close, NULL);
 	}
@@ -40,14 +42,16 @@ static mlt_frame process(mlt_filter filter, mlt_frame frame)
 {
 	mlt_properties properties = MLT_FILTER_PROPERTIES(filter);
 	mlt_filter instance = mlt_properties_get_data(properties, "instance", NULL);
+	const char* name = mlt_properties_get(properties, "filter");
+
+	if (!name || !strcmp("", name))
+		return frame;
 
 	// Create the filter if needed.
-	if (!instance) {
-		const char* name = mlt_properties_get(properties, "filter");
+	if (!instance
+		|| !mlt_properties_get(MLT_FILTER_PROPERTIES(instance), "mlt_service") 
+		|| strcmp(name, mlt_properties_get(MLT_FILTER_PROPERTIES(instance), "mlt_service"))) {
 		mlt_profile profile = mlt_service_profile(MLT_FILTER_SERVICE(filter));
-
-		if (!name || !strcmp("", name))
-			name = "frei0r.alphaspot";
 		instance = mlt_factory_filter(profile, name, NULL);
 		mlt_properties_set_data(properties, "instance", instance, 0, (mlt_destructor) mlt_filter_close, NULL);
 	}
@@ -69,7 +73,7 @@ mlt_filter filter_mask_start_init(mlt_profile profile, mlt_service_type type, co
 {
 	mlt_filter filter = mlt_filter_new();
 	if (filter)	{
-		mlt_properties_set(MLT_FILTER_PROPERTIES(filter), "filter", arg);
+		mlt_properties_set(MLT_FILTER_PROPERTIES(filter), "filter", arg? arg : "frei0r.alphaspot");
 		filter->process = process;
 	}
 	return filter;
