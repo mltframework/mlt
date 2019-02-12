af3e74a6cde5ceb93ebdca44634e148e2cb43b2f
Brian Matherly <code@brianmatherly.com>
2019-02-11 22:17:43 -0600

    Add createdate keyword to dynamictext

--- src/modules/plus/filter_dynamictext.c
+++ src/modules/plus/filter_dynamictext.c
@@ -165,6 +165,39 @@ static void get_resource_str( mlt_filter filter, mlt_frame frame, char* text )
 	strncat( text, mlt_properties_get( producer_properties, "resource" ), MAX_TEXT_LEN - strlen( text ) - 1 );
 }
 
+static void get_createdate_str( const char* keyword, mlt_filter filter, mlt_frame frame, char* text )
+{
+	char* datestr = mlt_properties_get( MLT_FRAME_PROPERTIES( frame ), "meta.attr.creation_time.markup");
+	const char *format = "%Y/%m/%d";
+	int n = strlen( "createdate" ) + 1;
+	if ( strlen( keyword ) > n )
+		format = &keyword[n];
+	struct tm time_info;
+	char *date = calloc( 1, MAX_TEXT_LEN );
+
+	if ( datestr && strptime( datestr, "%Y-%m-%dT%H:%M:%S", &time_info ) )
+	{
+		// Prefer creation_time property if available.
+		strftime( date, MAX_TEXT_LEN, format, &time_info );
+	}
+	else
+	{
+		// Fall back to file modification time.
+		mlt_producer producer = mlt_producer_cut_parent( mlt_frame_get_original_producer( frame ) );
+		mlt_properties producer_properties = MLT_PRODUCER_PROPERTIES( producer );
+		char* filename = mlt_properties_get( producer_properties, "resource");
+		struct stat file_info;
+		if ( !stat( filename, &file_info ) )
+		{
+			strftime( date, MAX_TEXT_LEN, format, gmtime( &(file_info.st_mtime) ) );
+
+		}
+	}
+
+	strncat( text, date, MAX_TEXT_LEN - strlen( text ) - 1 );
+	free( date );
+}
+
 /** Perform substitution for keywords that are enclosed in "# #".
 */
 static void substitute_keywords(mlt_filter filter, char* result, char* value, mlt_frame frame)
@@ -206,6 +239,10 @@ static void substitute_keywords(mlt_filter filter, char* result, char* value, ml
 		else if ( !strcmp( keyword, "resource" ) )
 		{
 			get_resource_str( filter, frame, result );
+		}
+		else if ( !strncmp( keyword, "createdate", 10 ) )
+		{
+			get_createdate_str( keyword, filter, frame, result );
 		}
 		else
 		{
