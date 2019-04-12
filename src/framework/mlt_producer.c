a799ee2f0a993631fb02578fb5254d4b7f6cd35f
Dan Dennedy <dan@dennedy.org>
2019-04-12 09:46:44 -0700

    Fix timegm() not available in mingw.

--- src/framework/mlt_producer.c
+++ src/framework/mlt_producer.c
@@ -3,7 +3,7 @@
  * \brief abstraction for all producer services
  * \see mlt_producer_s
  *
- * Copyright (C) 2003-2018 Meltytech, LLC
+ * Copyright (C) 2003-2019 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -1100,6 +1100,75 @@ void mlt_producer_close( mlt_producer self )
 	}
 }
 
+
+/*
+ * Boost implementation of timegm()
+ * (C) Copyright Howard Hinnant
+ * (C) Copyright 2010-2011 Vicente J. Botet Escriba
+ */
+
+static inline int32_t is_leap(int32_t year)
+{
+	if(year % 400 == 0)
+		return 1;
+	if(year % 100 == 0)
+		return 0;
+	if(year % 4 == 0)
+		return 1;
+	return 0;
+}
+
+static inline int32_t days_from_0(int32_t year)
+{
+	year--;
+	return 365 * year + (year / 400) - (year/100) + (year / 4);
+}
+
+static inline int32_t days_from_1970(int32_t year)
+{
+	const int days_from_0_to_1970 = days_from_0(1970);
+	return days_from_0(year) - days_from_0_to_1970;
+}
+
+static inline int32_t days_from_1jan(int32_t year,int32_t month,int32_t day)
+{
+	static const int32_t days[2][12] =
+	{
+		{ 0,31,59,90,120,151,181,212,243,273,304,334},
+		{ 0,31,60,91,121,152,182,213,244,274,305,335}
+	};
+	return days[is_leap(year)][month-1] + day - 1;
+}
+
+static inline time_t internal_timegm(struct tm const *t)
+{
+	int year = t->tm_year + 1900;
+	int month = t->tm_mon;
+	if(month > 11)
+	{
+		year += month/12;
+		month %= 12;
+	}
+	else if(month < 0)
+	{
+		int years_diff = (-month + 11)/12;
+		year -= years_diff;
+		month+=12 * years_diff;
+	}
+	month++;
+	int day = t->tm_mday;
+	int day_of_year = days_from_1jan(year,month,day);
+	int days_since_epoch = days_from_1970(year) + day_of_year;
+
+	time_t seconds_in_day = 3600 * 24;
+	time_t result = seconds_in_day * days_since_epoch + 3600 * t->tm_hour + 60 * t->tm_min + t->tm_sec;
+
+	return result;
+}
+
+/* End of Boost implementation of timegm(). */
+
+
 /** Get the creation time for the producer.
  *
  * The creation_time value is searched in the following order:
@@ -1136,7 +1205,7 @@ int64_t mlt_producer_get_creation_time( mlt_producer self )
 			time_info.tm_mon  -= 1;
 			time_info.tm_year -= 1900;
 			time_info.tm_isdst =-1;
-			int64_t milliseconds = (int64_t)timegm(&time_info) * 1000;
+			int64_t milliseconds = (int64_t) internal_timegm(&time_info) * 1000;
 			milliseconds += (seconds - (double)time_info.tm_sec) * 1000.0;
 			return milliseconds;
 		}
