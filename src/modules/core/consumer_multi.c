0d7100745a7a77ff9a0f16052bd1efc34d3ae8da
Dan Dennedy <dan@dennedy.org>
2019-09-23 18:48:07 -0700

    change the vendor ID for ProRes

--- src/modules/core/consumer_multi.c
+++ src/modules/core/consumer_multi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-1024 Meltytech, LLC
+ * Copyright (C) 2011-2019 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
