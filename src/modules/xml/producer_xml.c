mltframework/mlt

commit 5e4e790d76436d14d0f1f2d203b27452f7df59a7

support consumer scale in melt and xml producer

src/modules/xml/producer_xml.c
@@ -1,6 +1,6 @@
 /*
  * producer_xml.c -- a libxml2 parser of mlt service networks
- * Copyright (C) 2003-2019 Meltytech, LLC
+ * Copyright (C) 2003-2020 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -80,6 +80,7 @@ struct deserialise_context_s
 	const xmlChar *systemId;
 	mlt_properties params;
 	mlt_profile profile;
+	mlt_profile consumer_profile;
 	int pass;
 	char *lc_numeric;
 	mlt_consumer consumer;
@@ -1165,6 +1166,19 @@ static void on_start_consumer( deserialise_context context, const xmlChar *name,
 	}
 }
 
+static void set_preview_scale(mlt_profile *consumer_profile, mlt_profile *profile, int scale)
+{
+	if (scale != 0) {
+		*consumer_profile = mlt_profile_clone(*profile);
+		if (*consumer_profile) {
+			(*consumer_profile)->width /= scale;
+			(*consumer_profile)->width -= (*consumer_profile)->width % 2;
+			(*consumer_profile)->height /= scale;
+			(*consumer_profile)->height -= (*consumer_profile)->height % 2;
+		}
+	}
+}
+
 static void on_end_consumer( deserialise_context context, const xmlChar *name )
 {
 	if ( context->pass == 1 )
@@ -1223,9 +1237,13 @@ static void on_end_consumer( deserialise_context context, const xmlChar *name )
 			}
 			else
 			{
+				double scale = mlt_properties_get_double(properties, "scale");
+				if (scale > 0.0) {
+					set_preview_scale(&context->consumer_profile, &context->profile, 1.0 / scale);
+				}
 				// Instantiate the consumer
 				char *id = trim( mlt_properties_get( properties, "mlt_service" ) );
-				context->consumer = mlt_factory_consumer( context->profile, id, resource );
+				context->consumer = mlt_factory_consumer( context->consumer_profile, id, resource );
 				if ( context->consumer )
 				{
 					// Track this consumer
@@ -1743,6 +1761,7 @@ static void context_close( deserialise_context context )
 	mlt_deque_close( context->stack_types );
 	mlt_deque_close( context->stack_node );
 	mlt_deque_close( context->stack_branch );
+	mlt_profile_close( context->consumer_profile );
 	xmlFreeDoc( context->entity_doc );
 	free( context->lc_numeric );
 	free( context );

src/melt/melt.c
@@ -727,6 +727,22 @@ static void on_fatal_error( mlt_properties owner, mlt_consumer consumer )
 	mlt_properties_set_int( MLT_CONSUMER_PROPERTIES(consumer), "melt_error", 1 );
 }
 
+static void set_preview_scale(mlt_profile *profile, mlt_profile *backup_profile, int scale)
+{
+	if (scale != 0) {
+		*backup_profile = mlt_profile_clone(*profile);
+		if (*backup_profile) {
+			mlt_profile temp = *profile;
+			*profile = *backup_profile;
+			*backup_profile = temp;
+			(*profile)->width /= scale;
+			(*profile)->width -= (*profile)->width % 2;
+			(*profile)->height /= scale;
+			(*profile)->height -= (*profile)->height % 2;
+		}
+	}
+}
+
 int main( int argc, char **argv )
 {
 	int i;
@@ -910,6 +926,7 @@ query_all:
 	     profile->colorspace != backup_profile->colorspace ) )
 		profile->is_explicit = 1;
 	mlt_profile_close( backup_profile );
+	backup_profile = NULL;
 
 	// Get melt producer
 	if ( argc > 1 )
@@ -923,6 +940,11 @@ query_all:
 			mlt_profile_from_producer( profile, melt );
 			mlt_producer_close( melt );
 			melt = mlt_factory_producer( profile, "melt", &argv[ 1 ] );
+		}
+
+		double scale = mlt_properties_get_double(MLT_CONSUMER_PROPERTIES(consumer), "scale");
+		if (scale > 0.0) {
+			set_preview_scale(&profile, &backup_profile, 1.0 / scale);
 		}
 		
 		// Reload the consumer with the fully qualified profile.
@@ -1066,6 +1088,7 @@ query_all:
 
 	// Close the factory
 	mlt_profile_close( profile );
+	mlt_profile_close( backup_profile );
 
 exit_factory:
 

commit 52bb81f8fa008820c70b5aa40cb7168037bcfad2

fix using correct mlt_profile for embedded consumer
regression in 5e4e790

src/modules/xml/producer_xml.c
@@ -1243,7 +1243,8 @@ static void on_end_consumer( deserialise_context context, const xmlChar *name )
 				}
 				// Instantiate the consumer
 				char *id = trim( mlt_properties_get( properties, "mlt_service" ) );
-				context->consumer = mlt_factory_consumer( context->consumer_profile, id, resource );
+				mlt_profile profile = context->consumer_profile? context->consumer_profile : context->profile;
+				context->consumer = mlt_factory_consumer( profile, id, resource );
 				if ( context->consumer )
 				{
 					// Track this consumer