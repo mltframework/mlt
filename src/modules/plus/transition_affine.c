mltframework/mlt

commit 1857e522f2e482858df2eb0b3256109989fe0d81

refactor to mlt_profile_scale_width/height()

src/modules/plus/transition_affine.c
@@ -468,10 +468,6 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 	int normalised_width = profile->width;
 	int normalised_height = profile->height;
 	double consumer_ar = mlt_profile_sar( profile );
-	double resolution_scale = mlt_frame_resolution_scale(a_frame);
-
-	if (resolution_scale == 1.0)
-		resolution_scale = (double) *width / normalised_width;
 
 	if ( mirror && position > length / 2 )
 		position = abs( position - length );
@@ -483,7 +479,9 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 		return error;
 
 	// Calculate the region now
-	mlt_rect result = {0, 0, normalised_width * resolution_scale, normalised_height * resolution_scale, 1.0};
+	double scale_width = mlt_profile_scale_width(profile, *width);
+	double scale_height = mlt_profile_scale_width(profile, *height);
+	mlt_rect result = {0, 0, normalised_width * scale_width, normalised_height * scale_height, 1.0};
 	mlt_service_lock( MLT_TRANSITION_SERVICE( transition ) );
 
 	if (mlt_properties_get(properties, "geometry"))
@@ -519,10 +517,10 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 		result.o = (result.o == DBL_MIN)? 1.0 : MIN(result.o, 1.0);
 	}
 	mlt_service_unlock( MLT_TRANSITION_SERVICE( transition ) );
-	result.x *= resolution_scale;
-	result.y *= resolution_scale;
-	result.w *= resolution_scale;
-	result.h *= resolution_scale;
+	result.x *= scale_width;
+	result.y *= scale_height;
+	result.w *= scale_width;
+	result.h *= scale_height;
 
 	double geometry_w = result.w;
 	double geometry_h = result.h;
@@ -544,7 +542,7 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 	}
 
 	// Fetch the b frame image
-	if (resolution_scale != 1.0 || mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale")) {
+	if (scale_width > 0.0 || scale_height > 0.0 || mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale")) {
 		// Request b frame image size just what is needed.
 		b_width = result.w;
 		b_height = result.h;

src/modules/avformat/filter_avfilter.c
@@ -686,10 +686,7 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 
 	if( pdata->reset || pdata->format != *format || pdata->width != *width || pdata->height != *height )
 	{
-		double scale = 1.0;
-		if (profile && profile->width) {
-			scale = (double) *width / profile->width;
-		}
+		double scale = mlt_profile_scale_width(profile, *width);
 		init_image_filtergraph( filter, *format, *width, *height, scale );
 		pdata->reset = 0;
 	}

src/modules/frei0r/frei0r_helper.c
@@ -106,7 +106,7 @@ int process_frei0r_item( mlt_service service, mlt_position position, double time
 	int slice_count = mlt_properties_get(prop, "threads") ? mlt_properties_get_int(prop, "threads") : -1;
 	const char *service_name = mlt_properties_get(prop, "mlt_service");
 	int is_cairoblend = service_name && !strcmp("frei0r.cairoblend", service_name);
-	double scale = mlt_frame_resolution_scale(frame);
+	double scale = mlt_profile_scale_width(mlt_service_profile(service), *width);
 	mlt_properties scale_map = mlt_properties_get_data(prop, "_resolution_scale", NULL);
 	
 	if (slice_count >= 0)

src/modules/gtk2/producer_pango.c
@@ -399,7 +399,8 @@ static void refresh_image( producer_pango self, mlt_frame frame, int width, int 
 	mlt_properties producer_props = MLT_PRODUCER_PROPERTIES( producer );
 
 	// Get producer properties
-	double scale = mlt_frame_resolution_scale(frame);
+	mlt_profile profile = mlt_service_profile(MLT_FILTER_SERVICE(producer));
+	double scale = mlt_profile_scale_width(profile, width);
 	char *fg = mlt_properties_get( producer_props, "fgcolour" );
 	char *bg = mlt_properties_get( producer_props, "bgcolour" );
 	char *ol = mlt_properties_get( producer_props, "olcolour" );

src/modules/kdenlive/filter_boxblur.c
@@ -20,6 +20,7 @@
 
 #include <framework/mlt_filter.h>
 #include <framework/mlt_frame.h>
+#include <framework/mlt_profile.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -114,9 +115,8 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 		factor = mlt_properties_anim_get_double( properties, "blur", position, length );
 	}
 	
-	double resolution_scale = mlt_frame_resolution_scale(frame);
-	boxw = (unsigned int)(factor * hori * resolution_scale);
-	boxh = (unsigned int)(factor * vert * resolution_scale);
+	boxw = (unsigned int)(factor * hori);
+	boxh = (unsigned int)(factor * vert);
 
 	if ( boxw == 0 && boxh == 0 )
 	{
@@ -128,15 +128,20 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 		// Get the image
 		*format = mlt_image_rgb24a;
 		int error = mlt_frame_get_image( frame, image, format, width, height, 1 );
-
+		
 		// Only process if we have no error and a valid colour space
 		if ( error == 0 )
 		{
-			int size = mlt_image_format_size( *format, *width, *height, NULL );
-			int32_t *rgba = mlt_pool_alloc( 4 * size );
-			PreCompute( *image, rgba, *width, *height );
-			DoBoxBlur( *image, rgba, *width, *height, MAX(1, boxw), MAX(1, boxh) );
-			mlt_pool_release( rgba );
+			mlt_profile profile = mlt_service_profile(MLT_FILTER_SERVICE(filter));
+			boxw *= mlt_profile_scale_width(profile, *width);
+			boxh *= mlt_profile_scale_height(profile, *height);
+			if (boxw || boxh) {
+				int size = mlt_image_format_size( *format, *width, *height, NULL );
+				int32_t *rgba = mlt_pool_alloc( 4 * size );
+				PreCompute( *image, rgba, *width, *height );
+				DoBoxBlur( *image, rgba, *width, *height, MAX(1, boxw), MAX(1, boxh) );
+				mlt_pool_release( rgba );
+			}
 		}
 	}
 	return error;

src/modules/kdenlive/filter_wave.c
@@ -20,6 +20,7 @@
 
 #include <framework/mlt_filter.h>
 #include <framework/mlt_frame.h>
+#include <framework/mlt_profile.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -94,9 +95,11 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 		{
 			factor = mlt_properties_anim_get_double( properties, "wave", f_pos, f_len );
 		}
-		factor *= mlt_frame_resolution_scale(frame);
 
-		if (factor != 0) 
+		mlt_profile profile = mlt_service_profile(MLT_FILTER_SERVICE(filter));
+		factor *= mlt_profile_scale_width(profile, *width);
+
+		if (factor > 0.0) 
 		{
 			int image_size = *width * (*height) * 2;
 			uint8_t *dst = mlt_pool_alloc (image_size);

src/modules/oldfilm/filter_lines.c
@@ -19,6 +19,7 @@
 
 #include <framework/mlt_filter.h>
 #include <framework/mlt_frame.h>
+#include <framework/mlt_profile.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -47,8 +48,9 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 		char buf[256];
 		char typebuf[256];
 
-		double scale = mlt_frame_resolution_scale(frame);
-		if (line_width > 1 && scale != 1.0)
+		mlt_profile profile = mlt_service_profile(MLT_FILTER_SERVICE(filter));
+		double scale = mlt_profile_scale_width(profile, *width);
+		if (line_width > 1 && scale > 0.0)
 			line_width = MAX(2, lrint(line_width * scale));
 		
 		if ( line_width < 1 )

src/modules/oldfilm/filter_oldfilm.c
@@ -19,6 +19,7 @@
 
 #include <framework/mlt_filter.h>
 #include <framework/mlt_frame.h>
+#include <framework/mlt_profile.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -78,9 +79,8 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 
 		int diffpic = 0;
 		if ( delta ) {
-			double scale = mlt_frame_resolution_scale(frame);
-			if (scale != 1.0)
-				delta *= scale;
+			mlt_profile profile = mlt_service_profile(MLT_FILTER_SERVICE(filter));
+			delta *= mlt_profile_scale_width(profile, *width);
 			diffpic = rand() % delta * 2 - delta;
 		}
 		int brightdelta = 0;

commit e1067518e06a275578cb7b1e754d1bdb9b083d28

fix #521 bad aspect ratio of B frame with consumer scale
This also fixes distort=1, which broke in 1857e52.

src/modules/plus/transition_affine.c
@@ -544,12 +544,19 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 	}
 
 	// Fetch the b frame image
-	if (scale_width > 0.0 || scale_height > 0.0 || mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale")) {
+	if (mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale")) {
 		// Request b frame image size just what is needed.
 		b_width = result.w;
 		b_height = result.h;
 		// Set the rescale interpolation to match the frame
 		mlt_properties_set( b_props, "rescale.interp", mlt_properties_get( a_props, "rescale.interp" ) );
+	} else if (scale_width != 1.0 || scale_height != 1.0) {
+		// Scale request of b frame image to consumer scale maintaining its aspect ratio.
+		b_height = *height;
+		b_width = b_height * b_dar / b_ar;
+		// Set the rescale interpolation to match the frame
+		mlt_properties_set( b_props, "rescale.interp", mlt_properties_get( a_props, "rescale.interp" ) );
+		mlt_properties_set_int( b_props, "distort", 1 );
 	} else {
 		// Request full resolution of b frame image.
 		mlt_properties_set_int( b_props, "rescale_width", b_width );