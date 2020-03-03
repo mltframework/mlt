mltframework/mlt

commit 2f4764d0afef442d1782a58bb8985feaaf3846bb

convert text services to mlt_properties_set_string()
This is to fix users getting unexpected results when using text starting
with '@'.

src/modules/plus/filter_dynamictext.c
@@ -1,6 +1,6 @@
 /*
  * filter_dynamictext.c -- dynamic text overlay filter
- * Copyright (C) 2011-2019 Meltytech, LLC
+ * Copyright (C) 2011-2020 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -249,7 +249,7 @@ static mlt_frame filter_process( mlt_filter filter, mlt_frame frame )
 	// Apply keyword substitution before passing the text to the filter.
 	char* result = calloc( 1, MAX_TEXT_LEN );
 	substitute_keywords( filter, result, dynamic_text, frame );
-	mlt_properties_set( text_filter_properties, "argument", result );
+	mlt_properties_set_string( text_filter_properties, "argument", dynamic_text );
 	free( result );
 	mlt_properties_pass_list( text_filter_properties, properties,
 		"geometry family size weight style fgcolour bgcolour olcolour pad halign valign outline" );
@@ -278,19 +278,19 @@ mlt_filter filter_dynamictext_init( mlt_profile profile, mlt_service_type type, 
 		mlt_properties_set_data( my_properties, "_text_filter", text_filter, 0, ( mlt_destructor )mlt_filter_close, NULL );
 
 		// Assign default values
-		mlt_properties_set( my_properties, "argument", arg ? arg: "#timecode#" );
-		mlt_properties_set( my_properties, "geometry", "0%/0%:100%x100%:100%" );
-		mlt_properties_set( my_properties, "family", "Sans" );
-		mlt_properties_set( my_properties, "size", "48" );
-		mlt_properties_set( my_properties, "weight", "400" );
-		mlt_properties_set( my_properties, "style", "normal" );
-		mlt_properties_set( my_properties, "fgcolour", "0x000000ff" );
-		mlt_properties_set( my_properties, "bgcolour", "0x00000020" );
-		mlt_properties_set( my_properties, "olcolour", "0x00000000" );
-		mlt_properties_set( my_properties, "pad", "0" );
-		mlt_properties_set( my_properties, "halign", "left" );
-		mlt_properties_set( my_properties, "valign", "top" );
-		mlt_properties_set( my_properties, "outline", "0" );
+		mlt_properties_set_string( my_properties, "argument", arg ? arg: "#timecode#" );
+		mlt_properties_set_string( my_properties, "geometry", "0%/0%:100%x100%:100%" );
+		mlt_properties_set_string( my_properties, "family", "Sans" );
+		mlt_properties_set_string( my_properties, "size", "48" );
+		mlt_properties_set_string( my_properties, "weight", "400" );
+		mlt_properties_set_string( my_properties, "style", "normal" );
+		mlt_properties_set_string( my_properties, "fgcolour", "0x000000ff" );
+		mlt_properties_set_string( my_properties, "bgcolour", "0x00000020" );
+		mlt_properties_set_string( my_properties, "olcolour", "0x00000000" );
+		mlt_properties_set_string( my_properties, "pad", "0" );
+		mlt_properties_set_string( my_properties, "halign", "left" );
+		mlt_properties_set_string( my_properties, "valign", "top" );
+		mlt_properties_set_string( my_properties, "outline", "0" );
 		mlt_properties_set_int( my_properties, "_filter_private", 1 );
 
 		filter->process = filter_process;

src/modules/gtk2/producer_pango.c
@@ -185,18 +185,18 @@ mlt_producer producer_pango_init( const char *filename )
 		mlt_events_listen( properties, producer, "fontmap-reload", (mlt_listener) on_fontmap_reload );
 
 		// Set the default properties
-		mlt_properties_set( properties, "fgcolour", "0xffffffff" );
-		mlt_properties_set( properties, "bgcolour", "0x00000000" );
-		mlt_properties_set( properties, "olcolour", "0x00000000" );
+		mlt_properties_set_string( properties, "fgcolour", "0xffffffff" );
+		mlt_properties_set_string( properties, "bgcolour", "0x00000000" );
+		mlt_properties_set_string( properties, "olcolour", "0x00000000" );
 		mlt_properties_set_int( properties, "align", pango_align_left );
 		mlt_properties_set_int( properties, "pad", 0 );
 		mlt_properties_set_int( properties, "outline", 0 );
-		mlt_properties_set( properties, "text", "" );
-		mlt_properties_set( properties, "font", NULL );
-		mlt_properties_set( properties, "family", "Sans" );
+		mlt_properties_set_string( properties, "text", "" );
+		mlt_properties_set_string( properties, "font", NULL );
+		mlt_properties_set_string( properties, "family", "Sans" );
 		mlt_properties_set_int( properties, "size", 48 );
-		mlt_properties_set( properties, "style", "normal" );
-		mlt_properties_set( properties, "encoding", "UTF-8" );
+		mlt_properties_set_string( properties, "style", "normal" );
+		mlt_properties_set_string( properties, "encoding", "UTF-8" );
 		mlt_properties_set_int( properties, "weight", PANGO_WEIGHT_NORMAL );
 		mlt_properties_set_int( properties, "stretch", PANGO_STRETCH_NORMAL + 1 );
 		mlt_properties_set_int( properties, "rotate", 0 );
@@ -207,7 +207,7 @@ mlt_producer producer_pango_init( const char *filename )
 			// workaround for old kdenlive countdown generator
 			|| strstr( filename, "&lt;producer&gt;" ) ) ) )
 		{
-			mlt_properties_set( properties, "markup", "" );
+			mlt_properties_set_string( properties, "markup", "" );
 		}
 		else if ( filename[ 0 ] == '+' || strstr( filename, "/+" ) )
 		{
@@ -219,8 +219,8 @@ mlt_producer producer_pango_init( const char *filename )
 				( *strrchr( markup, '.' ) ) = '\0';
 			while ( strchr( markup, '~' ) )
 				( *strchr( markup, '~' ) ) = '\n';
-			mlt_properties_set( properties, "resource", filename );
-			mlt_properties_set( properties, "markup", markup );
+			mlt_properties_set_string( properties, "resource", filename );
+			mlt_properties_set_string( properties, "markup", markup );
 			free( copy );
 		}
 		else if ( strstr( filename, ".mpl" ) ) 
@@ -230,13 +230,13 @@ mlt_producer producer_pango_init( const char *filename )
 			mlt_properties contents = mlt_properties_load( filename );
 			mlt_geometry key_frames = mlt_geometry_init( );
 			struct mlt_geometry_item_s item;
-			mlt_properties_set( properties, "resource", filename );
+			mlt_properties_set_string( properties, "resource", filename );
 			mlt_properties_set_data( properties, "contents", contents, 0, ( mlt_destructor )mlt_properties_close, NULL );
 			mlt_properties_set_data( properties, "key_frames", key_frames, 0, ( mlt_destructor )mlt_geometry_close, NULL );
 
 			// Make sure we have at least one entry
 			if ( mlt_properties_get( contents, "0" ) == NULL )
-				mlt_properties_set( contents, "0", "" );
+				mlt_properties_set_string( contents, "0", "" );
 
 			for ( i = 0; i < mlt_properties_count( contents ); i ++ )
 			{
@@ -254,7 +254,7 @@ mlt_producer producer_pango_init( const char *filename )
 		}
 		else
 		{
-			mlt_properties_set( properties, "resource", filename );
+			mlt_properties_set_string( properties, "resource", filename );
 			FILE *f = mlt_fopen( filename, "r" );
 			if ( f != NULL )
 			{
@@ -283,9 +283,9 @@ mlt_producer producer_pango_init( const char *filename )
 					markup[ strlen( markup ) - 1 ] = '\0';
 
 				if ( markup )
-					mlt_properties_set( properties, "markup", markup );
+					mlt_properties_set_string( properties, "markup", markup );
 				else
-					mlt_properties_set( properties, "markup", "" );
+					mlt_properties_set_string( properties, "markup", "" );
 				free( markup );
 			}
 			else
@@ -373,9 +373,9 @@ static int iconv_utf8( mlt_properties properties, const char *prop_name, const c
 		memset( outbuf, 0, outbuf_n );
 
 		if ( text != NULL && strcmp( text, "" ) && iconv( cd, &inbuf_p, &inbuf_n, &outbuf_p, &outbuf_n ) != -1 )
-			mlt_properties_set( properties, prop_name, outbuf );
+			mlt_properties_set_string( properties, prop_name, outbuf );
 		else
-			mlt_properties_set( properties, prop_name, "" );
+			mlt_properties_set_string( properties, prop_name, "" );
 
 		mlt_pool_release( outbuf );
 		result = 0;

src/modules/plus/filter_text.c
@@ -45,16 +45,16 @@ static void setup_producer( mlt_producer producer, mlt_properties my_properties 
 	mlt_properties producer_properties = MLT_PRODUCER_PROPERTIES( producer );
 
 	// Pass the properties to the text producer
-	mlt_properties_set( producer_properties, "family", mlt_properties_get( my_properties, "family" ) );
-	mlt_properties_set( producer_properties, "size", mlt_properties_get( my_properties, "size" ) );
-	mlt_properties_set( producer_properties, "weight", mlt_properties_get( my_properties, "weight" ) );
-	mlt_properties_set( producer_properties, "style", mlt_properties_get( my_properties, "style" ) );
-	mlt_properties_set( producer_properties, "fgcolour", mlt_properties_get( my_properties, "fgcolour" ) );
-	mlt_properties_set( producer_properties, "bgcolour", mlt_properties_get( my_properties, "bgcolour" ) );
-	mlt_properties_set( producer_properties, "olcolour", mlt_properties_get( my_properties, "olcolour" ) );
-	mlt_properties_set( producer_properties, "pad", mlt_properties_get( my_properties, "pad" ) );
-	mlt_properties_set( producer_properties, "outline", mlt_properties_get( my_properties, "outline" ) );
-	mlt_properties_set( producer_properties, "align", mlt_properties_get( my_properties, "halign" ) );
+	mlt_properties_set_string( producer_properties, "family", mlt_properties_get( my_properties, "family" ) );
+	mlt_properties_set_string( producer_properties, "size", mlt_properties_get( my_properties, "size" ) );
+	mlt_properties_set_string( producer_properties, "weight", mlt_properties_get( my_properties, "weight" ) );
+	mlt_properties_set_string( producer_properties, "style", mlt_properties_get( my_properties, "style" ) );
+	mlt_properties_set_string( producer_properties, "fgcolour", mlt_properties_get( my_properties, "fgcolour" ) );
+	mlt_properties_set_string( producer_properties, "bgcolour", mlt_properties_get( my_properties, "bgcolour" ) );
+	mlt_properties_set_string( producer_properties, "olcolour", mlt_properties_get( my_properties, "olcolour" ) );
+	mlt_properties_set_string( producer_properties, "pad", mlt_properties_get( my_properties, "pad" ) );
+	mlt_properties_set_string( producer_properties, "outline", mlt_properties_get( my_properties, "outline" ) );
+	mlt_properties_set_string( producer_properties, "align", mlt_properties_get( my_properties, "halign" ) );
 }
 
 static void setup_transition( mlt_filter filter, mlt_transition transition, mlt_frame frame, mlt_properties my_properties )
@@ -73,8 +73,8 @@ static void setup_transition( mlt_filter filter, mlt_transition transition, mlt_
 		rect.h *= profile->height;
 	}
 	mlt_properties_set_rect( transition_properties, "rect", rect );
-	mlt_properties_set( transition_properties, "halign", mlt_properties_get( my_properties, "halign" ) );
-	mlt_properties_set( transition_properties, "valign", mlt_properties_get( my_properties, "valign" ) );
+	mlt_properties_set_string( transition_properties, "halign", mlt_properties_get( my_properties, "halign" ) );
+	mlt_properties_set_string( transition_properties, "valign", mlt_properties_get( my_properties, "valign" ) );
 	mlt_service_unlock( MLT_TRANSITION_SERVICE(transition) );
 }
 
@@ -107,7 +107,7 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 		setup_producer( producer, properties );
 		setup_transition( filter, transition, frame, properties );
 	}
-	mlt_properties_set( MLT_PRODUCER_PROPERTIES( producer ), "text", argument );
+	mlt_properties_set_string( MLT_PRODUCER_PROPERTIES( producer ), "text", argument );
 
 	// Make sure the producer is in the correct position
 	position = mlt_filter_get_position( filter, frame );
@@ -200,25 +200,25 @@ mlt_filter filter_text_init( mlt_profile profile, mlt_service_type type, const c
 		mlt_properties_set_data( my_properties, "_producer", producer, 0, ( mlt_destructor )mlt_producer_close, NULL );
 
 		// Ensure that we loop
-		mlt_properties_set( MLT_PRODUCER_PROPERTIES( producer ), "eof", "loop" );
+		mlt_properties_set_string( MLT_PRODUCER_PROPERTIES( producer ), "eof", "loop" );
 
 		// Listen for property changes.
 		mlt_events_listen( MLT_FILTER_PROPERTIES(filter), filter, "property-changed", (mlt_listener)property_changed );
 
 		// Assign default values
-		mlt_properties_set( my_properties, "argument", arg ? arg: "text" );
-		mlt_properties_set( my_properties, "geometry", "0%/0%:100%x100%:100%" );
-		mlt_properties_set( my_properties, "family", "Sans" );
-		mlt_properties_set( my_properties, "size", "48" );
-		mlt_properties_set( my_properties, "weight", "400" );
-		mlt_properties_set( my_properties, "style", "normal" );
-		mlt_properties_set( my_properties, "fgcolour", "0x000000ff" );
-		mlt_properties_set( my_properties, "bgcolour", "0x00000020" );
-		mlt_properties_set( my_properties, "olcolour", "0x00000000" );
-		mlt_properties_set( my_properties, "pad", "0" );
-		mlt_properties_set( my_properties, "halign", "left" );
-		mlt_properties_set( my_properties, "valign", "top" );
-		mlt_properties_set( my_properties, "outline", "0" );
+		mlt_properties_set_string( my_properties, "argument", arg ? arg: "text" );
+		mlt_properties_set_string( my_properties, "geometry", "0%/0%:100%x100%:100%" );
+		mlt_properties_set_string( my_properties, "family", "Sans" );
+		mlt_properties_set_string( my_properties, "size", "48" );
+		mlt_properties_set_string( my_properties, "weight", "400" );
+		mlt_properties_set_string( my_properties, "style", "normal" );
+		mlt_properties_set_string( my_properties, "fgcolour", "0x000000ff" );
+		mlt_properties_set_string( my_properties, "bgcolour", "0x00000020" );
+		mlt_properties_set_string( my_properties, "olcolour", "0x00000000" );
+		mlt_properties_set_string( my_properties, "pad", "0" );
+		mlt_properties_set_string( my_properties, "halign", "left" );
+		mlt_properties_set_string( my_properties, "valign", "top" );
+		mlt_properties_set_string( my_properties, "outline", "0" );
 		mlt_properties_set_int( my_properties, "_reset", 1 );
 		mlt_properties_set_int( my_properties, "_filter_private", 1 );
 

src/modules/qt/filter_qtext.cpp
@@ -333,19 +333,19 @@ mlt_filter filter_qtext_init( mlt_profile profile, mlt_service_type type, const 
 
 	mlt_properties filter_properties = MLT_FILTER_PROPERTIES( filter );
 	// Assign default values
-	mlt_properties_set( filter_properties, "argument", arg ? arg: "text" );
-	mlt_properties_set( filter_properties, "geometry", "0%/0%:100%x100%:100%" );
-	mlt_properties_set( filter_properties, "family", "Sans" );
-	mlt_properties_set( filter_properties, "size", "48" );
-	mlt_properties_set( filter_properties, "weight", "400" );
-	mlt_properties_set( filter_properties, "style", "normal" );
-	mlt_properties_set( filter_properties, "fgcolour", "0x000000ff" );
-	mlt_properties_set( filter_properties, "bgcolour", "0x00000020" );
-	mlt_properties_set( filter_properties, "olcolour", "0x00000000" );
-	mlt_properties_set( filter_properties, "pad", "0" );
-	mlt_properties_set( filter_properties, "halign", "left" );
-	mlt_properties_set( filter_properties, "valign", "top" );
-	mlt_properties_set( filter_properties, "outline", "0" );
+	mlt_properties_set_string( filter_properties, "argument", arg ? arg: "text" );
+	mlt_properties_set_string( filter_properties, "geometry", "0%/0%:100%x100%:100%" );
+	mlt_properties_set_string( filter_properties, "family", "Sans" );
+	mlt_properties_set_string( filter_properties, "size", "48" );
+	mlt_properties_set_string( filter_properties, "weight", "400" );
+	mlt_properties_set_string( filter_properties, "style", "normal" );
+	mlt_properties_set_string( filter_properties, "fgcolour", "0x000000ff" );
+	mlt_properties_set_string( filter_properties, "bgcolour", "0x00000020" );
+	mlt_properties_set_string( filter_properties, "olcolour", "0x00000000" );
+	mlt_properties_set_string( filter_properties, "pad", "0" );
+	mlt_properties_set_string( filter_properties, "halign", "left" );
+	mlt_properties_set_string( filter_properties, "valign", "top" );
+	mlt_properties_set_string( filter_properties, "outline", "0" );
 	mlt_properties_set_int( filter_properties, "_filter_private", 1 );
 
 	return filter;

commit 0faaa180db0f77bc30dd49786773b593515d505e

fix substitution regression in 2f4764d

src/modules/plus/filter_dynamictext.c
@@ -249,7 +249,7 @@ static mlt_frame filter_process( mlt_filter filter, mlt_frame frame )
 	// Apply keyword substitution before passing the text to the filter.
 	char* result = calloc( 1, MAX_TEXT_LEN );
 	substitute_keywords( filter, result, dynamic_text, frame );
-	mlt_properties_set_string( text_filter_properties, "argument", dynamic_text );
+	mlt_properties_set_string( text_filter_properties, "argument", result );
 	free( result );
 	mlt_properties_pass_list( text_filter_properties, properties,
 		"geometry family size weight style fgcolour bgcolour olcolour pad halign valign outline" );