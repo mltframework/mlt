f58b44d73442986eeffec7431e59b7d19d214c1b
Heiko Becker <heirecka@exherbo.org>
2020-03-24 21:17:05 +0100

    Fix build with Qt 5.15.0
    QPainterPath is no longer included via qtransform.h (since
    5.15.0-beta2, 50d2acdc93b4de2ba56eb67787e2bdcb21dd4bea in qtbase.git).

--- src/modules/qt/filter_qtext.cpp
+++ src/modules/qt/filter_qtext.cpp
@@ -21,6 +21,7 @@
 #include <framework/mlt.h>
 #include <framework/mlt_log.h>
 #include <QPainter>
+#include <QPainterPath>
 #include <QString>
 
 static QRectF get_text_path( QPainterPath* qpath, mlt_properties filter_properties, const char* text, double scale )
