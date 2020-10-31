mltframework/mlt

commit 6a856fdcd77834bb5c5358b840d3aa540b98c51f

fix #590 bad interpolation when scaling with affine rect
We now request upstream to supply a scaled image unless not using fill or
distort modes and the image is smaller than the rect (unscaled use
case). The overall strategy is not scale with a dedicated image scaler
and use affine with sub-pixel precision via interpolation for positioning,
rotation, and shear.

src/modules/plus/transition_affine.c
@@ -540,8 +540,10 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 
 	double geometry_w = result.w;
 	double geometry_h = result.h;
+	int fill = mlt_properties_get_int( properties, "fill" );
+	int distort = mlt_properties_get_int( properties, "distort" );
 
-	if ( !mlt_properties_get_int( properties, "fill" ) )
+	if ( !fill )
 	{
 		double geometry_dar = result.w * consumer_ar / result.h;
 
@@ -558,22 +560,19 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 	}
 
 	// Fetch the b frame image
-	if (mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale")) {
+	if (fill || distort || b_width > result.w || b_height > result.h || scale_width != 1.0 || scale_height != 1.0
+			|| mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale")) {
 		// Request b frame image size just what is needed.
-		b_width = result.w;
 		b_height = result.h;
+		b_width  = b_height * b_dar / b_ar;
 		// Set the rescale interpolation to match the frame
 		mlt_properties_set( b_props, "rescale.interp", mlt_properties_get( a_props, "rescale.interp" ) );
-		mlt_properties_set_int( b_props, "distort", 1 );
-	} else if (scale_width != 1.0 || scale_height != 1.0) {
-		// Scale request of b frame image to consumer scale maintaining its aspect ratio.
-		b_height = *height;
-		b_width = b_height * b_dar / b_ar;
-		// Set the rescale interpolation to match the frame
-		mlt_properties_set( b_props, "rescale.interp", mlt_properties_get( a_props, "rescale.interp" ) );
+		// Disable padding (resize filter)
 		mlt_properties_set_int( b_props, "distort", 1 );
 	} else {
-		// Request full resolution of b frame image.
+		// Request at resolution of b frame image. This only happens when not using fill or distort mode
+		// and the image is smaller than the rect with the intention to prevent scaling of the
+		// image and merely position and possibly transform.
 		mlt_properties_set_int( b_props, "rescale_width", b_width );
 		mlt_properties_set_int( b_props, "rescale_height", b_height );
 
@@ -588,6 +587,7 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 			mlt_properties_set( b_props, "rescale.interp", "none" );
 		}
 	}
+	mlt_log_debug(MLT_TRANSITION_SERVICE(transition), "requesting image B at resolution %dx%d\n", b_width, b_height);
 
 	// This is not a field-aware transform.
 	mlt_properties_set_int( b_props, "consumer_deinterlace", 1 );
@@ -659,7 +659,7 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 		}
 
 		// Factor scaling into the transformation based on output resolution.
-		if ( mlt_properties_get_int( properties, "distort" ) )
+		if ( distort )
 		{
 			scale_x = geom_scale_x * ( scale_x == 0 ? 1 : scale_x );
 			scale_y = geom_scale_y * ( scale_y == 0 ? 1 : scale_y );

commit eaf46b3fda159af847f411cb28ffce0233ce0e37

address some regressions in 6a856fdc
See https://forum.shotcut.org/t/text-rich-clashes-with-size-position-
rotates-zoom/22134/

I also found a regression in that case when using preview scaling.
However, I am keeping the new behavior on non-color clips. People that
want to do motion effects on rich text can use a transparent color clip.

src/modules/core/producer_colour.c
@@ -1,6 +1,6 @@
 /*
  * producer_colour.c
- * Copyright (C) 2003-2019 Meltytech, LLC
+ * Copyright (C) 2003-2020 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -255,6 +255,10 @@ static int producer_get_frame( mlt_producer producer, mlt_frame_ptr frame, int i
 		// Push the get_image method
 		mlt_frame_push_service( *frame, producer );
 		mlt_frame_push_get_image( *frame, producer_get_image );
+
+		// A hint to scalers and affine transition that this producer does not
+		// benefit from interpolation.
+		mlt_properties_set_int(properties, "interpolation_not_required", 1);
 	}
 
 	// Calculate the next timecode

src/modules/plus/transition_affine.c
@@ -562,9 +562,18 @@ static int transition_get_image( mlt_frame a_frame, uint8_t **image, mlt_image_f
 	}
 
 	// Fetch the b frame image
-	if (fill || distort || b_width > result.w || b_height > result.h || scale_width != 1.0 || scale_height != 1.0
-			|| mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale")) {
-		// Request b frame image size just what is needed.
+	if (scale_width != 1.0 || scale_height != 1.0) {
+			// Scale request of b frame image to consumer scale maintaining its aspect ratio.
+			b_height = CLAMP(*height, 1, MLT_AFFINE_MAX_DIMENSION);
+			b_width  = CLAMP(b_height * b_dar / b_ar, 1, MLT_AFFINE_MAX_DIMENSION);
+			// Set the rescale interpolation to match the frame
+			mlt_properties_set( b_props, "rescale.interp", mlt_properties_get( a_props, "rescale.interp" ) );
+			// Disable padding (resize filter)
+			mlt_properties_set_int( b_props, "distort", 1 );
+	} else if (!mlt_properties_get_int(b_props, "interpolation_not_required")
+			   && (fill || distort || b_width > result.w || b_height > result.h
+				   || mlt_properties_get_int(properties, "b_scaled") || mlt_properties_get_int(b_props, "always_scale"))) {
+		// Request b frame image scaled to what is needed.
 		b_height = CLAMP(result.h, 1, MLT_AFFINE_MAX_DIMENSION);
 		b_width  = CLAMP(b_height * b_dar / b_ar, 1, MLT_AFFINE_MAX_DIMENSION);
 		// Set the rescale interpolation to match the frame