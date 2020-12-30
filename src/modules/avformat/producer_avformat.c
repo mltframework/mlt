mltframework/mlt

commit 42622ceccab0fa60fc48170a44b706be3733e4fc

fix a memory leak when using avcodec_receive_frame()
Also, fixes possibly using an unitialized codec_params.

src/modules/avformat/producer_avformat.c
@@ -1579,15 +1579,16 @@ static int producer_get_image( mlt_frame frame, uint8_t **buffer, mlt_image_form
 
 	// Fetch the video format context
 	AVFormatContext *context = self->video_format;
+	AVCodecParameters *codec_params = NULL;
 	if ( !context )
 		goto exit_get_image;
 
 	// Get the video stream
 	AVStream *stream = context->streams[ self->video_index ];
+	codec_params = stream->codecpar;
 
 	// Get codec context
 	AVCodecContext *codec_context = stream->codec;
-	AVCodecParameters *codec_params = stream->codecpar;
 
 	// Always use the image cache for album art.
 	int is_album_art = (codec_params->codec_id == AV_CODEC_ID_MJPEG
@@ -1951,12 +1952,13 @@ exit_get_image:
 	// Set the progressive flag
 	if ( mlt_properties_get( properties, "force_progressive" ) ) {
 		mlt_properties_set_int( frame_properties, "progressive", !!mlt_properties_get_int( properties, "force_progressive" ) );
-	} else if ( self->video_frame ) {
+	} else if ( self->video_frame && codec_params ) {
 		mlt_properties_set_int( frame_properties, "progressive",
 			!self->video_frame->interlaced_frame &&
 				(codec_params->field_order == AV_FIELD_PROGRESSIVE ||
 				 codec_params->field_order == AV_FIELD_UNKNOWN) );
 	}
+	av_frame_unref(self->video_frame);
 
 	// Set the field order property for this frame
 	if ( mlt_properties_get( properties, "force_tff" ) )

commit 299939b1751251b7785a85fb1abd19f556dde159

fix a/v sync and video rate regression in 42622ce

src/modules/avformat/producer_avformat.c
@@ -1810,6 +1810,8 @@ static int producer_get_image( mlt_frame frame, uint8_t **buffer, mlt_image_form
 		// Construct an AVFrame for YUV422 conversion
 		if ( !self->video_frame )
 			self->video_frame = av_frame_alloc();
+		else
+			av_frame_unref( self->video_frame );
 #if USE_HWACCEL
 		if ( !self->sw_video_frame )
 			self->sw_video_frame = av_frame_alloc();
@@ -2081,6 +2083,9 @@ static int producer_get_image( mlt_frame frame, uint8_t **buffer, mlt_image_form
 	self->video_expected = position + 1;
 
 exit_get_image:
+#if USE_HWACCEL
+	av_frame_unref( self->sw_video_frame );
+#endif
 	pthread_mutex_unlock( &self->video_mutex );
 
 	// Set the progressive flag
@@ -2092,10 +2097,7 @@ exit_get_image:
 				(codec_params->field_order == AV_FIELD_PROGRESSIVE ||
 				 codec_params->field_order == AV_FIELD_UNKNOWN) );
 	}
-	av_frame_unref( self->video_frame );
-#if USE_HWACCEL
-	av_frame_unref( self->sw_video_frame );
-#endif
+
 	// Set the field order property for this frame
 	if ( mlt_properties_get( properties, "force_tff" ) )
 		mlt_properties_set_int( frame_properties, "top_field_first", !!mlt_properties_get_int( properties, "force_tff" ) );