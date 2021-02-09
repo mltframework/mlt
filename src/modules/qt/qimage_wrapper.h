dd69f0cefef28a7bd822bfc695fe623e649746c6
Brian Matherly <code@brianmatherly.com>
2021-02-08 22:06:10 -0600

    Remove deprecated gtk2 module

--- src/modules/qt/qimage_wrapper.h
+++ src/modules/qt/qimage_wrapper.h
@@ -1,9 +1,6 @@
 /*
  * qimage_wrapper.h -- a QT/QImage based producer for MLT
  *
- * NB: This module is designed to be functionally equivalent to the 
- * gtk2 image loading module so it can be used as replacement.
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
