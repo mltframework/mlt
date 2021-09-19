925995fdabb405a3bbfab09d3f78e5d9e5c31436
Jean-Baptiste Mardelle <jb@kdenlive.org>
2021-09-19 07:38:54 +0200

    Opencv.tracker: make pixelate an option of blur type instead of its own parameter

--- src/modules/opencv/filter_opencv_tracker.cpp
+++ src/modules/opencv/filter_opencv_tracker.cpp
@@ -400,12 +400,11 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 	mlt_position position = mlt_filter_get_position( filter, frame );
 	mlt_service_lock( MLT_FILTER_SERVICE( filter ) );
 	int shape_width = mlt_properties_get_int( filter_properties, "shape_width" );
-	int pixelate = mlt_properties_get_int( filter_properties, "pixelate" );
 	int blur = mlt_properties_get_int( filter_properties, "blur" );
 	cv::Mat cvFrame;
 
 	private_data* data = (private_data*) filter->child;
-	if ( shape_width == 0 && blur == 0 && !data->playback && pixelate == 0) {
+	if ( shape_width == 0 && blur == 0 && !data->playback ) {
 		error = mlt_frame_get_image( frame, image, format, width, height, 1 );
 		mlt_service_unlock( MLT_FILTER_SERVICE( filter ) );
 		return error;
@@ -477,7 +476,21 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 			case 1:
 				// Gaussian Blur
 				cv::GaussianBlur( cvFrame( data->boundingBox ), cvFrame( data->boundingBox ), cv::Size( 0, 0 ), blur );
-                                break;
+				break;
+			case 2:
+				// Pixelate
+				{
+					cv::Mat roi = cvFrame( data->boundingBox );
+					cv::Mat res;
+					cv::resize( roi, res, cv::Size( MAX( 2, data->boundingBox.width / blur ), MAX( 2, data->boundingBox.height / blur )), cv::INTER_NEAREST );
+					cv::resize( res, roi, cv::Size( data->boundingBox.width, data->boundingBox.height ), 0, 0, cv::INTER_NEAREST );
+					cvFrame( data->boundingBox ) = roi;
+				}
+				break;
+			case 3:
+				// Opaque fill, handled in shape_width option
+				shape_width = -1;
+				break;
 			case 0:
 			default:
 				// Median Blur
@@ -490,15 +503,6 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 				cv::medianBlur( cvFrame( data->boundingBox ), cvFrame( data->boundingBox ), blur );
 				break;
 		}
-	}
-
-	if ( pixelate > 0 )
-	{
-		cv::Mat roi = cvFrame( data->boundingBox );
-		cv::Mat res;
-		cv::resize( roi, res, cv::Size( MAX( 2, data->boundingBox.width / pixelate ), MAX( 2, data->boundingBox.height / pixelate )), cv::INTER_NEAREST );
-		cv::resize( res, roi, cv::Size( data->boundingBox.width, data->boundingBox.height ), 0, 0, cv::INTER_NEAREST );
-		cvFrame( data->boundingBox ) = roi;
 	}
 
 	// Paint overlay shape
