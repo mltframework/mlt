08621e53d7b2a3d132b73443e65e002218f7dc8b
Jean-Baptiste Mardelle <jb@kdenlive.org>
2022-01-14 14:02:46 +0100

    Fix missing header in previous commit

--- src/modules/qt/qimage_wrapper.h
+++ src/modules/qt/qimage_wrapper.h
@@ -54,6 +54,7 @@ extern void refresh_image( producer_qimage, mlt_frame, mlt_image_format, int wid
 extern void make_tempfile( producer_qimage, const char *xml );
 extern int init_qimage(mlt_producer producer, const char *filename);
 extern int load_sequence_sprintf( producer_qimage self, mlt_properties properties, const char *filename );
+extern int load_folder( producer_qimage self, const char *filename );
 
 
 #ifdef __cplusplus
