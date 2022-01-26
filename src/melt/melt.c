8fda8fcecc76b6312bf3975822da8278b949ac07
Dan Dennedy <dan@dennedy.org>
2022-01-26 10:18:25 -0800

    extend copyright year on user-facing elements

--- src/melt/melt.c
+++ src/melt/melt.c
@@ -884,7 +884,7 @@ query_all:
 		else if ( !strcmp( argv[ i ], "-version" ) || !strcmp( argv[ i ], "--version" ) )
 		{
 			fprintf( stdout, "%s " VERSION "\n"
-				"Copyright (C) 2002-2021 Meltytech, LLC\n"
+				"Copyright (C) 2002-2022 Meltytech, LLC\n"
 				"<https://www.mltframework.org/>\n"
 				"This is free software; see the source for copying conditions.  There is NO\n"
 				"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n",
