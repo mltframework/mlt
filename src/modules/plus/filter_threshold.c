88cf34bedc93bc707cc8297064faffc3ea8e1fc3
Brian Matherly <code@brianmatherly.com>
2022-03-14 21:17:38 -0500

    Add slice threading to threshold filter

--- src/modules/plus/filter_threshold.c
+++ src/modules/plus/filter_threshold.c
@@ -22,6 +22,69 @@
 #include <framework/mlt_factory.h>
 #include <framework/mlt_frame.h>
 #include <framework/mlt_producer.h>
+#include <framework/mlt_slices.h>
+
+typedef struct {
+	int midpoint;
+	int use_alpha;
+	int invert;
+	int full_luma;
+	uint8_t* image;
+	uint8_t* alpha;
+	int width;
+	int height;
+} slice_desc;
+
+static int do_slice_proc(int id, int index, int jobs, void* data)
+{
+	(void) id; // unused
+	slice_desc* desc = (slice_desc*) data;
+	int slice_line_start, slice_height = mlt_slices_size_slice(jobs, index, desc->height, &slice_line_start);
+	int slice_line_end = slice_line_start + slice_height;
+	int size = desc->width * slice_height * 2;
+	uint8_t white = desc->full_luma? 255 : 235;
+	uint8_t black = desc->full_luma? 0 : 16;
+	uint8_t A = desc->invert? white : black;
+	uint8_t B = desc->invert? black : white;
+	uint8_t* p = desc->image + (slice_line_start * desc->width * 2);
+	int i = 0;
+
+	if ( !desc->use_alpha )
+	{
+		for (i = 0; i < size; i += 2)
+		{
+			if ( p[i] < desc->midpoint )
+				p[i] = A;
+			else
+				p[i] = B;
+			p[i+1] = 128;
+		}
+	}
+	else
+	{
+
+		if ( desc->alpha )
+		{
+			uint8_t *alpha = desc->alpha + (slice_line_start * desc->width);
+			for (i = 0; i < size; i += 2)
+			{
+				if ( alpha[i/2] < desc->midpoint )
+					p[i] = A;
+				else
+					p[i] = B;
+				p[i+1] = 128;
+			}
+		}
+		else
+		{
+			for (i = 0; i < size; i += 2)
+			{
+				p[i] = B;
+				p[i+1] = 128;
+			}
+		}
+	}
+}
 
 /** Get the images and apply the luminance of the mask to the alpha of the frame.
 */
@@ -34,54 +97,23 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
 	*format = mlt_image_yuv422;
 	if ( mlt_frame_get_image( frame, image, format, width, height, writable ) == 0 )
 	{
+		slice_desc desc;
 		mlt_properties properties = mlt_filter_properties(filter);
 		mlt_position position = mlt_filter_get_position(filter, frame);
 		mlt_position length = mlt_filter_get_length2(filter, frame);
-		int midpoint = mlt_properties_anim_get_int(properties, "midpoint", position, length);
-		int use_alpha = mlt_properties_get_int(properties, "use_alpha");
-		int invert = mlt_properties_get_int(properties, "invert");
-		int full_luma = mlt_properties_get_int(MLT_FRAME_PROPERTIES(frame), "full_luma");
-		uint8_t white = full_luma? 255 : 235;
-		uint8_t black = full_luma? 0 : 16;
-		uint8_t *p = *image;
-		uint8_t A = invert? white : black;
-		uint8_t B = invert? black : white;
-		int size = *width * *height + 1;
-
-		if ( !use_alpha )
+		desc.midpoint = mlt_properties_anim_get_int(properties, "midpoint", position, length);
+		desc.use_alpha = mlt_properties_get_int(properties, "use_alpha");
+		desc.invert = mlt_properties_get_int(properties, "invert");
+		desc.full_luma = mlt_properties_get_int(MLT_FRAME_PROPERTIES(frame), "full_luma");
+		desc.image = *image;
+		desc.alpha = NULL;
+		desc.width = *width;
+		desc.height = *height;
+		if ( desc.use_alpha )
 		{
-			while (--size)
-			{
-				if ( *p < midpoint )
-					*p ++ = A;
-				else
-					*p ++ = B;
-				*p ++ = 128;
-			}
+			desc.alpha = mlt_frame_get_alpha( frame );
 		}
-		else
-		{
-			uint8_t *alpha = mlt_frame_get_alpha( frame );
-			if ( alpha )
-			{
-				while (--size)
-				{
-					if ( *alpha ++ < midpoint )
-						*p ++ = A;
-					else
-						*p ++ = B;
-					*p ++ = 128;
-				}
-			}
-			else
-			{
-				while (--size)
-				{
-					*p ++ = B;
-					*p ++ = 128;
-				}
-			}
-		}
+		mlt_slices_run_normal(0, do_slice_proc, &desc);
 	}
 
 	return 0;
