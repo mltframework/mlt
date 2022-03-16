9060cb63777539c2020239128ef1b25dab7c27e4
Dan Dennedy <dan@dennedy.org>
2022-03-16 16:24:49 -0700

    refactor fast_rand() into common code

---
+++ src/modules/oldfilm/common.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2022 Meltytech, LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef struct
+{
+	unsigned int x;
+	unsigned int y;
+} oldfilm_rand_seed;
+
+void oldfilm_init_seed( oldfilm_rand_seed* seed, int init );
+int oldfilm_fast_rand( oldfilm_rand_seed* seed );
+
+#endif // COMMON_H
