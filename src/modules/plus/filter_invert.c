cb26fcebc1edb39d996612a3c30a65e2651692ac
Brian Matherly <code@brianmatherly.com>
2022-03-16 21:38:26 -0500

    Add slice processing to invert filter

--- src/modules/plus/filter_invert.c
+++ src/modules/plus/filter_invert.c
@@ -19,47 +19,61 @@
 
 #include <framework/mlt_filter.h>
 #include <framework/mlt_frame.h>
+#include <framework/mlt_slices.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-static inline int clamp( int v, int l, int u )
-{
-	return v < l ? l : ( v > u ? u : v );
-}
+typedef struct {
+	uint8_t* image;
+	int height;
+	int width;
+	int full_range;
+} slice_desc;
 
-/** Do it :-).
-*/
+static int do_slice_proc(int id, int index, int jobs, void* data)
+{
+	(void) id; // unused
+	slice_desc* desc = (slice_desc*) data;
+	int slice_line_start, slice_height = mlt_slices_size_slice(jobs, index, desc->height, &slice_line_start);
+	int slice_line_end = slice_line_start + slice_height;
+	int line_size = desc->width * 2;
+	int min = desc->full_range ? 0 : 16;
+	int max_luma = desc->full_range ? 255 : 235;
+	int max_chroma = desc->full_range ? 255 : 240;
+	int invert_luma = desc->full_range ? 255 : 251;
+	int x,y;
+	for ( y = slice_line_start; y < slice_line_end; y++)
+	{
+		uint8_t* p = desc->image + y * line_size;
+		for ( x = 0; x < line_size; x += 2)
+		{
+			p[x] = CLAMP(invert_luma - p[x], min, max_luma);
+			p[x+1] = CLAMP(256 - p[x+1], min, max_chroma);
+		}
+	}
+}
 
 static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format *format, int *width, int *height, int writable )
 {
 	// Get the image
 	mlt_filter filter = mlt_frame_pop_service( frame );
 
-	int mask = mlt_properties_get_int( MLT_FILTER_PROPERTIES( filter ), "alpha" );
 	*format = mlt_image_yuv422;
 	int error = mlt_frame_get_image( frame, image, format, width, height, 1 );
-	int full_range = mlt_properties_get_int(MLT_FRAME_PROPERTIES(frame), "full_range");
-	int min = full_range? 0 : 16;
-	int max_luma = full_range? 255 : 235;
-	int max_chroma = full_range? 255 : 240;
-	int invert_luma = full_range? 255 : 251;
-
-	// Only process if we have no error and a valid colour space
-	if ( error == 0 )
+	// Only process if we have no error and a valid color space
+	if ( error == 0 && *format == mlt_image_yuv422)
 	{
-		uint8_t *p = *image;
-		uint8_t *q = *image + *width * *height * 2;
-		uint8_t *r = *image;
+		slice_desc desc;
+		desc.image = *image;
+		desc.width = *width;
+		desc.height = *height;
+		desc.full_range = mlt_properties_get_int(MLT_FRAME_PROPERTIES(frame), "full_range");
+		mlt_slices_run_normal(0, do_slice_proc, &desc);
 
-		while ( p != q )
-		{
-			*p++ = clamp(invert_luma - *r++, min, max_luma);
-			*p++ = clamp(256 - *r++, min, max_chroma);
-		}
-
+		int mask = mlt_properties_get_int( MLT_FILTER_PROPERTIES( filter ), "alpha" );
 		if ( mask )
 		{
 			int size = *width * *height;
