ec02baf63fd062c1b2492de6aef17ff600f08571
Dan Dennedy <dan@dennedy.org>
2022-04-28 21:20:44 -0700

    fix #788 compile warning

--- src/modules/xml/gps_parser.c
+++ src/modules/xml/gps_parser.c
@@ -17,6 +17,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#define __USE_XOPEN
+#define _GNU_SOURCE
+
 #include "gps_parser.h"
 #include <inttypes.h>
 
