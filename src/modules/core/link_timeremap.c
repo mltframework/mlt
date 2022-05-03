mltframework/mlt

commit 685698ab01fea2ac5322ab074b3e827ead5a6739

Fix data race in time remapping (#730)
* Fix data race in time remapping

Fix an issue in `link_timeremap.c`, where multiple concurrent threads
calling `mlt_frame_get_image` would cause the returned image data to be
corrupted, or to be consumed incorrectly. As a result, the produced
frames would be seemingly arbitrarily corrupted, more so in "blend" mode
than in "nearest" mode, due to the increased number of frames fetched.

Add a `mlt_service_lock` and `mlt_service_unlock` call around the
critical function calls to ensure data returned is intact.

Fixes #729.

src/modules/core/link_timeremap.c
@@ -268,7 +268,10 @@ static int link_get_image_blend( mlt_frame frame, uint8_t** image, mlt_image_for
 		{
 			break;
 		}
-		if ( mlt_frame_get_image( src_frame, &images[image_count], format, &requested_width, &requested_height, 0 ) != 0 )
+		mlt_service_lock( MLT_LINK_SERVICE(self) );
+		int result = mlt_frame_get_image( src_frame, &images[image_count], format, &requested_width, &requested_height, 0 );
+		mlt_service_unlock( MLT_LINK_SERVICE(self) );
+		if ( !result )
 		{
 			mlt_log_error( MLT_LINK_SERVICE(self), "Failed to get image %s\n", key );
 			break;
@@ -333,7 +336,10 @@ static int link_get_image_nearest( mlt_frame frame, uint8_t** image, mlt_image_f
 	if ( src_frame )
 	{
 		uint8_t* in_image;
-		if ( !mlt_frame_get_image( src_frame, &in_image, format, width, height, 0 ) )
+		mlt_service_lock( MLT_LINK_SERVICE(self) );
+		int result = mlt_frame_get_image( src_frame, &in_image, format, width, height, 0 );
+		mlt_service_unlock( MLT_LINK_SERVICE(self) );
+		if ( !result )
 		{
 			int size = mlt_image_format_size( *format, *width, *height, NULL );
 			*image = mlt_pool_alloc( size );

commit 12381397b68c1bf1eb222c82f0e3a41310dad6fd

Fix #1261: Time Remap filter blend image mode does not work
As reported here:
https://github.com/mltframework/shotcut/issues/1261
https://forum.shotcut.org/t/no-preview-in-time-remap-with-blend/33628

This regression was caused by:
https://github.com/mltframework/mlt/commit/685698ab01fea2ac5322ab074b3e827ead5a6739

src/modules/core/link_timeremap.c
@@ -266,10 +266,11 @@ static int link_get_image_blend( mlt_frame frame, uint8_t** image, mlt_image_for
 		{
 			break;
 		}
+
 		mlt_service_lock( MLT_LINK_SERVICE(self) );
-		int result = mlt_frame_get_image( src_frame, &images[image_count], format, &requested_width, &requested_height, 0 );
+		int error = mlt_frame_get_image( src_frame, &images[image_count], format, &requested_width, &requested_height, 0 );
 		mlt_service_unlock( MLT_LINK_SERVICE(self) );
-		if ( !result )
+		if ( error )
 		{
 			mlt_log_error( MLT_LINK_SERVICE(self), "Failed to get image %s\n", key );
 			break;
@@ -335,9 +336,9 @@ static int link_get_image_nearest( mlt_frame frame, uint8_t** image, mlt_image_f
 	{
 		uint8_t* in_image;
 		mlt_service_lock( MLT_LINK_SERVICE(self) );
-		int result = mlt_frame_get_image( src_frame, &in_image, format, width, height, 0 );
+		int error = mlt_frame_get_image( src_frame, &in_image, format, width, height, 0 );
 		mlt_service_unlock( MLT_LINK_SERVICE(self) );
-		if ( !result )
+		if ( !error )
 		{
 			int size = mlt_image_format_size( *format, *width, *height, NULL );
 			*image = mlt_pool_alloc( size );