ee9452cab0a522834b79aafd0b5a21981d2e7895
Hans-Peter Jansen <hp@urpla.net>
2022-06-21 19:00:58 +0200

    fix #800: reserve enough space for any unicode conversion in producer_avformat:filter_restricted()

--- src/modules/avformat/producer_avformat.c
+++ src/modules/avformat/producer_avformat.c
@@ -334,7 +334,7 @@ static char* filter_restricted( const char *in )
 {
 	if ( !in ) return NULL;
 	size_t n = strlen( in );
-	char *out = calloc( 1, n + 1 );
+	char *out = calloc( 1, n*3 + 1 );
 	char *p = out;
 	mbstate_t mbs;
 	memset( &mbs, 0, sizeof(mbs) );
