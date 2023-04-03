mltframework/mlt

commit db1f3ceff723912c2ff4b6207a3f446f6677e8f8

Fix compilation: parameter type mismatch (#896)
* Fix compilation: parameter type mismatch

* Fix incompatible arg

src/win32/win32.c
@@ -70,7 +70,7 @@ int setenv(const char *name, const char *value, int overwrite)
 
 static int iconv_from_utf8( mlt_properties properties, const char *prop_name, const char *prop_name_out, const char* encoding )
 {
-	char *text = mlt_properties_get( properties, prop_name );
+	const char *text = mlt_properties_get( properties, prop_name );
 	int result = 0;
 
 	if ( text ) {
@@ -99,7 +99,7 @@ static int iconv_from_utf8( mlt_properties properties, const char *prop_name, co
 
 static int iconv_to_utf8( mlt_properties properties, const char *prop_name, const char *prop_name_out, const char* encoding )
 {
-	char *text = mlt_properties_get( properties, prop_name );
+	const char *text = mlt_properties_get( properties, prop_name );
 	int result = 0;
 
 	if ( text ) {

src/modules/avformat/factory.c
@@ -32,7 +32,7 @@ extern mlt_filter filter_swscale_init(mlt_profile profile, char *arg);
 extern mlt_producer producer_avformat_init(mlt_profile profile, const char *service, char *file);
 extern mlt_filter filter_avfilter_init(mlt_profile, mlt_service_type, const char *, char *);
 extern mlt_link link_avdeinterlace_init(mlt_profile, mlt_service_type, const char *, char *);
-extern mlt_link link_swresample_init(mlt_profile profile, char *arg);
+extern mlt_link link_swresample_init(mlt_profile profile, mlt_service_type, const char *, char *);
 
 // ffmpeg Header files
 #include <libavcodec/avcodec.h>
@@ -95,7 +95,7 @@ static void *create_service(mlt_profile profile, mlt_service_type type, const ch
         if (type == mlt_service_filter_type)
             return filter_swresample_init(profile, arg);
         else if (type == mlt_service_link_type)
-            return link_swresample_init(profile, arg);
+            return link_swresample_init(profile, type, id, arg);
     }
 #endif
     return NULL;

commit 9f3c5414e73eea1235de198033c16885ea7d0137

fix compile regression on Windows in db1f3ce

src/win32/win32.c
@@ -70,7 +70,7 @@ int setenv(const char *name, const char *value, int overwrite)
 
 static int iconv_from_utf8( mlt_properties properties, const char *prop_name, const char *prop_name_out, const char* encoding )
 {
-	const char *text = mlt_properties_get( properties, prop_name );
+	char *text = mlt_properties_get( properties, prop_name );
 	int result = 0;
 
 	if ( text ) {
@@ -99,7 +99,7 @@ static int iconv_from_utf8( mlt_properties properties, const char *prop_name, co
 
 static int iconv_to_utf8( mlt_properties properties, const char *prop_name, const char *prop_name_out, const char* encoding )
 {
-	const char *text = mlt_properties_get( properties, prop_name );
+	char *text = mlt_properties_get( properties, prop_name );
 	int result = 0;
 
 	if ( text ) {