5057574e5a79b25dfb309c356090646123ae7015
Brian Matherly <brian.matherly@yahoo.com>
2023-06-13 21:43:21 -0500

    Add track auto fade filter

--- src/modules/core/factory.c
+++ src/modules/core/factory.c
@@ -45,6 +45,10 @@ extern mlt_filter filter_audiowave_init(mlt_profile profile,
                                         mlt_service_type type,
                                         const char *id,
                                         char *arg);
+extern mlt_filter filter_autofade_init(mlt_profile profile,
+                                       mlt_service_type type,
+                                       const char *id,
+                                       char *arg);
 extern mlt_filter filter_box_blur_init(mlt_profile profile,
                                        mlt_service_type type,
                                        const char *id,
@@ -203,6 +207,7 @@ MLT_REPOSITORY
     MLT_REGISTER(mlt_service_filter_type, "audiomap", filter_audiomap_init);
     MLT_REGISTER(mlt_service_filter_type, "audioseam", filter_audioseam_init);
     MLT_REGISTER(mlt_service_filter_type, "audiowave", filter_audiowave_init);
+    MLT_REGISTER(mlt_service_filter_type, "autofade", filter_autofade_init);
     MLT_REGISTER(mlt_service_filter_type, "box_blur", filter_box_blur_init);
     MLT_REGISTER(mlt_service_filter_type, "brightness", filter_brightness_init);
     MLT_REGISTER(mlt_service_filter_type, "channelcopy", filter_channelcopy_init);
@@ -264,6 +269,7 @@ MLT_REPOSITORY
     MLT_REGISTER_METADATA(mlt_service_filter_type, "audiomap", metadata, "filter_audiomap.yml");
     MLT_REGISTER_METADATA(mlt_service_filter_type, "audioseam", metadata, "filter_audioseam.yml");
     MLT_REGISTER_METADATA(mlt_service_filter_type, "audiowave", metadata, "filter_audiowave.yml");
+    MLT_REGISTER_METADATA(mlt_service_filter_type, "autofade", metadata, "filter_autofade.yml");
     MLT_REGISTER_METADATA(mlt_service_filter_type, "box_blur", metadata, "filter_box_blur.yml");
     MLT_REGISTER_METADATA(mlt_service_filter_type, "brightness", metadata, "filter_brightness.yml");
     MLT_REGISTER_METADATA(mlt_service_filter_type,
