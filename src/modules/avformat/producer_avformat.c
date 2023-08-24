mltframework/mlt

commit 35402e6e6ea83e67cf1686743f3d21e42dfd4dd9

fix video and audio out of sync when the start time of video stream is not 0 (#838)
Co-authored-by: songzhuo <songzhuo@bluepulse.cn>

src/modules/avformat/producer_avformat.c
@@ -2633,9 +2633,13 @@ static int decode_audio( producer_avformat self, int *ignore, const AVPacket *pk
 	{
 		int64_t pts = pkt->pts;
 		if ( self->first_pts != AV_NOPTS_VALUE )
-			pts -= self->first_pts;
+			pts -= av_rescale_q(self->first_pts,
+								context->streams[self->video_index]->time_base,
+								context->streams[ index ]->time_base);
 		else if ( context->start_time != AV_NOPTS_VALUE && self->video_index != -1 )
-			pts -= context->start_time;
+			pts -= av_rescale_q(context->start_time,
+								AV_TIME_BASE_Q,
+								context->streams[ index ]->time_base);
 		double timebase = av_q2d( context->streams[ index ]->time_base );
 		int64_t int_position = llrint( timebase * pts * fps );
 		int64_t req_position = llrint( timecode * fps );

commit 8f2b043a3febbb5cd063fe31b6e2f773c281df48

Fix crash when setting video_index to -1
video_index can be used to index the streams array when it has a value
of -1.

This is a regression caused by 35402e6e6ea83e67cf1686743f3d21e42dfd4dd9

src/modules/avformat/producer_avformat.c
@@ -3151,10 +3151,12 @@ static int decode_audio(producer_avformat self,
         int64_t pts_offset = lrint((double) audio_used_at_start / timebase
                                    / (double) codec_context->sample_rate);
         int64_t pts = pkt->pts - pts_offset;
-        if (self->first_pts != AV_NOPTS_VALUE)
+        if (self->first_pts != AV_NOPTS_VALUE && self->video_index != -1)
             pts -= av_rescale_q(self->first_pts,
                                 context->streams[self->video_index]->time_base,
                                 context->streams[index]->time_base);
+        else if (self->first_pts != AV_NOPTS_VALUE)
+            pts -= self->first_pts;
         else if (context->start_time != AV_NOPTS_VALUE && self->video_index != -1)
             pts -= av_rescale_q(context->start_time,
                                 AV_TIME_BASE_Q,