3f0d9e4bb0186268e6648267064e54bf3044547a
Dan Dennedy <dan@dennedy.org>
2023-09-09 10:14:15 -0700

    fix detecting color string in mlt_property_interpolate

--- src/framework/mlt_property.c
+++ src/framework/mlt_property.c
@@ -1238,8 +1238,8 @@ int mlt_property_interpolate(mlt_property self,
     int colorstring = 0;
     const char *value = self->prop_string;
     if (value
-        && ((strlen(value) > 6 && value[0] == '#')
-            || (strlen(value) > 7 && value[0] == '0' && value[1] == 'x'))) {
+        && (((strlen(value) == 6 || strlen(value) == 8) && value[0] == '#')
+            || (strlen(value) == 10 && value[0] == '0' && value[1] == 'x'))) {
         colorstring = 1;
     }
 
