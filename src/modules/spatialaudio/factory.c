bb9a5bd4d9c5586f8e7479c1c2fc474ce9f61ecc
Dan Dennedy <dan@dennedy.org>
2024-02-07 17:29:55 -0800

    add spatialaudio module & ambisonic-decoder filter
    Depends on https://github.com/videolabs/libspatialaudio

---
+++ src/modules/spatialaudio/factory.c
@@ -0,0 +1,46 @@
+/*
+ * factory.c -- the factory method interfaces
+ * Copyright (C) 2024 Meltytech, LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <framework/mlt.h>
+
+#include <limits.h>
+#include <string.h>
+
+extern mlt_filter filter_ambisonic_decoder_init(mlt_profile profile,
+                                                mlt_service_type type,
+                                                const char *id,
+                                                char *arg);
+
+static mlt_properties metadata(mlt_service_type type, const char *id, void *data)
+{
+    char file[PATH_MAX];
+    mlt_properties result = NULL;
+
+    // Load the yaml file
+    snprintf(file, PATH_MAX, "%s/spatialaudio/filter_%s.yml", mlt_environment("MLT_DATA"), id);
+    result = mlt_properties_parse_yaml(file);
+
+    return result;
+}
+
+MLT_REPOSITORY
+{
+    MLT_REGISTER(mlt_service_filter_type, "ambisonic-decoder", filter_ambisonic_decoder_init);
+    MLT_REGISTER_METADATA(mlt_service_filter_type, "ambisonic-decoder", metadata, NULL);
+}
---
+++ src/modules/spatialaudio/filter_ambisonic-decoder.cpp
@@ -0,0 +1,192 @@
+/*
+ * filter_ambisonic-decoder.cpp -- decode ambisonic audio to speaker channels
+ * Copyright (C) 2024 Meltytech, LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <framework/mlt.h>
+#include <spatialaudio/Ambisonics.h>
+
+static const auto MAX_CHANNELS = 6;
+static const auto AMBISONICS_BLOCK_SIZE = 1024;
+static const auto AMBISONICS_ORDER = 1;
+static const auto AMBISONICS_1_CHANNELS = 4;
+
+extern "C" {
+static mlt_frame process(mlt_filter filter, mlt_frame frame);
+static void close_filter(mlt_filter filter);
+}
+
+class SpatialAudio
+{
+private:
+    mlt_filter m_filter;
+    CAmbisonicDecoder decoder;
+    CAmbisonicProcessor processor;
+    CAmbisonicZoomer zoomer;
+    float *speakers[MAX_CHANNELS];
+
+public:
+    SpatialAudio()
+    {
+        m_filter = mlt_filter_new();
+        if (m_filter) {
+            m_filter->child = this;
+            m_filter->close = close_filter;
+            m_filter->process = process;
+        }
+    }
+
+    ~SpatialAudio()
+    {
+        m_filter->child = nullptr;
+        m_filter->close = nullptr;
+        m_filter->parent.close = nullptr;
+        mlt_service_close(MLT_FILTER_SERVICE(m_filter));
+        m_filter = nullptr;
+    }
+
+    mlt_filter filter() { return m_filter; }
+    double getDouble(const char *name, int position, int length)
+    {
+        return mlt_properties_anim_get_double(MLT_FILTER_PROPERTIES(filter()),
+                                              name,
+                                              position,
+                                              length);
+    }
+
+    bool getAudio(mlt_frame frame, float *buffer, int samples, int channels)
+    {
+        bool error = false;
+        if (decoder.GetChannelCount() == 0) {
+            mlt_log_verbose(MLT_FILTER_SERVICE(filter()), "configuring spatial audio decoder\n");
+            error = !decoder.Configure(AMBISONICS_ORDER,
+                                       true,
+                                       /*AMBISONICS_BLOCK_SIZE*/ samples,
+                                       channels == 6   ? kAmblib_51
+                                       : channels == 2 ? kAmblib_Stereo
+                                       : channels == 4 ? kAmblib_Quad
+                                                       : kAmblib_CustomSpeakerSetUp,
+                                       channels);
+            if (!error) {
+                mlt_log_verbose(MLT_FILTER_SERVICE(filter()),
+                                "configuring spatial audio processor\n");
+                error = !processor.Configure(AMBISONICS_ORDER, true, AMBISONICS_BLOCK_SIZE, 0);
+                if (!error) {
+                    mlt_log_verbose(MLT_FILTER_SERVICE(filter()),
+                                    "configuring spatial audio zoomer\n");
+                    error = !zoomer.Configure(AMBISONICS_ORDER, true, AMBISONICS_BLOCK_SIZE, 0);
+                } else {
+                    mlt_log_error(MLT_FILTER_SERVICE(filter()),
+                                  "failed to configure spatial audio zoomer\n");
+                }
+            } else {
+                mlt_log_error(MLT_FILTER_SERVICE(filter()),
+                              "failed to configure spatial audio processor\n");
+            }
+        }
+        if (!error) {
+            CBFormat bformat;
+            bformat.Configure(1, true, samples);
+            for (unsigned i = 0; i < 4; ++i)
+                bformat.InsertStream(&buffer[samples * i], i, samples);
+
+            mlt_position position = mlt_filter_get_position(filter(), frame);
+            mlt_position length = mlt_filter_get_length2(filter(), frame);
+            processor.SetOrientation({-DegreesToRadians(getDouble("yaw", position, length)),
+                                      DegreesToRadians(getDouble("pitch", position, length)),
+                                      DegreesToRadians(getDouble("roll", position, length))});
+            processor.Refresh();
+            processor.Process(&bformat, samples);
+            zoomer.SetZoom(getDouble("zoom", position, length));
+            zoomer.Refresh();
+            zoomer.Process(&bformat, samples);
+
+            for (int i = 0; i < channels; ++i)
+                speakers[i] = &buffer[samples * i];
+            decoder.Process(&bformat, samples, speakers);
+        }
+        return error;
+    }
+};
+
+extern "C" {
+
+static int get_audio(mlt_frame frame,
+                     void **buffer,
+                     mlt_audio_format *format,
+                     int *frequency,
+                     int *channels,
+                     int *samples)
+{
+    auto error = 0;
+    // Get the filter service
+    mlt_filter filter = (mlt_filter) mlt_frame_pop_audio(frame);
+
+    mlt_service_lock(MLT_FILTER_SERVICE(filter));
+
+    // Get the producer's audio
+    if (*channels > 1) {
+        *format = mlt_audio_float;
+        int requestChannels = MAX(AMBISONICS_1_CHANNELS, *channels);
+        error = mlt_frame_get_audio(frame, buffer, format, frequency, &requestChannels, samples);
+
+        if (!error && requestChannels >= AMBISONICS_1_CHANNELS) {
+            auto spatial = static_cast<SpatialAudio *>(filter->child);
+            error = spatial->getAudio(frame, static_cast<float *>(*buffer), *samples, *channels);
+        }
+    } else {
+        // pass through mono
+        error = mlt_frame_get_audio(frame, buffer, format, frequency, channels, samples);
+    }
+
+    mlt_service_unlock(MLT_FILTER_SERVICE(filter));
+
+    return error;
+}
+
+static mlt_frame process(mlt_filter filter, mlt_frame frame)
+{
+    if (mlt_frame_is_test_audio(frame) == 0) {
+        // Add the filter to the frame
+        mlt_frame_push_audio(frame, filter);
+        mlt_frame_push_audio(frame, (void *) get_audio);
+    }
+    return frame;
+}
+
+static void close_filter(mlt_filter filter)
+{
+    delete reinterpret_cast<SpatialAudio *>(filter->child);
+}
+
+mlt_filter filter_ambisonic_decoder_init(mlt_profile profile,
+                                         mlt_service_type type,
+                                         const char *id,
+                                         char *arg)
+{
+    mlt_filter filter = nullptr;
+    auto *spatial = new SpatialAudio;
+
+    if (spatial) {
+        filter = spatial->filter();
+        if (!filter)
+            delete spatial;
+    }
+    return filter;
+}
+
+} // extern
