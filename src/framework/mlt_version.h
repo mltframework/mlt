mltframework/mlt

commit 884ef05c26452a700630ce6d61d0d36880cd4a79

let consumer properties update automatic profile
fixes #1012

src/melt/melt.c
@@ -751,7 +751,7 @@ int main(int argc, char **argv)
     int is_abort = 0;
     int is_getc = 0;
     int error = 0;
-    mlt_profile backup_profile;
+    mlt_profile backup_profile = NULL;
     mlt_repository repo = NULL;
     const char *repo_path = NULL;
     int is_consumer_explicit = 0;
@@ -922,8 +922,6 @@ int main(int argc, char **argv)
             || profile->frame_rate_num != backup_profile->frame_rate_num
             || profile->colorspace != backup_profile->colorspace))
         profile->is_explicit = 1;
-    mlt_profile_close(backup_profile);
-    backup_profile = NULL;
 
     // Get melt producer
     if (argc > 1)
@@ -931,7 +929,7 @@ int main(int argc, char **argv)
 
     if (melt) {
         // Generate an automatic profile if needed.
-        if (!profile->is_explicit) {
+        if (backup_profile && !backup_profile->is_explicit) {
             mlt_producer first_producer = mlt_properties_get_data(MLT_PRODUCER_PROPERTIES(melt),
                                                                   "first_producer",
                                                                   NULL);
@@ -946,6 +944,7 @@ int main(int argc, char **argv)
 
         double scale = mlt_properties_get_double(MLT_CONSUMER_PROPERTIES(consumer), "scale");
         if (scale > 0.0) {
+            mlt_profile_close(backup_profile);
             set_preview_scale(&profile, &backup_profile, scale);
         }
 

commit f104d17bc9fafa4fc44ec62a763c0d9fe8ce2b49

fix melt auto-profile with xml-injected qglsl consumer
regression in 884ef05c

CMakeLists.txt
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.14)
 
 project(MLT
-  VERSION 7.26.0
+  VERSION 7.27.0
   DESCRIPTION "Multimedia Framework"
   HOMEPAGE_URL "https://www.mltframework.org"
   LANGUAGES C CXX

src/framework/mlt_version.h
@@ -27,7 +27,7 @@
 #define MLT_STRINGIZE(s) MLT_STRINGIZE2(s)
 
 #define LIBMLT_VERSION_MAJOR 7
-#define LIBMLT_VERSION_MINOR 26
+#define LIBMLT_VERSION_MINOR 27
 #define LIBMLT_VERSION_REVISION 0
 #define LIBMLT_VERSION_INT \
     ((LIBMLT_VERSION_MAJOR << 16) + (LIBMLT_VERSION_MINOR << 8) + LIBMLT_VERSION_REVISION)

src/melt/melt.c
@@ -287,7 +287,7 @@ static int load_consumer(mlt_consumer *consumer, mlt_profile profile, int argc, 
     for (i = 1; i < argc; i++) {
         // See if we need multi consumer.
         multi += !strcmp(argv[i], "-consumer");
-        // Seee if we need the qglsl variant of multi consumer.
+        // See if we need the qglsl variant of multi consumer.
         if (!strncmp(argv[i], "glsl.", 5) || !strncmp(argv[i], "movit.", 6))
             qglsl = 1;
 #if SDL_MAJOR_VERSION == 2
@@ -964,7 +964,8 @@ int main(int argc, char **argv)
 
     if (melt) {
         // Generate an automatic profile if needed.
-        if (backup_profile && !backup_profile->is_explicit) {
+        if ((consumer && backup_profile && !backup_profile->is_explicit)
+            || (!consumer && !profile->is_explicit)) {
             mlt_producer first_producer = mlt_properties_get_data(MLT_PRODUCER_PROPERTIES(melt),
                                                                   "first_producer",
                                                                   NULL);