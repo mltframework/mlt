a5fc48daa61b37d37706c2badbf16aaa5c947f13
wangguangyue <896659682@qq.com>
2024-12-11 13:49:21 +0800

    try-fix: https://github.com/mltframework/mlt/issues/1058

--- src/framework/mlt_service.h
+++ src/framework/mlt_service.h
@@ -90,6 +90,7 @@ extern int mlt_service_get_frame(mlt_service self, mlt_frame_ptr frame, int inde
 extern mlt_properties mlt_service_properties(mlt_service self);
 extern mlt_service mlt_service_consumer(mlt_service self);
 extern mlt_service mlt_service_producer(mlt_service self);
+extern mlt_service mlt_service_peek_producer(mlt_service self, int index);
 extern int mlt_service_attach(mlt_service self, mlt_filter filter);
 extern int mlt_service_detach(mlt_service self, mlt_filter filter);
 extern void mlt_service_apply_filters(mlt_service self, mlt_frame frame, int index);
