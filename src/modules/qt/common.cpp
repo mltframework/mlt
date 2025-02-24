17c9559f8055ef0b5865b4b97837dd224484f97d
Jean-Baptiste Mardelle <jb@kdenlive.org>
2025-02-24 08:06:15 +0100

    better variable name

--- src/modules/qt/common.cpp
+++ src/modules/qt/common.cpp
@@ -37,8 +37,8 @@ bool createQApplicationIfNeeded(mlt_service service)
 #endif
 #if defined(Q_OS_UNIX) && !defined(Q_OS_MAC) && !defined(Q_OS_ANDROID)
         if (getenv("DISPLAY") == 0 && getenv("WAYLAND_DISPLAY") == 0) {
-            const char* s = getenv("QT_QPA_PLATFORM");
-            if (s == 0 || strcmp(s, "offscreen") != 0) {
+            const char *qt_qpa = getenv("QT_QPA_PLATFORM");
+            if (qt_qpa == 0 || strcmp(qt_qpa, "offscreen") != 0) {
                 mlt_log_error(service,
                           "The MLT Qt module requires a X11 or Wayland environment.\n"
                           "Please either run melt from a session with a display server or use a "
