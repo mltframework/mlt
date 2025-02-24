74e4939ac6e7eb57fd42340918af1383b39197c9
j-b-m <jb@kdenlive.org>
2025-02-24 09:14:51 +0100

    Check for QT's offscreen platform (#1069)
    * Don't abort Qt module if QT_QPA_PLATFORM is set to offscreen

--- src/modules/qt/common.cpp
+++ src/modules/qt/common.cpp
@@ -37,12 +37,15 @@ bool createQApplicationIfNeeded(mlt_service service)
 #endif
 #if defined(Q_OS_UNIX) && !defined(Q_OS_MAC) && !defined(Q_OS_ANDROID)
         if (getenv("DISPLAY") == 0 && getenv("WAYLAND_DISPLAY") == 0) {
-            mlt_log_error(service,
+            const char *qt_qpa = getenv("QT_QPA_PLATFORM");
+            if (!qt_qpa || strcmp(qt_qpa, "offscreen")) {
+                mlt_log_error(service,
                           "The MLT Qt module requires a X11 or Wayland environment.\n"
                           "Please either run melt from a session with a display server or use a "
                           "fake X server like xvfb:\n"
                           "xvfb-run -a melt (...)\n");
-            return false;
+                return false;
+            }
         }
 #endif
         if (!mlt_properties_get(mlt_global_properties(), "qt_argv"))
