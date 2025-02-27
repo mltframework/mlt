mltframework/mlt

commit cb0ff750ec1497a5825090f92a4e5f0cf1a83494

fix #850 sdl2 consumer crashes with radeonsi_dri

src/modules/sdl2/consumer_sdl2.c
@@ -1,6 +1,6 @@
 /*
  * consumer_sdl.c -- A Simple DirectMedia Layer consumer
- * Copyright (C) 2017-2021 Meltytech, LLC
+ * Copyright (C) 2017-2023 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -221,8 +221,17 @@ int consumer_start(mlt_consumer parent)
         }
 
         // Initialize SDL video if needed.
-        if (setup_sdl_video(self))
-            return 1;
+        if (!SDL_WasInit(SDL_INIT_VIDEO)) {
+            pthread_mutex_lock(&mlt_sdl_mutex);
+            int ret = SDL_Init(SDL_INIT_VIDEO);
+            pthread_mutex_unlock(&mlt_sdl_mutex);
+            if (ret < 0) {
+                mlt_log_error(MLT_CONSUMER_SERVICE(&self->parent),
+                              "Failed to initialize SDL: %s\n",
+                              SDL_GetError());
+                return 1;
+            }
+        }
 
         pthread_create(&self->thread, NULL, consumer_thread, self);
     }
@@ -482,18 +491,6 @@ static int setup_sdl_video(consumer_sdl self)
     if (video_off || preview_off)
         return error;
 
-    if (!SDL_WasInit(SDL_INIT_VIDEO)) {
-        pthread_mutex_lock(&mlt_sdl_mutex);
-        int ret = SDL_Init(SDL_INIT_VIDEO);
-        pthread_mutex_unlock(&mlt_sdl_mutex);
-        if (ret < 0) {
-            mlt_log_error(MLT_CONSUMER_SERVICE(&self->parent),
-                          "Failed to initialize SDL: %s\n",
-                          SDL_GetError());
-            return -1;
-        }
-    }
-
 #ifdef MLT_IMAGE_FORMAT
     int image_format = mlt_properties_get_int(self->properties, "mlt_image_format");
 
@@ -580,6 +577,12 @@ static int consumer_play_video(consumer_sdl self, mlt_frame frame)
     int display_off = video_off | preview_off;
 
     if (self->running && !display_off) {
+        if (!self->sdl_window) {
+            int error = setup_sdl_video(self);
+            if (error)
+                return error;
+        }
+
         // Get the image, width and height
         mlt_frame_get_image(frame, &image, &vfmt, &width, &height, 0);
 

commit 6beecfc99e56e6e79426dedb79b77c9177506330

fix #1071 sdl2 consumer broken on macOS
regression in cb0ff75

src/modules/sdl2/consumer_sdl2.c
@@ -1,6 +1,6 @@
 /*
  * consumer_sdl.c -- A Simple DirectMedia Layer consumer
- * Copyright (C) 2017-2024 Meltytech, LLC
+ * Copyright (C) 2017-2025 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -216,6 +216,10 @@ int consumer_start(mlt_consumer parent)
         }
 
         // Initialize SDL video if needed.
+#ifdef __APPLE__
+        if (setup_sdl_video(self))
+            return 1;
+#else
         if (!SDL_WasInit(SDL_INIT_VIDEO)) {
             pthread_mutex_lock(&mlt_sdl_mutex);
             int ret = SDL_Init(SDL_INIT_VIDEO);
@@ -227,6 +231,7 @@ int consumer_start(mlt_consumer parent)
                 return 1;
             }
         }
+#endif
 
         pthread_create(&self->thread, NULL, consumer_thread, self);
     }
@@ -480,6 +485,20 @@ static int setup_sdl_video(consumer_sdl self)
 
     if (video_off || preview_off)
         return error;
+
+#ifdef __APPLE__
+    if (!SDL_WasInit(SDL_INIT_VIDEO)) {
+        pthread_mutex_lock(&mlt_sdl_mutex);
+        int ret = SDL_Init(SDL_INIT_VIDEO);
+        pthread_mutex_unlock(&mlt_sdl_mutex);
+        if (ret < 0) {
+            mlt_log_error(MLT_CONSUMER_SERVICE(&self->parent),
+                          "Failed to initialize SDL: %s\n",
+                          SDL_GetError());
+            return -1;
+        }
+    }
+#endif
 
 #ifdef MLT_IMAGE_FORMAT
     int image_format = mlt_properties_get_int(self->properties, "mlt_image_format");