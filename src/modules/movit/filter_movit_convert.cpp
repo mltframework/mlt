mltframework/mlt

commit 57cfe60840647fa1d0db3f99e0c9e6b137a80bcb

Fix default gamma for 10-bit Y'CbCr from movit
If consumer does not specify "color_trc" property, it would default to
sRGB instead of Rec. 709.

src/modules/movit/filter_movit_convert.cpp
@@ -1,6 +1,6 @@
 /*
  * filter_movit_convert.cpp
- * Copyright (C) 2013-2024 Dan Dennedy <dan@dennedy.org>
+ * Copyright (C) 2013-2025 Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -106,7 +106,7 @@ enum AVColorTransferCharacteristic {
 };
 
 // Get the gamma from the frame "color_trc" property as set by producer or this filter.
-static GammaCurve getGammaCurve(int color_trc)
+static GammaCurve getFrameGamma(int color_trc)
 {
     switch (color_trc) {
     case AVCOL_TRC_LINEAR:
@@ -125,7 +125,7 @@ static GammaCurve getGammaCurve(int color_trc)
 
 // Get the gamma from the consumer's "color_trc" property.
 // Also, update the frame's color_trc property with the selection.
-static GammaCurve getGammaCurve(mlt_properties properties)
+static GammaCurve getOutputGamma(mlt_properties properties)
 {
     const char *color_trc = mlt_properties_get(properties, "consumer.color_trc");
     if (color_trc) {
@@ -166,7 +166,7 @@ static GammaCurve getGammaCurve(mlt_properties properties)
             break;
         }
     }
-    return GAMMA_sRGB;
+    return GAMMA_INVALID;
 }
 
 static void get_format_from_properties(mlt_properties properties,
@@ -201,7 +201,7 @@ static void get_format_from_properties(mlt_properties properties,
             image_format->color_space = COLORSPACE_REC_709;
             break;
         }
-        image_format->gamma_curve = getGammaCurve(mlt_properties_get_int(properties, "color_trc"));
+        image_format->gamma_curve = getFrameGamma(mlt_properties_get_int(properties, "color_trc"));
     }
 
     if (mlt_properties_get_int(properties, "force_full_luma")) {
@@ -359,20 +359,22 @@ static void finalize_movit_chain(mlt_service leaf_service, mlt_frame frame, mlt_
         chain->effect_chain->add_effect(new Mlt::VerticalFlip);
 
         ImageFormat output_format;
-        output_format.color_space = COLORSPACE_sRGB;
-        output_format.gamma_curve = getGammaCurve(MLT_FRAME_PROPERTIES(frame));
         if (format == mlt_image_yuv444p10 || format == mlt_image_yuv420p10) {
             YCbCrFormat ycbcr_format = {};
-            get_format_from_properties(MLT_FRAME_PROPERTIES(frame), nullptr, &ycbcr_format);
+            get_format_from_properties(MLT_FRAME_PROPERTIES(frame), &output_format, &ycbcr_format);
+            output_format.gamma_curve = std::max(GAMMA_REC_709,
+                                                 getOutputGamma(MLT_FRAME_PROPERTIES(frame)));
             ycbcr_format.num_levels = 1024;
             ycbcr_format.chroma_subsampling_x = ycbcr_format.chroma_subsampling_y = 1;
             chain->effect_chain->add_ycbcr_output(output_format,
                                                   OUTPUT_ALPHA_FORMAT_POSTMULTIPLIED,
                                                   ycbcr_format,
-                                                  movit::YCBCR_OUTPUT_INTERLEAVED,
+                                                  YCBCR_OUTPUT_INTERLEAVED,
                                                   GL_UNSIGNED_SHORT);
             chain->effect_chain->set_dither_bits(16);
         } else {
+            output_format.color_space = COLORSPACE_sRGB;
+            output_format.gamma_curve = GAMMA_sRGB;
             chain->effect_chain->add_output(output_format, OUTPUT_ALPHA_FORMAT_POSTMULTIPLIED);
             chain->effect_chain->set_dither_bits(8);
         }
@@ -732,7 +734,7 @@ static int convert_image(mlt_frame frame,
                 chain->add_effect(new Mlt::VerticalFlip());
                 ImageFormat movit_output_format;
                 movit_output_format.color_space = COLORSPACE_sRGB;
-                movit_output_format.gamma_curve = getGammaCurve(properties);
+                movit_output_format.gamma_curve = GAMMA_sRGB;
                 chain->add_output(movit_output_format, OUTPUT_ALPHA_FORMAT_POSTMULTIPLIED);
                 chain->set_dither_bits(8);
                 chain->finalize();

commit 7327264173be743bde912d9078c47dea023f84b7

Fix gamma of rgba output in movit
regression in 57cfe608

src/modules/movit/filter_movit_convert.cpp
@@ -188,6 +188,12 @@ static void get_format_from_properties(mlt_properties properties,
                                        ImageFormat *image_format,
                                        YCbCrFormat *ycbcr_format)
 {
+    mlt_log_verbose(nullptr,
+                    "movit.convert colospace %d color_trc %d consumer.color_trc %s full_range %d\n",
+                    mlt_properties_get_int(properties, "colorspace"),
+                    mlt_properties_get_int(properties, "color_trc"),
+                    mlt_properties_get(properties, "consumer.color_trc"),
+                    mlt_properties_get_int(properties, "full_range"));
     switch (mlt_properties_get_int(properties, "colorspace")) {
     case 601:
         ycbcr_format->luma_coefficients = YCBCR_REC_601;
@@ -392,7 +398,8 @@ static void finalize_movit_chain(mlt_service leaf_service, mlt_frame frame, mlt_
             chain->effect_chain->set_dither_bits(16);
         } else {
             output_format.color_space = COLORSPACE_sRGB;
-            output_format.gamma_curve = GAMMA_sRGB;
+            output_format.gamma_curve = std::max(GAMMA_REC_709,
+                                                 getOutputGamma(MLT_FRAME_PROPERTIES(frame)));
             chain->effect_chain->add_output(output_format, OUTPUT_ALPHA_FORMAT_POSTMULTIPLIED);
             chain->effect_chain->set_dither_bits(8);
         }
@@ -521,6 +528,9 @@ static int movit_render(EffectChain *chain,
 
     GlslManager *glsl = GlslManager::get_instance();
     int error;
+    mlt_log_verbose(nullptr,
+                    "movit.convert render format %s\n",
+                    mlt_image_format_name(output_format));
     if (output_format == mlt_image_opengl_texture) {
         error = glsl->render_frame_texture(chain, frame, width, height, image);
     } else if (output_format == mlt_image_yuv444p10 || output_format == mlt_image_yuv420p10) {
@@ -623,11 +633,11 @@ static int convert_image(mlt_frame frame,
 
     mlt_properties properties = MLT_FRAME_PROPERTIES(frame);
 
-    mlt_log_debug(NULL,
-                  "filter_movit_convert: %s -> %s (%d)\n",
-                  mlt_image_format_name(*format),
-                  mlt_image_format_name(output_format),
-                  mlt_frame_get_position(frame));
+    mlt_log_verbose(NULL,
+                    "filter_movit_convert: %s -> %s (%d)\n",
+                    mlt_image_format_name(*format),
+                    mlt_image_format_name(output_format),
+                    mlt_frame_get_position(frame));
 
     // Use CPU if glsl not initialized or not supported.
     GlslManager *glsl = GlslManager::get_instance();
@@ -718,6 +728,7 @@ static int convert_image(mlt_frame frame,
     // If we've been asked to render some frame directly to a texture (without any
     // effects in-between), we create a new mini-chain to do so.
     if (*format != mlt_image_movit && output_format == mlt_image_opengl_texture) {
+        mlt_log_verbose(nullptr, "movit.convert No Render\n");
         // We might already have a texture from a previous conversion from mlt_image_movit.
         glsl_texture texture = (glsl_texture) mlt_properties_get_data(properties,
                                                                       "movit.convert.texture",