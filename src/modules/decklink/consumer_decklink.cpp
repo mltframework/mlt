255cd41bb85bd4fb223a55127915328cee4d02ff
Dan Dennedy <dan@dennedy.org>
2025-05-03 13:11:42 -0700

    Fix decklink HDR output flickers with SDR source

--- src/modules/decklink/consumer_decklink.cpp
+++ src/modules/decklink/consumer_decklink.cpp
@@ -51,31 +51,6 @@ static const unsigned PREROLL_MINIMUM = 3;
 enum { OP_NONE = 0, OP_OPEN, OP_START, OP_STOP, OP_EXIT };
 enum { EOTF_SDR = 0, EOTF_HDR = 1, EOTF_PQ = 2, EOTF_HLG = 3 }; ///< CEA 861.3
 
-// Copied from libavcodec, for checking the frame property color_trc
-
-enum AVColorTransferCharacteristic {
-    AVCOL_TRC_BT709 = 1, ///< also ITU-R BT1361
-    AVCOL_TRC_UNSPECIFIED = 2,
-    AVCOL_TRC_GAMMA22 = 4, ///< also ITU-R BT470M / ITU-R BT1700 625 PAL & SECAM
-    AVCOL_TRC_GAMMA28 = 5, ///< also ITU-R BT470BG
-    AVCOL_TRC_SMPTE170M
-    = 6, ///< also ITU-R BT601-6 525 or 625 / ITU-R BT1358 525 or 625 / ITU-R BT1700 NTSC
-    AVCOL_TRC_SMPTE240M = 7,
-    AVCOL_TRC_LINEAR = 8,    ///< "Linear transfer characteristics"
-    AVCOL_TRC_LOG = 9,       ///< "Logarithmic transfer characteristic (100:1 range)"
-    AVCOL_TRC_LOG_SQRT = 10, ///< "Logarithmic transfer characteristic (100 * Sqrt( 10 ) : 1 range)"
-    AVCOL_TRC_IEC61966_2_4 = 11, ///< IEC 61966-2-4
-    AVCOL_TRC_BT1361_ECG = 12,   ///< ITU-R BT1361 Extended Colour Gamut
-    AVCOL_TRC_IEC61966_2_1 = 13, ///< IEC 61966-2-1 (sRGB or sYCC)
-    AVCOL_TRC_BT2020_10 = 14,    ///< ITU-R BT2020 for 10 bit system
-    AVCOL_TRC_BT2020_12 = 15,    ///< ITU-R BT2020 for 12 bit system
-    AVCOL_TRC_SMPTE2084 = 16,    ///< SMPTE ST 2084 for 10-, 12-, 14- and 16-bit systems
-    AVCOL_TRC_SMPTEST2084 = AVCOL_TRC_SMPTE2084,
-    AVCOL_TRC_SMPTE428 = 17, ///< SMPTE ST 428-1
-    AVCOL_TRC_SMPTEST428_1 = AVCOL_TRC_SMPTE428,
-    AVCOL_TRC_ARIB_STD_B67 = 18, ///< ARIB STD-B67, known as "Hybrid log-gamma"
-    AVCOL_TRC_NB,                ///< Not part of ABI
-};
 class DeckLinkConsumer : public IDeckLinkVideoOutputCallback, public IDeckLinkAudioOutputCallback
 {
 private:
@@ -668,18 +643,14 @@ protected:
 
                 // Set HDR a transfer function
                 if (mlt_properties_exists(consumer_properties, "color_trc")) {
-                    if ((AVCOL_TRC_ARIB_STD_B67
-                         == mlt_properties_get_int(MLT_FRAME_PROPERTIES(frame), "color_trc"))
-                        && !strcmp("arib-std-b67",
-                                   mlt_properties_get(consumer_properties, "color_trc"))) {
+                    if (!strcmp("arib-std-b67",
+                                mlt_properties_get(consumer_properties, "color_trc"))) {
                         frameMeta->SetInt(bmdDeckLinkFrameMetadataHDRElectroOpticalTransferFunc,
                                           EOTF_HLG);
                         decklinkFrame->SetFlags(decklinkFrame->GetFlags()
                                                 & ~bmdFrameContainsHDRMetadata);
-                    } else if ((AVCOL_TRC_SMPTE2084
-                                == mlt_properties_get_int(MLT_FRAME_PROPERTIES(frame), "color_trc"))
-                               && !strcmp("smpte2084",
-                                          mlt_properties_get(consumer_properties, "color_trc"))) {
+                    } else if (!strcmp("smpte2084",
+                                       mlt_properties_get(consumer_properties, "color_trc"))) {
                         frameMeta->SetInt(bmdDeckLinkFrameMetadataHDRElectroOpticalTransferFunc,
                                           EOTF_PQ);
                         // CEA/SMPTE HDR metadata
