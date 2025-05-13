mltframework/mlt

commit 7752c6824c7e9845b301e69880221c98f2a5032a

improve full range handling, both in and out
- Fixes a bug in avfilter ignores consumer.color_range
- Removes forcing full range 10-bit to use mlt_image_rgb

As a result of this and the previous change 56a45888 now one can use
mlt_image_format=yuvp10 pix_fmt=yuv410p10le on the consumer and without
Movit to do cuts, scaling, and many avfilters in >8-bit YUV without
conversion to RGB or 8-bit YUV. With HLG HDR input and color_trc=arib-
std-b67 on the consumer, one can do cuts-only editing, some filtering,
and output HLG. Use AV1 or HEVC (vprofile=main10) with:

mlt_image_format=yuv420p10
pix_fmt=yuv420p10le
color_trc=arib-std-b67

src/modules/avformat/producer_avformat.c
@@ -603,7 +603,7 @@ static mlt_properties find_default_streams(producer_avformat self)
             else
                 mlt_layout = av_channel_layout_to_mlt(codec_params->channel_layout);
 #endif
-            char *layout = mlt_audio_channel_layout_name(mlt_layout);
+            const char *layout = mlt_audio_channel_layout_name(mlt_layout);
             snprintf(key, sizeof(key), "meta.media.%u.codec.layout", i);
             mlt_properties_set(meta_media, key, layout);
             break;
@@ -783,7 +783,6 @@ static enum AVPixelFormat pick_pix_fmt(enum AVPixelFormat pix_fmt)
 }
 
 static mlt_image_format pick_image_format(enum AVPixelFormat pix_fmt,
-                                          int full_range,
                                           mlt_image_format current_format)
 {
     if (current_format == mlt_image_none || current_format == mlt_image_movit
@@ -819,7 +818,7 @@ static mlt_image_format pick_image_format(enum AVPixelFormat pix_fmt,
             current_format = mlt_image_yuv422;
         }
     }
-    if (pix_fmt == AV_PIX_FMT_BAYER_RGGB16LE || (pix_fmt == AV_PIX_FMT_YUV420P10LE && full_range)) {
+    if (pix_fmt == AV_PIX_FMT_BAYER_RGGB16LE) {
         return mlt_image_rgb;
     } else if (pix_fmt == AV_PIX_FMT_YUVA444P10LE || pix_fmt == AV_PIX_FMT_GBRAP10LE
                || pix_fmt == AV_PIX_FMT_GBRAP12LE) {
@@ -915,9 +914,7 @@ static int get_basic_info(producer_avformat self, mlt_profile profile, const cha
                                                         NULL);
             if (context) {
                 sws_freeContext(context);
-                mlt_image_format format = pick_image_format(pix_fmt,
-                                                            self->full_range,
-                                                            mlt_image_yuv422);
+                mlt_image_format format = pick_image_format(pix_fmt, mlt_image_yuv422);
                 mlt_properties_set_int(properties, "format", format);
             } else
                 error = 1;
@@ -2192,8 +2189,12 @@ static int producer_get_image(mlt_frame frame,
     int got_picture = 0;
     int image_size = 0;
     const char *dst_color_range = mlt_properties_get(frame_properties, "consumer.color_range");
-    int dst_full_range = dst_color_range
-                         && (!strcmp("pc", dst_color_range) || !strcmp("jpeg", dst_color_range));
+    int dst_full_range = mlt_image_full_range(dst_color_range);
+
+    // if depth > 8 libswscale only changes range when scaling, not simple pix_fmt conversion
+    const struct AVPixFmtDescriptor *pix_desc = av_pix_fmt_desc_get(self->video_codec->pix_fmt);
+    if (pix_desc && pix_desc->nb_components > 0 && pix_desc->comp[0].depth > 8)
+        dst_full_range = self->full_range;
 
     mlt_service_lock(MLT_PRODUCER_SERVICE(producer));
     pthread_mutex_lock(&self->video_mutex);
@@ -2291,10 +2292,9 @@ static int producer_get_image(mlt_frame frame,
 #ifdef AVFILTER
     *format = pick_image_format(self->vfilter_out ? av_buffersink_get_format(self->vfilter_out)
                                                   : codec_params->format,
-                                self->full_range,
                                 *format);
 #else
-    *format = pick_image_format(codec_params->format, self->full_range, *format);
+    *format = pick_image_format(codec_params->format, *format);
 #endif
 
     // Duplicate the last image if necessary

src/modules/avformat/filter_avfilter.c
@@ -809,9 +809,11 @@ static int filter_get_image(mlt_frame frame,
         pdata->avinframe->color_primaries = mlt_properties_get_int(frame_properties,
                                                                    "color_primaries");
         pdata->avinframe->color_trc = mlt_properties_get_int(frame_properties, "color_trc");
-        pdata->avinframe->color_range = mlt_properties_get_int(frame_properties, "full_range")
-                                            ? AVCOL_RANGE_JPEG
-                                            : AVCOL_RANGE_MPEG;
+        // Setting full_range here causes full range input to always be down-converted to limited
+        // range when operating in YUV, regardless of the consumer.color_range -- for each avfilter!
+        // pdata->avinframe->color_range = mlt_properties_get_int(frame_properties, "full_range")
+        //                                     ? AVCOL_RANGE_JPEG
+        //                                     : AVCOL_RANGE_MPEG;
 
         switch (mlt_properties_get_int(frame_properties, "colorspace")) {
         case 240:

src/modules/avformat/filter_swscale.c
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include "common.h"
 #include <framework/mlt_factory.h>
 #include <framework/mlt_filter.h>
 #include <framework/mlt_frame.h>
@@ -142,6 +143,28 @@ static int filter_scale(mlt_frame frame,
             goto exit;
         }
 
+        mlt_profile profile = mlt_service_profile(
+            MLT_PRODUCER_SERVICE(mlt_frame_get_original_producer(frame)));
+        int dst_colorspace = profile ? profile->colorspace : 601;
+        int src_colorspace = mlt_properties_get_int(properties, "colorspace");
+        int src_full_range = mlt_properties_get_int(properties, "full_range");
+        const char *dst_color_range = mlt_properties_get(properties, "consumer.color_range");
+        int dst_full_range = mlt_image_full_range(dst_color_range);
+
+        result = mlt_set_luma_transfer(context,
+                                       src_colorspace,
+                                       dst_colorspace,
+                                       src_full_range,
+                                       dst_full_range);
+        if (result < 0) {
+            mlt_log_error(NULL,
+                          "[filter swscale] Setting swscale color options failed with %d (%s)\n",
+                          result,
+                          av_err2str(result));
+            result = 1;
+            goto exit;
+        }
+
         // Setup the input image
         avinframe->width = iwidth;
         avinframe->height = iheight;
@@ -235,6 +258,7 @@ static int filter_scale(mlt_frame frame,
 
         // Now update the MLT frame
         mlt_frame_set_image(frame, outbuf, out_size, mlt_pool_release);
+        mlt_properties_set_int(properties, "full_range", dst_full_range);
 
         // Return the output image
         *image = outbuf;

commit 5f91b6955a5bf97628a76b57a166c43f6e78f151

Fix converting 10-bit full to limited range
Without Movit, 10-bit full range was not able to convert to limited
range. This regressed in 7752c682 in order to accomodate 10-bit pass-
through editing (no processing). Now, it returns by going through RGB to
do the conversion but only when the consumer does not request 10-bit.
However, that still leaves a gap: 10-bit full to 10-bit limited or visa
versa. In that case, you must either set the consumer color_range same
as source, or specify mlt_image_rgb(a).

With Movit, 10-bit range conversion works even when the consumer
requests 10-bit Y'CbCr.

src/modules/avformat/common.c
@@ -1,6 +1,6 @@
 /*
  * common.h
- * Copyright (C) 2018-2024 Meltytech, LLC
+ * Copyright (C) 2018-2025 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -253,14 +253,6 @@ int mlt_set_luma_transfer(struct SwsContext *context,
     int src_range = src_full_range ? 1 : 0;
     int dst_range = dst_full_range ? 1 : 0;
 
-    sws_getColorspaceDetails(context,
-                             (int **) &src_coefficients,
-                             &src_range,
-                             (int **) &dst_coefficients,
-                             &dst_range,
-                             &brightness,
-                             &contrast,
-                             &saturation);
     switch (src_colorspace) {
     case 170:
     case 470:

src/modules/avformat/producer_avformat.c
@@ -1937,12 +1937,14 @@ static int convert_image(producer_avformat self,
     mlt_log_timings_begin();
 
     mlt_log_debug(MLT_PRODUCER_SERVICE(self->parent),
-                  "%s @ %dx%d space %d->%d\n",
+                  "%s @ %dx%d colorspace %d->%d range %d->%d\n",
                   mlt_image_format_name(*format),
                   width,
                   height,
                   self->yuv_colorspace,
-                  profile->colorspace);
+                  profile->colorspace,
+                  self->full_range,
+                  dst_full_range);
 
     // extract alpha from planar formats - only supports 8-bit
     if ((pix_fmt == AV_PIX_FMT_YUVA420P || pix_fmt == AV_PIX_FMT_YUVA422P
@@ -2227,10 +2229,17 @@ static int producer_get_image(mlt_frame frame,
     const char *dst_color_range = mlt_properties_get(frame_properties, "consumer.color_range");
     int dst_full_range = mlt_image_full_range(dst_color_range);
 
-    // if depth > 8 libswscale only changes range when scaling, not simple pix_fmt conversion
+    // if 10-bit libswscale only changes range when scaling, not simple pix_fmt conversion
     const struct AVPixFmtDescriptor *pix_desc = av_pix_fmt_desc_get(self->video_codec->pix_fmt);
-    if (pix_desc && pix_desc->nb_components > 0 && pix_desc->comp[0].depth > 8)
-        dst_full_range = self->full_range;
+    if (dst_full_range != self->full_range && pix_desc && pix_desc->nb_components > 0
+        && pix_desc->comp[0].depth == 10) {
+        if (*format == mlt_image_yuv420p10 || *format == mlt_image_yuv444p10)
+            // When the consumer requests 10-bit do not convert
+            dst_full_range = self->full_range;
+        else
+            // Otherwise, convert via RGB
+            *format = mlt_image_rgb;
+    }
 
     mlt_service_lock(MLT_PRODUCER_SERVICE(producer));
     pthread_mutex_lock(&self->video_mutex);

src/modules/movit/filter_movit_convert.cpp
@@ -375,13 +375,17 @@ static void finalize_movit_chain(mlt_service leaf_service, mlt_frame frame, mlt_
 
         ImageFormat output_format;
         if (format == mlt_image_yuv444p10 || format == mlt_image_yuv420p10) {
+            auto properties = MLT_FRAME_PROPERTIES(frame);
             YCbCrFormat ycbcr_format = {};
-            get_format_from_properties(MLT_FRAME_PROPERTIES(frame), &output_format, &ycbcr_format);
-            output_format.gamma_curve = std::max(GAMMA_REC_709,
-                                                 getOutputGamma(MLT_FRAME_PROPERTIES(frame)));
+            get_format_from_properties(properties, &output_format, &ycbcr_format);
+            output_format.gamma_curve = std::max(GAMMA_REC_709, getOutputGamma(properties));
+            ycbcr_format.full_range = mlt_image_full_range(
+                mlt_properties_get(properties, "consumer.color_range"));
             mlt_log_debug(nullptr,
-                          "[filter movit.convert] output gamma %d\n",
-                          output_format.gamma_curve);
+                            "[filter movit.convert] output gamma %d full-range %d\n",
+                            output_format.gamma_curve,
+                            ycbcr_format.full_range);
+            mlt_properties_set_int(properties, "full_range", ycbcr_format.full_range);
             ycbcr_format.num_levels = 1024;
             ycbcr_format.chroma_subsampling_x = ycbcr_format.chroma_subsampling_y = 1;
             chain->effect_chain->add_ycbcr_output(output_format,
@@ -404,6 +408,13 @@ static void finalize_movit_chain(mlt_service leaf_service, mlt_frame frame, mlt_
     } else {
         // Delete all the created Effect instances to avoid memory leaks.
         dispose_movit_effects(leaf_service, frame);
+
+        auto properties = MLT_FRAME_PROPERTIES(frame);
+        getOutputGamma(properties);
+        mlt_properties_set_int(properties,
+                               "full_range",
+                               mlt_image_full_range(
+                                   mlt_properties_get(properties, "consumer.color_range")));
     }
 }
 