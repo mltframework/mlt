mltframework/mlt

commit ed0bb1199e0ae2a29ac4b5a3f77e23431a3397a9

Fix mask_apply with custom transition
When using transition=frei0r.cairoblend or qtblend and multiple
mask_apply with filter=frei0r.alphaspot, for example, to make compound
shapes, the compositing does not work correctly. It would have a black
background instead of the original frame. I found it has something to do
with cloning a frame that was already cloned. Also, it is wasteful to
clone for each mask_start; only the first one is needed.

src/modules/core/filter_mask_apply.c
@@ -1,6 +1,6 @@
 /*
  * filter_mask_apply.c -- composite atop a cloned frame made by mask_start
- * Copyright (C) 2018 Meltytech, LLC
+ * Copyright (C) 2018-2025 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -63,6 +63,7 @@ static int get_image(mlt_frame frame,
                 int size = mlt_image_format_size(*format, *width, *height, NULL);
                 mlt_frame_set_image(frame, *image, size, NULL);
             }
+            mlt_properties_clear(properties, "mask frame");
         }
     }
     return error;

src/modules/core/filter_mask_start.c
@@ -1,6 +1,6 @@
 /*
  * filter_mask_start.c -- clone a frame before invoking a filter
- * Copyright (C) 2018-2024 Meltytech, LLC
+ * Copyright (C) 2018-2025 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -33,7 +33,7 @@ static int get_image(mlt_frame frame,
 {
     mlt_properties properties = MLT_FRAME_PROPERTIES(frame);
     int error = mlt_frame_get_image(frame, image, format, width, height, writable);
-    if (!error) {
+    if (!error && !mlt_properties_exists(properties, "mask frame")) {
         mlt_frame clone = mlt_frame_clone(frame, 1);
         clone->convert_audio = frame->convert_audio;
         clone->convert_image = frame->convert_image;

src/modules/movit/filter_movit_convert.cpp
@@ -382,9 +382,9 @@ static void finalize_movit_chain(mlt_service leaf_service, mlt_frame frame, mlt_
             ycbcr_format.full_range = mlt_image_full_range(
                 mlt_properties_get(properties, "consumer.color_range"));
             mlt_log_debug(nullptr,
-                            "[filter movit.convert] output gamma %d full-range %d\n",
-                            output_format.gamma_curve,
-                            ycbcr_format.full_range);
+                          "[filter movit.convert] output gamma %d full-range %d\n",
+                          output_format.gamma_curve,
+                          ycbcr_format.full_range);
             mlt_properties_set_int(properties, "full_range", ycbcr_format.full_range);
             ycbcr_format.num_levels = 1024;
             ycbcr_format.chroma_subsampling_x = ycbcr_format.chroma_subsampling_y = 1;

commit b452d9c27a362d8783bd6c7ce16d98b7183fe716

Fix #1107 mask_apply filter on image with alpha
regression in ed0bb11

src/modules/core/filter_mask_apply.c
@@ -63,7 +63,6 @@ static int get_image(mlt_frame frame,
                 int size = mlt_image_format_size(*format, *width, *height, NULL);
                 mlt_frame_set_image(frame, *image, size, NULL);
             }
-            mlt_properties_clear(properties, "mask frame");
         }
     }
     return error;