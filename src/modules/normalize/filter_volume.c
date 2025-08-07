mltframework/mlt

commit 3412c3fa05343ae165c68dc798e2e0041329f24a

Add `channel_mask` to the volume filter

src/modules/normalize/filter_volume.c
@@ -1,6 +1,6 @@
 /*
  * filter_volume.c -- adjust audio volume
- * Copyright (C) 2003-2023 Meltytech, LLC
+ * Copyright (C) 2003-2025 Meltytech, LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -237,7 +237,7 @@ static int filter_get_audio(mlt_frame frame,
     // Save the current gain for the next iteration
     mlt_properties_set_double(filter_props, "_previous_gain", gain);
     mlt_properties_set_position(filter_props, "_last_position", current_position);
-
+    int channel_mask = mlt_properties_get_int(filter_props, "channel_mask");
     mlt_service_unlock(MLT_FILTER_SERVICE(filter));
 
     // Ramp from the previous gain to the current
@@ -250,23 +250,25 @@ static int filter_get_audio(mlt_frame frame,
         int bytes_per_samp = (samp_width - 1) / 8 + 1;
         int samplemax = (1 << (bytes_per_samp * 8 - 1)) - 1;
 
-        for (i = 0; i < *samples; i++, gain += gain_step) {
-            for (j = 0; j < *channels; j++) {
-                sample = *p * gain;
-                *p = ROUND(sample);
-                if (gain > 1.0 && normalize) {
-                    /* use limiter function instead of clipping */
-                    *p = ROUND(samplemax * limiter(sample / (double) samplemax, limiter_level));
+        for (j = 0; j < *channels; j++) {
+            if (channel_mask & (1 << j))
+                for (i = 0; i < *samples; i++, gain += gain_step) {
+                    sample = *p * gain;
+                    *p = ROUND(sample);
+                    if (gain > 1.0 && normalize) {
+                        /* use limiter function instead of clipping */
+                        p[*channels * i + j] = ROUND(
+                            samplemax * limiter(sample / (double) samplemax, limiter_level));
+                    }
                 }
-                p++;
-            }
         }
     } else {
         float *p = *buffer;
-        for (i = 0; i < *samples; i++, gain += gain_step) {
-            for (j = 0; j < *channels; j++, p++) {
-                p[0] *= gain;
-            }
+        for (j = 0; j < *channels; j++) {
+            if (channel_mask & (1 << j))
+                for (i = 0; i < *samples; i++, gain += gain_step) {
+                    p[*channels * i + j] *= gain;
+                }
         }
     }
     return 0;
@@ -435,8 +437,8 @@ mlt_filter filter_volume_init(mlt_profile profile, mlt_service_type type, const 
 
         mlt_properties_set_int(properties, "window", 75);
         mlt_properties_set(properties, "max_gain", "20dB");
-
         mlt_properties_set(properties, "level", NULL);
+        mlt_properties_set_int(properties, "channel_mask", -1);
     }
     return filter;
 }

src/modules/core/filter_mono.yml
@@ -31,6 +31,7 @@ parameters:
     required: no
     minimum: 0
     default: 7
+    mutable: yes
 
   - identifier: output_chmask
     title: Output Channel Mask
@@ -41,3 +42,4 @@ parameters:
     required: no
     minimum: 0
     default: 0xFFFFFFFF
+    mutable: yes

src/modules/jackrack/filter_ladspa.yml
@@ -35,3 +35,4 @@ parameters:
     required: no
     minimum: 0
     default: 0xFFFFFFFFFFFFFFFF
+    mutable: yes

src/modules/normalize/filter_volume.yml
@@ -2,7 +2,7 @@ schema_version: 7.0
 type: filter
 identifier: volume
 title: Volume
-version: 2
+version: 3
 copyright: Meltytech, LLC
 creator: Dan Denneedy
 license: GPLv2
@@ -29,6 +29,7 @@ parameters:
       target amplitude -12dBFS.
       
       This value is discarded if value for property "level" is set.
+
   - identifier: window
     title: Window
     type: integer
@@ -36,6 +37,7 @@ parameters:
        The number of video frames over which to smooth normalization.
     default: 75
     mutable: yes
+
   - identifier: normalize
     title: Normalize
     type: string
@@ -49,9 +51,11 @@ parameters:
       suffix "dB" to set the amplitude in decibels.
     default: -12dBFS
     mutable: yes
+
   - identifier: normalise
     title: Normalise (*DEPRECATED*)
     description: Deprecated. See normalize
+
   - identifier: limiter
     title: Limiter
     type: string
@@ -65,6 +69,7 @@ parameters:
       "dB" to set the limiting amplitude in decibels.
     default: -6dBFS
     mutable: yes
+
   - identifier: max_gain
     title: Max gain
     type: string
@@ -73,6 +78,7 @@ parameters:
       applied during normalization.
     default: 20dB
     mutable: yes
+
   - identifier: end (*DEPRECATED*)
     title: End gain
     type: string
@@ -82,6 +88,7 @@ parameters:
        
       This value is discarded if value for property "level" is set.
     mutable: yes
+
   - identifier: max_gain
     title: Max gain
     type: string
@@ -90,6 +97,7 @@ parameters:
       applied during normalization.
     default: 20dB
     mutable: yes
+
   - identifier: level
     title: Level
     type: float
@@ -99,3 +107,16 @@ parameters:
     unit: dB
     mutable: yes
     animation: yes
+
+  - identifier: channel_mask
+    title: Channel Mask
+    type: integer
+    description: >
+      Which channels to affect.
+      Each bit represents a channel; for example, 1 = channel 0 (left),
+      2 = channel 1 (right), 3 = both left and right channels.
+      The default is to overwrite all of the channels.
+    required: no
+    minimum: 0
+    default: 0xFFFFFFFF
+    mutable: yes

commit 647071f286365db0e92bdf7c922c78899c8687f2

Fix #1112 volume filter regression in 3412c3f

src/modules/normalize/filter_volume.c
@@ -249,13 +249,15 @@ static int filter_get_audio(mlt_frame frame,
         // Determine numeric limits
         int bytes_per_samp = (samp_width - 1) / 8 + 1;
         int samplemax = (1 << (bytes_per_samp * 8 - 1)) - 1;
+        double g;
 
         for (j = 0; j < *channels; j++) {
             if (channel_mask & (1 << j))
-                for (i = 0; i < *samples; i++, gain += gain_step) {
-                    sample = *p * gain;
-                    *p = ROUND(sample);
-                    if (gain > 1.0 && normalize) {
+                for (i = 0; i < *samples; i++) {
+                    g = gain + i * gain_step;
+                    sample = p[0] * g;
+                    p[0] = ROUND(sample);
+                    if (g > 1.0 && normalize) {
                         /* use limiter function instead of clipping */
                         p[*channels * i + j] = ROUND(
                             samplemax * limiter(sample / (double) samplemax, limiter_level));
@@ -266,8 +268,8 @@ static int filter_get_audio(mlt_frame frame,
         float *p = *buffer;
         for (j = 0; j < *channels; j++) {
             if (channel_mask & (1 << j))
-                for (i = 0; i < *samples; i++, gain += gain_step) {
-                    p[*channels * i + j] *= gain;
+                for (i = 0; i < *samples; i++) {
+                    p[*channels * i + j] *= gain + i * gain_step;
                 }
         }
     }