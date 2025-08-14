mltframework/mlt

commit 604972b255c53927082de3989237f857b7827b74

Make all FFmpeg modules required (#1122)
* Make all FFmpeg modules required

As discussed in #1115

* Remove FFmpeg module ifdefs

src/modules/avformat/factory.c
@@ -38,12 +38,8 @@ extern mlt_link link_swresample_init(mlt_profile profile, mlt_service_type, cons
 // ffmpeg Header files
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-#ifdef AVDEVICE
 #include <libavdevice/avdevice.h>
-#endif
-#ifdef AVFILTER
 #include <libavfilter/avfilter.h>
-#endif
 #include <libavutil/opt.h>
 
 // A static flag used to determine if avformat has been initialised
@@ -54,9 +50,7 @@ static void avformat_init()
     // Initialise avformat if necessary
     if (avformat_initialised == 0) {
         avformat_initialised = 1;
-#ifdef AVDEVICE
         avdevice_register_all();
-#endif
         avformat_network_init();
         av_log_set_level(mlt_log_get_level());
         if (getenv("MLT_AVFORMAT_PRODUCER_CACHE")) {
@@ -77,7 +71,6 @@ static void *create_service(mlt_profile profile, mlt_service_type type, const ch
             return consumer_avformat_init(profile, arg);
     }
 #endif
-#ifdef FILTERS
     if (!strcmp(id, "avcolor_space"))
         return filter_avcolour_space_init(arg);
     if (!strcmp(id, "avcolour_space"))
@@ -90,15 +83,12 @@ static void *create_service(mlt_profile profile, mlt_service_type type, const ch
     }
     if (!strcmp(id, "swscale"))
         return filter_swscale_init(profile, arg);
-#endif
-#ifdef SWRESAMPLE
     if (!strcmp(id, "swresample")) {
         if (type == mlt_service_filter_type)
             return filter_swresample_init(profile, arg);
         else if (type == mlt_service_link_type)
             return link_swresample_init(profile, type, id, arg);
     }
-#endif
     return NULL;
 }
 
@@ -326,7 +316,6 @@ static mlt_properties avformat_metadata(mlt_service_type type, const char *id, v
     return result;
 }
 
-#ifdef AVFILTER
 static mlt_properties avfilter_metadata(mlt_service_type type, const char *id, void *name)
 {
     AVFilter *f = (AVFilter *) avfilter_get_by_name(name);
@@ -419,7 +408,6 @@ static mlt_properties avfilter_metadata(mlt_service_type type, const char *id, v
 
     return metadata;
 }
-#endif
 
 static mlt_properties metadata(mlt_service_type type, const char *id, void *data)
 {
@@ -430,7 +418,6 @@ static mlt_properties metadata(mlt_service_type type, const char *id, void *data
 
 MLT_REPOSITORY
 {
-#ifdef CODECS
     MLT_REGISTER(mlt_service_consumer_type, "avformat", create_service);
     MLT_REGISTER(mlt_service_producer_type, "avformat", create_service);
     MLT_REGISTER(mlt_service_producer_type, "avformat-novalidate", create_service);
@@ -440,8 +427,6 @@ MLT_REPOSITORY
                           "avformat-novalidate",
                           metadata,
                           "producer_avformat-novalidate.yml");
-#endif
-#ifdef FILTERS
     MLT_REGISTER(mlt_service_filter_type, "avcolour_space", create_service);
     MLT_REGISTER(mlt_service_filter_type, "avcolor_space", create_service);
     MLT_REGISTER(mlt_service_filter_type, "avdeinterlace", create_service);
@@ -471,7 +456,6 @@ MLT_REPOSITORY
                           "link_avdeinterlace.yml");
     MLT_REGISTER_METADATA(mlt_service_link_type, "swscale", mlt_link_filter_metadata, NULL);
 
-#ifdef AVFILTER
     char dirname[PATH_MAX];
     snprintf(dirname, PATH_MAX, "%s/avformat/blacklist.txt", mlt_environment("MLT_DATA"));
     mlt_properties blacklist = mlt_properties_load(dirname);
@@ -519,12 +503,8 @@ MLT_REPOSITORY
         }
     }
     mlt_properties_close(blacklist);
-#endif // AVFILTER
-#endif
-#ifdef SWRESAMPLE
     MLT_REGISTER(mlt_service_filter_type, "swresample", create_service);
     MLT_REGISTER_METADATA(mlt_service_filter_type, "swresample", metadata, "filter_swresample.yml");
     MLT_REGISTER(mlt_service_link_type, "swresample", create_service);
     MLT_REGISTER_METADATA(mlt_service_link_type, "swresample", metadata, "link_swresample.yml");
-#endif
 }

CMakeLists.txt
@@ -219,10 +219,6 @@ if(MOD_AVFORMAT)
     AVFORMAT
     SWSCALE
     AVUTIL
-  )
-
-  # Optional deps
-  find_package(FFmpeg COMPONENTS
     AVCODEC
     AVFILTER
     AVDEVICE

src/modules/avformat/CMakeLists.txt
@@ -4,6 +4,18 @@ add_library(mltavformat MODULE
   filter_avcolour_space.c
   filter_avdeinterlace.c
   filter_swscale.c
+  # avformat
+  producer_avformat.c
+  consumer_avformat.c
+  # avfilter
+  filter_avfilter.c
+  link_avdeinterlace.c
+  link_avfilter.c
+  # swresample
+  common_swr.c
+  common_swr.h
+  filter_swresample.c
+  link_swresample.c
 )
 
 file(GLOB YML "*.yml")
@@ -18,45 +30,18 @@ target_compile_options(mltavformat PRIVATE ${MLT_COMPILE_OPTIONS})
 target_link_libraries(mltavformat PRIVATE
   mlt
   Threads::Threads
-  ${AVFORMAT_LIBRARIES}
-  ${SWSCALE_LIBRARIES}
-  ${AVUTIL_LIBRARIES}
+  ${FFMPEG_LIBRARIES}
 )
 
 if(NOT MSVC)
   target_link_libraries(mltavformat PRIVATE m)
 endif()
 
-target_compile_definitions(mltavformat PRIVATE FILTERS)
-
 if(WIN32)
   target_compile_definitions(mltavformat PRIVATE AVDATADIR="share/ffmpeg/")
   if(MINGW)
     target_link_libraries(mltavformat PRIVATE ${MLT_PTHREAD_LIBS})
   endif()
-endif()
-
-if(AVCODEC_FOUND)
-  target_sources(mltavformat PRIVATE producer_avformat.c consumer_avformat.c)
-  target_link_libraries(mltavformat PRIVATE ${AVCODEC_LIBRARIES})
-  target_compile_definitions(mltavformat PRIVATE CODECS)
-endif()
-
-if(AVFILTER_FOUND)
-  target_sources(mltavformat PRIVATE filter_avfilter.c link_avdeinterlace.c link_avfilter.c)
-  target_link_libraries(mltavformat PRIVATE ${AVFILTER_LIBRARIES})
-  target_compile_definitions(mltavformat PRIVATE AVFILTER)
-endif()
-
-if(AVDEVICE_FOUND)
-  target_link_libraries(mltavformat PRIVATE ${AVDEVICE_LIBRARIES})
-  target_compile_definitions(mltavformat PRIVATE AVDEVICE)
-endif()
-
-if(SWRESAMPLE_FOUND)
-  target_sources(mltavformat PRIVATE common_swr.c common_swr.h filter_swresample.c link_swresample.c)
-  target_link_libraries(mltavformat PRIVATE ${SWRESAMPLE_LIBRARIES})
-  target_compile_definitions(mltavformat PRIVATE SWRESAMPLE)
 endif()
 
 if(CPU_MMX)

src/modules/avformat/consumer_avformat.c
@@ -47,11 +47,9 @@
 #include <libavutil/samplefmt.h>
 #include <libavutil/version.h>
 #include <libswscale/swscale.h>
-#ifdef AVFILTER
 #include <libavfilter/avfilter.h>
 #include <libavfilter/buffersink.h>
 #include <libavfilter/buffersrc.h>
-#endif
 
 #define MAX_AUDIO_STREAMS (8)
 #define MAX_SUBTITLE_STREAMS (8)

src/modules/avformat/producer_avformat.c
@@ -51,11 +51,9 @@
 #include <libavutil/version.h>
 #include <libswscale/swscale.h>
 
-#ifdef AVFILTER
 #include <libavfilter/avfilter.h>
 #include <libavfilter/buffersink.h>
 #include <libavfilter/buffersrc.h>
-#endif
 
 // System header files
 #include <limits.h>
@@ -129,11 +127,9 @@ struct producer_avformat_s
     AVRational video_time_base;
     mlt_frame last_good_frame; // for video error concealment
     int last_good_position;    // for video error concealment
-#ifdef AVFILTER
     AVFilterGraph *vfilter_graph;
     AVFilterContext *vfilter_in;
     AVFilterContext *vfilter_out;
-#endif
     int autorotate;
     double rotation;
     int is_audio_synchronizing;
@@ -920,12 +916,9 @@ static int get_basic_info(producer_avformat self, mlt_profile profile, const cha
         mlt_properties_set_int(properties, "height", codec_params->height);
         get_aspect_ratio(properties, format->streams[self->video_index], codec_params);
 
-#ifdef AVFILTER
         int pix_fmt = self->vfilter_out ? av_buffersink_get_format(self->vfilter_out)
                                         : codec_params->format;
-#else
-        int pix_fmt = codec_params->format;
-#endif
+
         pick_av_pixel_format(&pix_fmt, self->full_range);
         if (pix_fmt != AV_PIX_FMT_NONE) {
             // Verify that we can convert this to one of our image formats.
@@ -952,7 +945,6 @@ static int get_basic_info(producer_avformat self, mlt_profile profile, const cha
     return error;
 }
 
-#ifdef AVFILTER
 static int setup_video_filters(producer_avformat self)
 {
     mlt_properties properties = MLT_PRODUCER_PROPERTIES(self->parent);
@@ -1093,7 +1085,6 @@ static int setup_filters(producer_avformat self)
     }
     return error;
 }
-#endif
 
 static void set_up_discard(producer_avformat self, int audio_index, int video_index)
 {
@@ -1258,13 +1249,11 @@ static int producer_open(
                 if (self->audio_format && !self->audio_streams)
                     get_audio_streams_info(self);
 
-#ifdef AVFILTER
                 if (!test_open) {
                     self->autorotate = !mlt_properties_get(properties, "autorotate")
                                        || mlt_properties_get_int(properties, "autorotate");
                     error = setup_filters(self);
                 }
-#endif
             }
         }
     }
@@ -1316,9 +1305,7 @@ static void prepare_reopen(producer_avformat self)
         avformat_close_input(&self->video_format);
     self->audio_format = NULL;
     self->video_format = NULL;
-#ifdef AVFILTER
     avfilter_graph_free(&self->vfilter_graph);
-#endif
     pthread_mutex_unlock(&self->open_mutex);
 
     // Cleanup the packet queues
@@ -2089,12 +2076,10 @@ static void convert_image(producer_avformat self,
 
 static void set_image_size(producer_avformat self, int *width, int *height)
 {
-#ifdef AVFILTER
     if (self->vfilter_out) {
         *width = av_buffersink_get_w(self->vfilter_out);
         *height = av_buffersink_get_h(self->vfilter_out);
     } else {
-#endif
         double dar = mlt_profile_dar(mlt_service_profile(MLT_PRODUCER_SERVICE(self->parent)));
         *width = self->video_codec->width;
         // Workaround 1088 encodings missing cropping info.
@@ -2102,9 +2087,7 @@ static void set_image_size(producer_avformat self, int *width, int *height)
             *height = 1080;
         else
             *height = self->video_codec->height;
-#ifdef AVFILTER
     }
-#endif
 }
 
 /** Allocate the image buffer and set it on the frame.
@@ -2262,14 +2245,12 @@ static int producer_get_image(mlt_frame frame,
     pthread_mutex_lock(&self->video_mutex);
     mlt_log_timings_begin();
 
-#ifdef AVFILTER
     if (self->autorotate && self->video_index != -1
         && get_rotation(properties, self->video_format->streams[self->video_index])
                != self->rotation) {
         // Rotation has changed. Clear any cached frames.
         self->reset_image_cache = 1;
     }
-#endif
 
     if (self->reset_image_cache) {
         self->reset_image_cache = 0;
@@ -2351,13 +2332,9 @@ static int producer_get_image(mlt_frame frame,
     codec_params = stream->codecpar;
 
     // Only change the requested image format for special cases
-#ifdef AVFILTER
     *format = pick_image_format(self->vfilter_out ? av_buffersink_get_format(self->vfilter_out)
                                                   : codec_params->format,
                                 *format);
-#else
-    *format = pick_image_format(codec_params->format, *format);
-#endif
 
     // Duplicate the last image if necessary
     if (self->video_frame && self->video_frame->linesize[0]
@@ -2598,7 +2575,6 @@ static int producer_get_image(mlt_frame frame,
                                             || codec_params->field_order == AV_FIELD_TB;
                 }
                 self->video_frame->top_field_first = self->top_field_first;
-#ifdef AVFILTER
                 if ((self->autorotate || mlt_properties_get(properties, "filtergraph"))
                     && !setup_filters(self) && self->vfilter_graph) {
                     int ret = av_buffersrc_add_frame(self->vfilter_in, self->video_frame);
@@ -2614,7 +2590,7 @@ static int producer_get_image(mlt_frame frame,
                         }
                     }
                 }
-#endif
+
                 set_image_size(self, width, height);
                 if ((image_size
                      = allocate_buffer(frame, codec_params, buffer, *format, *width, *height))) {
@@ -4051,9 +4027,7 @@ static void producer_avformat_close(producer_avformat self)
         avformat_close_input(&self->video_format);
     if (self->is_mutex_init)
         pthread_mutex_unlock(&self->open_mutex);
-#ifdef AVFILTER
     avfilter_graph_free(&self->vfilter_graph);
-#endif
 
     // Cleanup caches.
     mlt_cache_close(self->image_cache);

commit e25fae1b77c4b24c117b1b5c413534b182b74419

Fix avformat unable to open files
regression in 604972b2 #1122

src/modules/avformat/factory.c
@@ -63,14 +63,12 @@ static void avformat_init()
 static void *create_service(mlt_profile profile, mlt_service_type type, const char *id, void *arg)
 {
     avformat_init();
-#ifdef CODECS
     if (!strncmp(id, "avformat", 8)) {
         if (type == mlt_service_producer_type)
             return producer_avformat_init(profile, id, arg);
         else if (type == mlt_service_consumer_type)
             return consumer_avformat_init(profile, arg);
     }
-#endif
     if (!strcmp(id, "avcolor_space"))
         return filter_avcolour_space_init(arg);
     if (!strcmp(id, "avcolour_space"))