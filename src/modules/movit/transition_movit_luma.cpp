mltframework/mlt

commit 58132ea1f778e2b106c0e801630a15f81bc7082e

Fix transition progress with linear color_trc
Transition progress would not appear to have a smooth, linear speed as
people expect.

See https://forum.shotcut.org/t/speed-control-for-transitions-and-video-
fades-for-both-cpu-and-gpu/48962

src/modules/movit/transition_movit_luma.cpp
@@ -1,6 +1,6 @@
 /*
  * transition_movit_luma.cpp
- * Copyright (C) 2014-2023 Dan Dennedy <dan@dennedy.org>
+ * Copyright (C) 2014-2025 Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,12 +19,10 @@
 
 #include <framework/mlt.h>
 #include <framework/mlt_luma_map.h>
-#include <limits.h>
 #include <math.h>
 #include <string.h>
 
 #include "filter_glsl_manager.h"
-#include "mlt_movit_input.h"
 #include <effect_chain.h>
 #include <init.h>
 #include <luma_mix_effect.h>
@@ -61,6 +59,13 @@ static int get_image(mlt_frame a_frame,
     double mix = mlt_transition_get_progress(transition, a_frame);
     double inverse = 1.0 - mix;
     double softness = mlt_properties_anim_get_double(properties, "softness", position, length);
+    int invert = mlt_properties_get_int(properties, "invert");
+
+    // Make progress appear linear using a gamma curve similar to sRGB or Rec. 709
+    if (invert)
+        mix = 1.0 - pow(1.0 - mix, 2.0);
+    else
+        mix = pow(mix, 2.0);
 
     if (c_frame) {
         // Set the Movit parameters.
@@ -72,9 +77,7 @@ static int get_image(mlt_frame a_frame,
         mlt_properties_set_double(properties,
                                   "_movit.parms.float.transition_width",
                                   1.0 / (softness + 1.0e-4));
-        mlt_properties_set_int(properties,
-                               "_movit.parms.int.inverse",
-                               !mlt_properties_get_int(properties, "invert"));
+        mlt_properties_set_int(properties, "_movit.parms.int.inverse", !invert);
 
         uint8_t *a_image, *b_image, *c_image;
 

src/modules/core/transition_luma.c
@@ -992,6 +992,16 @@ static int transition_get_image(mlt_frame a_frame,
             mlt_properties_set(&b_frame->parent,
                                "distort",
                                mlt_properties_get(&a_frame->parent, "distort"));
+
+        if (mlt_color_trc_linear
+            == mlt_image_color_trc_id(mlt_properties_get(a_props, "consumer.mlt_color_trc"))) {
+            // Make progress appear linear using a gamma curve similar to sRGB or Rec. 709
+            if (reverse || invert)
+                mix = 1.0 - pow(1.0 - mix, 2.0);
+            else
+                mix = pow(mix, 2.0);
+        }
+
         mix = (reverse || invert) ? 1 - mix : mix;
         invert = 0;
         // Dissolve the frames using the time offset for mix value

commit ee8adcfbc203995544a8da44f7146cc23fb63fd6

Fix alpha channel regression in movit.luma transition
Regression in 58132ea1

src/modules/movit/transition_movit_luma.cpp
@@ -57,7 +57,6 @@ static int get_image(mlt_frame a_frame,
     mlt_position length = mlt_transition_get_length(transition);
     int reverse = mlt_properties_get_int(properties, "reverse");
     double mix = mlt_transition_get_progress(transition, a_frame);
-    double inverse = 1.0 - mix;
     double softness = mlt_properties_anim_get_double(properties, "softness", position, length);
     int invert = mlt_properties_get_int(properties, "invert");
 
@@ -66,6 +65,7 @@ static int get_image(mlt_frame a_frame,
         mix = 1.0 - pow(1.0 - mix, 2.0);
     else
         mix = pow(mix, 2.0);
+    double inverse = 1.0 - mix;
 
     if (c_frame) {
         // Set the Movit parameters.