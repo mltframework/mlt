mltframework/mlt

commit 1ddb9c69da752339eb04888139f2bd1d3a5a31d9

Fix #1182 crash on invalid profile (CVE-2025-65834)

src/framework/mlt_factory.c
@@ -327,6 +327,9 @@ mlt_producer mlt_factory_producer(mlt_profile profile, const char *service, cons
 {
     mlt_producer obj = NULL;
 
+    if (!mlt_profile_is_valid(profile))
+        return obj;
+
     // Pick up the default normalizing producer if necessary
     if (service == NULL)
         service = mlt_environment("MLT_PRODUCER");

src/framework/mlt_consumer.c
@@ -478,6 +478,11 @@ int mlt_consumer_start(mlt_consumer self)
         return error;
 
     consumer_private *priv = self->local;
+    mlt_profile profile = mlt_service_profile(MLT_CONSUMER_SERVICE(self));
+
+    if (!mlt_profile_is_valid(profile)) {
+        return 1;
+    }
 
     // Stop listening to the property-changed event
     mlt_event_block(priv->event_listener);
@@ -498,7 +503,6 @@ int mlt_consumer_start(mlt_consumer self)
     if (test_card != NULL) {
         if (mlt_properties_get_data(properties, "test_card_producer", NULL) == NULL) {
             // Create a test card producer
-            mlt_profile profile = mlt_service_profile(MLT_CONSUMER_SERVICE(self));
             mlt_producer producer = mlt_factory_producer(profile, NULL, test_card);
 
             // Do we have a producer
@@ -523,7 +527,7 @@ int mlt_consumer_start(mlt_consumer self)
     }
 
     // The profile could have changed between a stop and a restart.
-    apply_profile_properties(self, mlt_service_profile(MLT_CONSUMER_SERVICE(self)), properties);
+    apply_profile_properties(self, profile, properties);
 
     // Set the frame duration in microseconds for the frame-dropping heuristic
     int frame_rate_num = mlt_properties_get_int(properties, "frame_rate_num");

src/modules/xml/producer_xml.c
@@ -2158,7 +2158,7 @@ mlt_producer producer_xml_init(mlt_profile profile,
     xmlcontext->sax = sax;
     xmlcontext->_private = (void *) context;
     xmlParseDocument(xmlcontext);
-    well_formed = xmlcontext->wellFormed;
+    well_formed = xmlcontext->wellFormed && mlt_profile_is_valid(profile);
 
     // Cleanup after parsing
     xmlFreeDoc(context->entity_doc);

commit 0af529c453c15ab505629897143f62e0890dae42

Fix xml-clip producer regression in 1ddb9c6
Also fixes constructing a producer through the factory with a null
profile.

CMakeLists.txt
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.14...3.31)
 
 project(MLT
-  VERSION 7.36.0
+  VERSION 7.36.1
   DESCRIPTION "Multimedia Framework"
   HOMEPAGE_URL "https://www.mltframework.org"
   LANGUAGES C CXX

docs/melt.1
@@ -1,5 +1,5 @@
 .\" DO NOT MODIFY THIS FILE!  It was generated by help2man 1.38.4.
-.TH MELT "1" "December 2025" "melt 7.36.0" "User Commands"
+.TH MELT "1" "December 2025" "melt 7.36.1" "User Commands"
 .SH NAME
 melt \- author, play, and encode multitrack audio/video compositions
 .SH SYNOPSIS

src/framework/mlt_factory.c
@@ -327,7 +327,7 @@ mlt_producer mlt_factory_producer(mlt_profile profile, const char *service, cons
 {
     mlt_producer obj = NULL;
 
-    if (!mlt_profile_is_valid(profile))
+    if (profile && !mlt_profile_is_valid(profile))
         return obj;
 
     // Pick up the default normalizing producer if necessary

src/framework/mlt_version.h
@@ -28,7 +28,7 @@
 
 #define LIBMLT_VERSION_MAJOR 7
 #define LIBMLT_VERSION_MINOR 36
-#define LIBMLT_VERSION_REVISION 0
+#define LIBMLT_VERSION_REVISION 1
 #define LIBMLT_VERSION_INT \
     ((LIBMLT_VERSION_MAJOR << 16) + (LIBMLT_VERSION_MINOR << 8) + LIBMLT_VERSION_REVISION)
 #define LIBMLT_VERSION \

src/modules/xml/producer_xml-clip.c
@@ -1,6 +1,6 @@
 /*
  * producer_xml-clip.c -- A wrapper for mlt XML in native profile
- * Copyright (C) 2024 Meltytech, LLC
+ * Copyright (C) 2024-2025 Meltytech, LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -173,7 +173,7 @@ mlt_producer producer_xmlclip_init(mlt_profile profile,
                                    const char *id,
                                    char *arg)
 {
-    mlt_profile native_profile = calloc(1, sizeof(struct mlt_profile_s));
+    mlt_profile native_profile = mlt_profile_init(NULL);
     mlt_producer xml_producer = mlt_factory_producer(native_profile, "xml", arg);
     mlt_producer self = mlt_producer_new(native_profile);
 