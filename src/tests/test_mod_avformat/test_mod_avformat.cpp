f6fa374486d53ad0c1b413cdf29fdc1204b2c3ab
Julius Künzel <julius.kuenzel@kde.org>
2026-02-02 23:14:00 +0100

    Add basic test for avformat producer

---
+++ src/tests/test_mod_avformat/test_mod_avformat.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2026 Julius Künzel <julius.kuenzel@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with consumer library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <QtTest>
+
+#include <mlt++/Mlt.h>
+using namespace Mlt;
+
+class TestModAvformat : public QObject
+{
+    Q_OBJECT
+
+public:
+    TestModAvformat() { Factory::init(); }
+
+    ~TestModAvformat() {}
+
+private Q_SLOTS:
+    void BasicAvformatProducer()
+    {
+        Profile profile;
+        Producer producer(profile, "avformat:blue.mpg");
+    }
+
+    void BasicAvformatNovalidateProducer()
+    {
+        Profile profile;
+        Producer producer(profile, "avformat-novalidate:blue.mpg");
+    }
+};
+
+QTEST_APPLESS_MAIN(TestModAvformat)
+
+#include "test_mod_avformat.moc"
