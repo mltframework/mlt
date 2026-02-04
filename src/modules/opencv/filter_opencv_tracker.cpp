f4641c55a9eb927872e55a1dfbd81bc5f0ed7dbd
Julius Künzel <julius.kuenzel@kde.org>
2026-02-04 22:25:58 +0100

    Enable and fix opencv module with MSVC

--- src/modules/opencv/filter_opencv_tracker.cpp
+++ src/modules/opencv/filter_opencv_tracker.cpp
@@ -29,7 +29,9 @@
 #include <opencv2/tracking/tracking_legacy.hpp>
 #include <sys/stat.h>  // for stat()
 #include <sys/types.h> // for stat()
+#ifndef _MSC_VER
 #include <unistd.h>    // for stat()
+#endif
 #endif
 
 typedef struct
